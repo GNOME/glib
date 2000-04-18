7c467669cc1d4f83bd7c5ecfe440bfee8c2f4504
Sebastian Wilhelmi <wilhelmi@ira.uka.de>
2000-04-18 13:00:35 +0000

    Changed comment for g_getenv to reflect, that the returned memory must not
    2000-04-18  Sebastian Wilhelmi  <wilhelmi@ira.uka.de>
    
    	* glib.h: Changed comment for g_getenv to reflect, that the
    	returned memory must not be freed. Fixes Bug #8983.

--- glib.h
+++ glib.h
@@ -1685,8 +1685,10 @@ gchar*  g_path_skip_root	(gchar       *file_name);
 /* strings are newly allocated with g_malloc() */
 gchar*	g_dirname		(const gchar *file_name);
 gchar*	g_get_current_dir	(void);
-gchar*  g_getenv		(const gchar *variable);
 
+/* return the environment string for the variable. The returned memory
+ * must not be freed. */
+gchar*  g_getenv		(const gchar *variable);
 
 /* we use a GLib function as a replacement for ATEXIT, so
  * the programmer is not required to check the return value
