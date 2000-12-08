0b6e1350e10dbf47a992d08744d0f2cd23f2341e
Raja R Harinath <harinath@src.gnome.org>
2000-12-08 21:04:52 +0000

    Rearrange into other veriables, and include run-markup-tests.sh.
    * tests/Makefile.am (TESTS): Rearrange into other veriables, and
    include run-markup-tests.sh.
    (TESTS_ENVIRONMENT): New.  Pass $srcdir to tests.
    (noinst_PROGRAMS): Rename to ...
    (check_PROGRAMS): ... this.  'automake' ensures that these are
    built before running the tests.
    * tests/run-markup-tests.sh: Support $srcdir != $builddir.

--- tests/run-markup-tests.sh
+++ tests/run-markup-tests.sh
@@ -6,12 +6,12 @@ fail ()
   exit 1
 }
 
-for I in markups/fail-*.gmarkup; do
+for I in $srcdir/markups/fail-*.gmarkup; do
   echo "Parsing $I, should fail"
   ./markup-test $I > /dev/null && fail "failed to generate error on $I"
 done
 
-for I in markups/valid-*.gmarkup; do
+for I in $srcdir/markups/valid-*.gmarkup; do
   echo "Parsing $I, should succeed"
   ./markup-test $I > /dev/null || fail "failed on $I"
 done
