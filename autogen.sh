33888fc0c841d6df20e2fb522e80e032073d9acd
Owen Taylor <otaylor@redhat.com>
2001-05-16 18:35:39 +0000

    Add inter-library dependencies.
    Sun May 13 10:31:17 2001  Owen Taylor  <otaylor@redhat.com>
    
    	* glib/Makefile.am gobject/Makefile.am gmodule/Makefile.am:
    	Add inter-library dependencies.
    
    	* acinclude.m4: Remove libtool macros.
    
    	* autogen.sh: Require libtool-1.4, automake-1.4p1.

--- autogen.sh
+++ autogen.sh
@@ -12,21 +12,49 @@ FILE=glib.h
 
 DIE=0
 
+have_libtool=false
+if libtool --version < /dev/null > /dev/null 2>&1 ; then
+	libtool_version=`libtoolize --version |  libtoolize --version | sed 's/^[^0-9]*\([0-9.]\+\).*/\1/'`
+	case $libtool_version in
+	    1.4*)
+		have_libtool=true
+		;;
+	esac
+fi
+if $have_libtool ; then : ; else
+	echo
+	echo "You must have libtool 1.4 installed to compile $PROJECT."
+	echo "Install the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	DIE=1
+fi
+
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
 	echo
 	echo "You must have autoconf installed to compile $PROJECT."
-	echo "Download the appropriate package for your distribution,"
+	echo "libtool the appropriate package for your distribution,"
 	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
 	DIE=1
 }
 
-(automake --version) < /dev/null > /dev/null 2>&1 || {
+have_automake=false
+if automake --version < /dev/null > /dev/null 2>&1 ; then
+	automake_version=`automake --version | grep 'automake (GNU automake)' | sed 's/^[^0-9]*\(.*\)/\1/'`
+	case $automake_version in
+	   1.2*|1.3*|1.4) 
+		;;
+	   *)
+		have_automake=true
+		;;
+	esac
+fi
+if $have_automake ; then : ; else
 	echo
-	echo "You must have automake installed to compile $PROJECT."
-	echo "Get ftp://sourceware.cygnus.com/pub/automake/automake-1.4.tar.gz"
+	echo "You must have automake 1.4-p1 installed to compile $PROJECT."
+	echo "Get ftp://ftp.gnu.org/pub/gnu/automake/automake-1.4-p1.tar.gz"
 	echo "(or a newer version if it is available)"
 	DIE=1
-}
+fi
 
 if test "$DIE" -eq 1; then
 	exit 1
