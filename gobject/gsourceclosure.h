0038581ed460f8fed8c373275d75f75a8fa3c559
Owen Taylor <otaylor@redhat.com>
2001-09-03 22:13:16 +0000

    Implement.
    Mon Aug 27 14:55:27 2001  Owen Taylor  <otaylor@redhat.com>
    
    	* gsourceclosure.[ch] (g_source_set_closure): Implement.
    
    	* gsourceclosure.[ch]: Add GType's for GIOChannel, GIOCondition.
    
    Mon Aug 27 14:56:12 2001  Owen Taylor  <otaylor@redhat.com>
    
    	* glib/gmain.h: Add closure_marshal/closure_callback
    	fields to GSourceFuncs for use by g_source_set_closure().
    
    	* glib/gmain.c glib/giounix.c glib/giowin32.c
    	  glib/gmain.h: Export the SourceFuncs vtables so GObject
    	can use them to figure out closure callbacks/marshallers
    	for the default source types.

---
+++ gobject/gsourceclosure.h
@@ -0,0 +1,37 @@
+/* GObject - GLib Type, Object, Parameter and Signal Library
+ * Copyright (C) 2001 Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __G_SOURCECLOSURE_H__
+
+#include <gobject/gclosure.h>
+
+G_BEGIN_DECLS
+
+void g_source_set_closure (GSource  *source,
+			   GClosure *closure);
+
+GType g_io_channel_get_type   (void);
+GType g_io_condition_get_type (void);
+
+#define G_TYPE_IO_CHANNEL (g_io_channel_get_type ())
+#define G_TYPE_IO_CONDITION (g_io_condition_get_type ())
+
+G_END_DECLS
+
+#endif /* __G_SOURCECLOSURE_H__ */
