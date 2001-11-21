241061620067326ab77713c12ac18047442654d0
Tor Lillqvist <tml@iki.fi>
2001-11-21 22:01:50 +0000

    Set G_LOG_DOMAIN as in Makefile.am.
    2001-11-21  Tor Lillqvist  <tml@iki.fi>
    
    	* glib/makefile.mingw.in (DEFINES): Set G_LOG_DOMAIN as in
    	Makefile.am.
    
    gobject:
    
    2001-11-22  Tor Lillqvist  <tml@iki.fi>
    
    	* makefile.mingw.in (DEFINES): Set G_LOG_DOMAIN as in Makefile.am.
    
    	* gparamspecs.h (GOBJECT_VAR): Can't use GLIB_VAR here, as that
    	makes sense only when building libglib. GOBJECT_VAR is similar for
    	libgobject.
    
    	* gobject.def: Add g_param_spec_types, remove
    	g_type_fundamental_branch_last.

--- gobject/gparamspecs.h
+++ gobject/gparamspecs.h
@@ -384,7 +384,30 @@ GParamSpec*	g_param_spec_object	 (const gchar	 *name,
 
 
 /* --- internal --- */
-GLIB_VAR GType *g_param_spec_types;
+/* We prefix variable declarations so they can
+ * properly get exported in windows dlls.
+ */
+#ifndef GOBJECT_VAR
+#  ifdef G_PLATFORM_WIN32
+#    ifdef GOBJECT_STATIC_COMPILATION
+#      define GOBJECT_VAR extern
+#    else /* !GOBJECT_STATIC_COMPILATION */
+#      ifdef GOBJECT_COMPILATION
+#        ifdef DLL_EXPORT
+#          define GOBJECT_VAR __declspec(dllexport)
+#        else /* !DLL_EXPORT */
+#          define GOBJECT_VAR extern
+#        endif /* !DLL_EXPORT */
+#      else /* !GOBJECT_COMPILATION */
+#        define GOBJECT_VAR extern __declspec(dllimport)
+#      endif /* !GOBJECT_COMPILATION */
+#    endif /* !GOBJECT_STATIC_COMPILATION */
+#  else /* !G_PLATFORM_WIN32 */
+#    define GOBJECT_VAR extern
+#  endif /* !G_PLATFORM_WIN32 */
+#endif /* GOBJECT_VAR */
+
+GOBJECT_VAR GType *g_param_spec_types;
 
 G_END_DECLS
 
