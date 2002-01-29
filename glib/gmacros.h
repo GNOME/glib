b19bcd9604fee9251130d109f068fc97e4198fd5
Havoc Pennington <hp@redhat.com>
2002-01-29 00:29:24 +0000

    get rid of warning here
    2002-01-28  Havoc Pennington  <hp@redhat.com>
    
    	* glib/gmacros.h: get rid of warning here

--- glib/gmacros.h
+++ glib/gmacros.h
@@ -82,7 +82,7 @@
 /* Wrap the gcc __PRETTY_FUNCTION__ and __FUNCTION__ variables with
  * macros, so we can refer to them as strings unconditionally.
  */
-#ifdef  __GNUC__ && (__GNUC__ < 3)
+#if defined (__GNUC__) && (__GNUC__ < 3)
 #define G_GNUC_FUNCTION         __FUNCTION__
 #define G_GNUC_PRETTY_FUNCTION  __PRETTY_FUNCTION__
 #else   /* !__GNUC__ */
