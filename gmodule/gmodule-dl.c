c2d49b8f7e57e5527ec400677164ec0862005fa9
Owen Taylor <otaylor@redhat.com>
2002-03-02 20:30:33 +0000

    Use G_MODULE_SUFFIX rather than a hardcoded ".so". (#72803)
    Sat Mar  2 14:46:17 2002  Owen Taylor  <otaylor@redhat.com>
    
            * gmodule-beos.c (_g_module_build_path) * gmodule-dyld.c (_g_module_build_path)
              gmodule-dl.c (_g_module_build_path): Use G_MODULE_SUFFIX rather than
            a hardcoded ".so". (#72803)

--- gmodule/gmodule-dl.c
+++ gmodule/gmodule-dl.c
@@ -154,9 +154,9 @@ _g_module_build_path (const gchar *directory,
     if (strncmp (module_name, "lib", 3) == 0)
       return g_strconcat (directory, "/", module_name, NULL);
     else
-      return g_strconcat (directory, "/lib", module_name, ".so", NULL);
+      return g_strconcat (directory, "/lib", module_name, "." G_MODULE_SUFFIX, NULL);
   } else if (strncmp (module_name, "lib", 3) == 0)
     return g_strdup (module_name);
   else
-    return g_strconcat ("lib", module_name, ".so", NULL);
+    return g_strconcat ("lib", module_name, "." G_MODULE_SUFFIX, NULL);
 }
