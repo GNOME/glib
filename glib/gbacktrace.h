21ec3dfd7f8a74ec80dbcfb81d3884e29dcaf5f8
Owen Taylor <otaylor@redhat.com>
2002-05-06 20:07:27 +0000

    Exclude OSF from alpha definition of G_BREAKPOINT(), since the assembler
    Mon May  6 16:00:41 2002  Owen Taylor  <otaylor@redhat.com>
    
            * glib/gbacktrace.h: Exclude OSF from alpha definition
            of G_BREAKPOINT(), since the assembler apparently
            doesn't support bpt. (#77852, Gareth Pierce)

--- glib/gbacktrace.h
+++ glib/gbacktrace.h
@@ -44,13 +44,13 @@ void g_on_error_query (const gchar *prg_name);
 void g_on_error_stack_trace (const gchar *prg_name);
 
 /* Hacker macro to place breakpoints for elected machines.
- * Actual use is strongly deprecated of course ;)
+ * Actual use is strongly discouraged of course ;)
  */
 #if defined (__i386__) && defined (__GNUC__) && __GNUC__ >= 2
 #  define G_BREAKPOINT()	G_STMT_START{ __asm__ __volatile__ ("int $03"); }G_STMT_END
 #elif defined (_MSC_VER) && defined (_M_IX86)
 #  define G_BREAKPOINT()	G_STMT_START{ __asm int 3h }G_STMT_END
-#elif defined (__alpha__) && defined (__GNUC__) && __GNUC__ >= 2
+#elif defined (__alpha__) && !defined(__osf__) && defined (__GNUC__) && __GNUC__ >= 2
 #  define G_BREAKPOINT()	G_STMT_START{ __asm__ __volatile__ ("bpt"); }G_STMT_END
 #else	/* !__i386__ && !__alpha__ */
 #  define G_BREAKPOINT()	G_STMT_START{ raise (SIGTRAP); }G_STMT_END
