a8b44c4d7905f8a58ed56fc73533626e881ff2c5
Owen Taylor <otaylor@redhat.com>
2002-05-07 19:03:23 +0000

    Fix include order for config.h (#71704)
    Tue May  7 15:03:02 2002  Owen Taylor  <otaylor@redhat.com>
    
            * glib-genmarshal.c: Fix include order for config.h (#71704)

--- gobject/glib-genmarshal.c
+++ gobject/glib-genmarshal.c
@@ -16,10 +16,11 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
+#include	"config.h"
+
 #undef G_LOG_DOMAIN
 #define G_LOG_DOMAIN "GLib-Genmarshal"
 #include	<glib.h>
-#include	"config.h"
 
 #include	<stdio.h>
 #include	<stdlib.h>
