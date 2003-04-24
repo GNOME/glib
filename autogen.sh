f322e6413e78342137c40985a56e574e23d4fc05
Owen Taylor <otaylor@redhat.com>
2003-04-24 23:51:37 +0000

    Accept libtool-1.5. (#111483)
    Thu Apr 24 19:12:05 2003  Owen Taylor  <otaylor@redhat.com>
    
            * autogen.sh (have_libtool): Accept libtool-1.5. (#111483)

--- autogen.sh
+++ autogen.sh
@@ -16,7 +16,7 @@ have_libtool=false
 if libtool --version < /dev/null > /dev/null 2>&1 ; then
 	libtool_version=`libtoolize --version |  libtoolize --version | sed 's/^[^0-9]*\([0-9.][0-9.]*\).*/\1/'`
 	case $libtool_version in
-	    1.4*)
+	    1.4*|1.5*)
 		have_libtool=true
 		;;
 	esac
