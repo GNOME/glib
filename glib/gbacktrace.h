d8ba1394da7996a779344e8274653c3936462b64
Manish Singh <yosh@gimp.org>
2004-03-12 23:31:23 +0000

    ia32's G_BREAKPOINT() implementation works on amd64 too. Enable it.
    Fri Mar 12 15:30:58 2004  Manish Singh  <yosh@gimp.org>
    
            * glib/gbacktrace.h: ia32's G_BREAKPOINT() implementation works on
            amd64 too. Enable it.

--- glib/gbacktrace.h
+++ glib/gbacktrace.h
@@ -43,10 +43,10 @@ G_BEGIN_DECLS
 void g_on_error_query (const gchar *prg_name);
 void g_on_error_stack_trace (const gchar *prg_name);
 
-/* Hacker macro to place breakpoints for elected machines.
+/* Hacker macro to place breakpoints for selected machines.
  * Actual use is strongly discouraged of course ;)
  */
-#if defined (__i386__) && defined (__GNUC__) && __GNUC__ >= 2
+#if (defined (__i386__) || defined (__x86_64__)) && defined (__GNUC__) && __GNUC__ >= 2
 #  define G_BREAKPOINT()	G_STMT_START{ __asm__ __volatile__ ("int $03"); }G_STMT_END
 #elif defined (_MSC_VER) && defined (_M_IX86)
 #  define G_BREAKPOINT()	G_STMT_START{ __asm int 3h }G_STMT_END
