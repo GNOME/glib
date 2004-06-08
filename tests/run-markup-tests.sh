6c195ce130585edf7ff4556a75e10cc81fc70d03
Matthias Clasen <matthiasc@src.gnome.org>
2004-06-08 02:29:16 +0000

    Make "make check" less noisy.

--- tests/run-markup-tests.sh
+++ tests/run-markup-tests.sh
@@ -6,17 +6,29 @@ fail ()
   exit 1
 }
 
+echo_v ()
+{
+  if [ "$verbose" = "1" ]; then
+    echo "$*"
+  fi
+}
+
+error_out=/dev/null
+if [ "$1" = "-v" ]; then
+  verbose=1
+  error_out=/dev/stderr
+fi  
 for I in $srcdir/markups/fail-*.gmarkup; do
-  echo "Parsing $I, should fail"
-  ./markup-test $I > /dev/null && fail "failed to generate error on $I"
+  echo_v "Parsing $I, should fail"
+  ./markup-test $I > /dev/null 2> $error_out && fail "failed to generate error on $I"
   if test "$?" != "1"; then
     fail "unexpected error on $I"
   fi  
 done
 
 for I in $srcdir/markups/valid-*.gmarkup; do
-  echo "Parsing $I, should succeed"
-  ./markup-test $I > /dev/null || fail "failed on $I"
+  echo_v "Parsing $I, should succeed"
+  ./markup-test $I > /dev/null 2> $error_out || fail "failed on $I"
 done
 
-echo "All tests passed."
+echo_v "All tests passed."
