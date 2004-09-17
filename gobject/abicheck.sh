d8357e2c516663c1b436b43f18827c8ecd3879d5
Manish Singh <yosh@gimp.org>
2004-09-17 01:41:30 +0000

    don't hardcode lengths for cut, instead split on the third field.
    Thu Sep 16 18:42:46 2004  Manish Singh  <yosh@gimp.org>
    
            * abicheck.sh: don't hardcode lengths for cut, instead split on
            the third field.

--- gobject/abicheck.sh
+++ gobject/abicheck.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 cpp -P -DG_OS_UNIX -DINCLUDE_INTERNAL_SYMBOLS ${srcdir:-.}/gobject.symbols | sed -e '/^$/d' | sort > expected-abi
-nm -D .libs/libgobject-2.0.so | grep " T " | cut -c12- | sort > actual-abi
+nm -D .libs/libgobject-2.0.so | grep " T " | cut -d ' ' -f 3 | sort > actual-abi
 diff -u expected-abi actual-abi && rm expected-abi actual-abi
