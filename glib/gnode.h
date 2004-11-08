a84cbafcecc40c05cbed34b2bfd2ab89d1de500b
Matthias Clasen <mclasen@redhat.com>
2004-11-08 19:14:04 +0000

    Add G_TRAVERSE_LEAVES and G_TRAVERSE_NON_LEAVES as alternative names for
    2004-11-08  Matthias Clasen  <mclasen@redhat.com>
    
    	* glib/gnode.h (enum GTraverseFlags): Add G_TRAVERSE_LEAVES
    	and G_TRAVERSE_NON_LEAVES as alternative names for
    	G_TRAVERSE_LEAFS and G_TRAVERSE_NON_LEAFS, which are
    	grammatically brutal.  (#138853, Morten Welinder)

--- glib/gnode.h
+++ glib/gnode.h
@@ -36,10 +36,12 @@ typedef struct _GNode		GNode;
 /* Tree traverse flags */
 typedef enum
 {
-  G_TRAVERSE_LEAFS      = 1 << 0,
-  G_TRAVERSE_NON_LEAFS  = 1 << 1,
-  G_TRAVERSE_ALL        = G_TRAVERSE_LEAFS | G_TRAVERSE_NON_LEAFS,
-  G_TRAVERSE_MASK       = 0x03
+  G_TRAVERSE_LEAVES     = 1 << 0,
+  G_TRAVERSE_NON_LEAVES = 1 << 1,
+  G_TRAVERSE_ALL        = G_TRAVERSE_LEAVES | G_TRAVERSE_NON_LEAVES,
+  G_TRAVERSE_MASK       = 0x03,
+  G_TRAVERSE_LEAFS      = G_TRAVERSE_LEAVES,
+  G_TRAVERSE_NON_LEAFS  = G_TRAVERSE_NON_LEAVES
 } GTraverseFlags;
 
 /* Tree traverse orders */
