43fde23ceed1ce2f38b2f5bf0d9a181b9c3d5afa
Manish Singh <yosh@gimp.org>
2004-11-25 22:10:25 +0000

    filter out G_GNUC stuff when doing the compare.
    Thu Nov 25 14:09:41 2004  Manish Singh  <yosh@gimp.org>
    
            * abicheck.sh: filter out G_GNUC stuff when doing the compare.

--- gobject/abicheck.sh
+++ gobject/abicheck.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-cpp -P -DG_OS_UNIX ${srcdir:-.}/gobject.symbols | sed -e '/^$/d' -e 's/ PRIVATE$//' | sort > expected-abi
+cpp -P -DG_OS_UNIX ${srcdir:-.}/gobject.symbols | sed -e '/^$/d' -e 's/ PRIVATE$//' -e 's/ G_GNUC.*$//' | sort > expected-abi
 nm -D .libs/libgobject-2.0.so | grep " T " | cut -d ' ' -f 3 | sort > actual-abi
 diff -u expected-abi actual-abi && rm expected-abi actual-abi
