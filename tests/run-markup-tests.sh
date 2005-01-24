5d549e5bdb4c1fad580de06f3f1addf734222baf
Matthias Clasen <mclasen@redhat.com>
2005-01-24 17:35:44 +0000

    Let $srcdir default to .
    2005-01-24  Matthias Clasen  <mclasen@redhat.com>
    
    	* tests/run-markup-tests.sh: Let $srcdir default to .

--- tests/run-markup-tests.sh
+++ tests/run-markup-tests.sh
@@ -18,7 +18,7 @@ if [ "$1" = "-v" ]; then
   verbose=1
   error_out=/dev/stderr
 fi  
-for I in $srcdir/markups/fail-*.gmarkup; do
+for I in ${srcdir:-.}/markups/fail-*.gmarkup; do
   echo_v "Parsing $I, should fail"
   ./markup-test $I > /dev/null 2> $error_out && fail "failed to generate error on $I"
   if test "$?" != "1"; then
@@ -26,7 +26,7 @@ for I in $srcdir/markups/fail-*.gmarkup; do
   fi  
 done
 
-for I in $srcdir/markups/valid-*.gmarkup; do
+for I in ${srcdir:-.}/markups/valid-*.gmarkup; do
   echo_v "Parsing $I, should succeed"
   ./markup-test $I > /dev/null 2> $error_out || fail "failed on $I"
 done
