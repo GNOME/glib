ccfa9102064dec5c060e83b83147a51834f23917
Manish Singh <yosh@gimp.org>
2005-02-07 06:08:14 +0000

    feed the contents of config.h and glibconfig.h into the preprocessor,
    Sun Feb  6 22:00:46 2005  Manish Singh  <yosh@gimp.org>
    
            * abicheck.sh: feed the contents of config.h and glibconfig.h
            into the preprocessor, since glib.symbols could need #defines
            from them.

--- gobject/abicheck.sh
+++ gobject/abicheck.sh
@@ -1,5 +1,13 @@
 #! /bin/sh
 
-cpp -P -DG_OS_UNIX ${srcdir:-.}/gobject.symbols | sed -e '/^$/d' -e 's/ G_GNUC.*$//' -e 's/ PRIVATE$//' | sort > expected-abi
+egrep '^#([^i]|if).*[^\]$' "${top_builddir:-..}/glibconfig.h" > glibconfig.cpp
+
+INCLUDES="-include ${top_builddir:-..}/config.h"
+INCLUDES="$INCLUDES -include glibconfig.cpp"
+
+cpp -P $INCLUDES ${srcdir:-.}/gobject.symbols | sed -e '/^$/d' -e 's/ G_GNUC.*$//' -e 's/ PRIVATE$//' | sort > expected-abi
+rm glibconfig.cpp
+
 nm -D .libs/libgobject-2.0.so | grep " T " | cut -d ' ' -f 3 | sort > actual-abi
+
 diff -u expected-abi actual-abi && rm expected-abi actual-abi
