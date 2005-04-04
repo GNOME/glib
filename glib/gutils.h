b73f494fab6efc2dd9f49c166ddf96496016113d
Matthias Clasen <mclasen@redhat.com>
2005-04-04 03:45:13 +0000

    Undef G_CAN_INLINE if G_IMPLEMENT_INLINES is defined. This was the pre-2.6
    2005-04-03  Matthias Clasen  <mclasen@redhat.com>
    
    	* glib/gutils.h: Undef G_CAN_INLINE if G_IMPLEMENT_INLINES is
    	defined. This was the pre-2.6 behaviour, and without it,
    	G_IMPLEMENT_INLINES cannot be used in multiple .c files
    	at all.  (#165852, Dave Benson)

--- glib/gutils.h
+++ glib/gutils.h
@@ -94,6 +94,7 @@ G_BEGIN_DECLS
 #endif
 #ifdef G_IMPLEMENT_INLINES
 #  define G_INLINE_FUNC
+#  undef  G_CAN_INLINE
 #elif defined (__GNUC__) 
 #  define G_INLINE_FUNC extern inline
 #elif defined (G_CAN_INLINE) 
