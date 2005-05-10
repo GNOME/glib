bc21d3a9760bef9c877c319425875d9555502e72
Matthias Clasen <mclasen@redhat.com>
2005-05-10 03:52:16 +0000

    Free the option context.
    2005-05-09  Matthias Clasen  <mclasen@redhat.com>
    
    	* src/compiler.c (main): Free the option context.

--- girepository/src/compiler.c
+++ girepository/src/compiler.c
@@ -149,6 +149,7 @@ main (int argc, char ** argv)
   context = g_option_context_new ("");
   g_option_context_add_main_entries (context, options, NULL);
   g_option_context_parse (context, &argc, &argv, &error);
+  g_option_context_free (context);
 
   if (!input) 
     { 
