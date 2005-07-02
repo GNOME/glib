99563f439ca2640960762eb5ec14b3dc0041b3cd
Hans Breuer <hans@breuer.org>
2005-07-02 19:50:29 +0000

    include <process.h> for getpid() include <io.h> for close() etc. add
    2005-07-01  Hans Breuer  <hans@breuer.org>
    
    	* glib/gmessages.c : include <process.h> for getpid()
    	* glib/gmappedfile.c : include <io.h> for close() etc.
    	* glib/makefile.msc.in : add gmappedfile.obj

--- glib/gmessages.c
+++ glib/gmessages.c
@@ -48,6 +48,7 @@
 #include "galias.h"
 
 #ifdef G_OS_WIN32
+#include <process.h>		/* For getpid() */
 #include <io.h>
 #  define STRICT		/* Strict typing, please */
 #  define _WIN32_WINDOWS 0x0401 /* to get IsDebuggerPresent */
