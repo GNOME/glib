9c45222942942bd76cc6a5d1704349a7a0418493
Tor Lillqvist <tml@novell.com>
2005-08-04 17:07:21 +0000

    Move the G_BEGIN_DECLS/G_END_DECLS pair outside the #if/#else/#endif
    2005-08-04  Tor Lillqvist  <tml@novell.com>
    
    	* glib/gstdio.h: Move the G_BEGIN_DECLS/G_END_DECLS pair outside
    	the #if/#else/#endif block. Otherwise we had G_BEGIN_DECLS without
    	matching G_END_DECLS on Unix, and G_END_DECLS without matching
    	G_BEGIN_DECLS on Win32.

--- glib/gstdio.h
+++ glib/gstdio.h
@@ -25,9 +25,9 @@
 
 #include <sys/stat.h>
 
-#if defined(G_OS_UNIX) && !defined(G_STDIO_NO_WRAP_ON_UNIX)
-
 G_BEGIN_DECLS
+
+#if defined(G_OS_UNIX) && !defined(G_STDIO_NO_WRAP_ON_UNIX)
 
 /* Just pass on to the system functions, so there's no potential for data
  * format mismatches, especially with large file interfaces.
@@ -100,8 +100,8 @@ FILE *g_freopen (const gchar *filename,
                  const gchar *mode,
                  FILE        *stream);
 
-G_END_DECLS
-
 #endif /* G_OS_UNIX */
+
+G_END_DECLS
 
 #endif /* __G_STDIO_H__ */
