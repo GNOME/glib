6c538d33ee948300fc1e9105169547b92ecac4ae
Manish Singh <yosh@gimp.org>
2005-08-09 00:25:18 +0000

    remove stray -V option to grep so it actually works.
    2005-08-08  Manish Singh  <yosh@gimp.org>
    
            * glib/abicheck.sh: remove stray -V option to grep so it actually
            works.

--- glib/abicheck.sh
+++ glib/abicheck.sh
@@ -8,6 +8,6 @@ INCLUDES="$INCLUDES -include glibconfig.cpp"
 cpp -P -DINCLUDE_INTERNAL_SYMBOLS -DINCLUDE_VARIABLES -DG_STDIO_NO_WRAP_ON_UNIX -DALL_FILES $INCLUDES "${srcdir:-.}/glib.symbols" | sed -e '/^$/d' -e 's/ G_GNUC.*$//' -e 's/ PRIVATE$//' | sort > expected-abi
 rm glibconfig.cpp
 
-nm -D .libs/libglib-2.0.so | grep -v " [BDTR] " | cut -d ' ' -f 3 | sort > actual-abi
+nm -D .libs/libglib-2.0.so | grep " [BDTR] " | cut -d ' ' -f 3 | sort > actual-abi
 
 diff -u expected-abi actual-abi && rm expected-abi actual-abi
