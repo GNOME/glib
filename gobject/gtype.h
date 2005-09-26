31659726ea45c44a6e4589a31386d15b12020209
David Odin <dindinx@src.gnome.org>
2005-09-26 18:35:32 +0000

    initialize all the fields of GInterfaceInfo in the G_IMPLEMENT_INTERFACE
    * gobject/gtype.h: initialize all the fields of GInterfaceInfo in the
      G_IMPLEMENT_INTERFACE macro, to shut up a warning when compiling at -W

--- gobject/gtype.h
+++ gobject/gtype.h
@@ -340,7 +340,9 @@ gpointer g_type_instance_get_private    (GTypeInstance              *instance,
  */
 #define G_IMPLEMENT_INTERFACE(TYPE_IFACE, iface_init)       { \
   static const GInterfaceInfo g_implement_interface_info = { \
-    (GInterfaceInitFunc) iface_init \
+    (GInterfaceInitFunc) iface_init, \
+    (GInterfaceFinalizeFunc) NULL, \
+    NULL \
   }; \
   g_type_add_interface_static (g_define_type_id, TYPE_IFACE, &g_implement_interface_info); \
 }
