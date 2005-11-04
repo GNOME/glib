86e364ea3b56cdea91ca55bc53a702a1fa9377f0
Matthias Clasen <mclasen@redhat.com>
2005-11-04 20:31:11 +0000

    Expand the docs. (#320466, Steffen Gutmann)
    2005-11-04  Matthias Clasen  <mclasen@redhat.com>
    
            * glib/gmain.c (g_child_watch_source_new): Expand
            the docs.  (#320466, Steffen Gutmann)

--- glib/gmain.c
+++ glib/gmain.c
@@ -3665,11 +3665,15 @@ g_child_watch_source_init (void)
  * @pid: process id of a child process to watch. On Windows, a HANDLE
  * for the process to watch (which actually doesn't have to be a child).
  * 
- * Creates a new child_watch source.
+ * Creates a new child watch source.
  *
  * The source will not initially be associated with any #GMainContext
  * and must be added to one with g_source_attach() before it will be
  * executed.
+ * 
+ * Note that child watch sources can only be used in conjunction with 
+ * <literal>g_spawn...</literal> when the %G_SPAWN_DO_NOT_REAP_CHILD
+ * flag is used.
  * 
  * Note that on platforms where #GPid must be explicitely closed
  * (see g_spawn_close_pid()) @pid must not be closed while the
