d87c91a396179516c8ba900dc402dfcb7063af83
Matthias Clasen <mclasen@redhat.com>
2005-12-03 06:44:02 +0000

    Make mapping of empty files work. (#321530)
    2005-12-03  Matthias Clasen  <mclasen@redhat.com>
    
            * glib/gmappedfile.c: Make mapping of empty files
            work.  (#321530)

--- glib/gmappedfile.c
+++ glib/gmappedfile.c
@@ -137,6 +137,14 @@ g_mapped_file_new (const gchar  *filename,
       goto out;
     }
 
+  if (st.st_size == 0)
+    {
+      file->length = 0;
+      file->contents = "";
+      close (fd);
+      return file;
+    }
+
   file->contents = MAP_FAILED;
 
 #ifdef HAVE_MMAP
@@ -251,13 +259,16 @@ g_mapped_file_free (GMappedFile *file)
 {
   g_return_if_fail (file != NULL);
 
+  if (file->length)
+  {
 #ifdef HAVE_MMAP
-  munmap (file->contents, file->length);
+    munmap (file->contents, file->length);
 #endif
 #ifdef G_OS_WIN32
-  UnmapViewOfFile (file->contents);
-  CloseHandle (file->mapping);
+    UnmapViewOfFile (file->contents);
+    CloseHandle (file->mapping);
 #endif
+  }
 
   g_free (file);
 }
