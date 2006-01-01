44d277efc0f14ea03f21bbc011516f6c2b00d666
Tor Lillqvist <tml@novell.com>
2006-01-01 11:09:10 +0000

    Set the GIOChannels for stdout and stderr to unbuffered. Otherwise the
    2006-01-01  Tor Lillqvist  <tml@novell.com>
    
    	* glib/gspawn-win32.c (g_spawn_sync_utf8): Set the GIOChannels for
    	stdout and stderr to unbuffered. Otherwise the giochannel layer
    	will try to read from them regardless whether the
    	g_io_channel_win32_poll() call here has indicated
    	readability or not. (#325310)

--- glib/gspawn-win32.c
+++ glib/gspawn-win32.c
@@ -177,7 +177,8 @@ protect_argv (gchar  **argv,
    * reconstructed correctly in the C runtime startup code.  Note that
    * the unquoting algorithm in the C runtime is really weird, and
    * rather different than what Unix shells do. See stdargv.c in the C
-   * runtime sources (in the Platform SDK, in src/crt).
+   * runtime sources (in the Platform SDK, in src/crt), and
+   * http://msdn.microsoft.com/library/default.asp?url=/library/en-us/vccelng/htm/progs_12.asp
    *
    * Note that an new_argv[0] constructed by this function should
    * *not* be passed as the filename argument to a spawn* or exec*
@@ -1038,9 +1039,12 @@ g_spawn_sync_utf8 (const gchar          *working_directory,
       outstr = g_string_new (NULL);
       outchannel = g_io_channel_win32_new_fd (outpipe);
       g_io_channel_set_encoding (outchannel, NULL, NULL);
+      g_io_channel_set_buffered (outchannel, FALSE);
       g_io_channel_win32_make_pollfd (outchannel,
 				      G_IO_IN | G_IO_ERR | G_IO_HUP,
 				      &outfd);
+      if (debug)
+	g_print ("outfd=%x\n", outfd.fd);
     }
       
   if (errpipe >= 0)
@@ -1048,9 +1052,12 @@ g_spawn_sync_utf8 (const gchar          *working_directory,
       errstr = g_string_new (NULL);
       errchannel = g_io_channel_win32_new_fd (errpipe);
       g_io_channel_set_encoding (errchannel, NULL, NULL);
+      g_io_channel_set_buffered (errchannel, FALSE);
       g_io_channel_win32_make_pollfd (errchannel,
 				      G_IO_IN | G_IO_ERR | G_IO_HUP,
 				      &errfd);
+      if (debug)
+	g_print ("errfd=%x\n", errfd.fd);
     }
 
   /* Read data until we get EOF on all pipes. */
