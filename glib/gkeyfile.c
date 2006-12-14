c8c025146002332619e8c32dc65e7aff2205c063
Matthias Clasen <mclasen@redhat.com>
2006-12-14 23:14:07 +0000

    If the group is already there, make it current. (#385910, Joe Halliwell)
    2006-12-14  Matthias Clasen  <mclasen@redhat.com>
    
            * glib/gkeyfile.c (g_key_file_add_group): If the group
            is already there, make it current.  (#385910, Joe Halliwell)
    
            * tests/keyfile-test.c: Add a test for duplicate groups/keys.
    
    2

--- glib/gkeyfile.c
+++ glib/gkeyfile.c
@@ -2887,8 +2887,12 @@ g_key_file_add_group (GKeyFile    *key_file,
   g_return_if_fail (key_file != NULL);
   g_return_if_fail (group_name != NULL);
 
-  if (g_key_file_lookup_group_node (key_file, group_name) != NULL)
-    return;
+  group = g_key_file_lookup_group (key_file, group_name);
+  if (group != NULL)
+    {
+      key_file->current_group = group;
+      return;
+    }
 
   group = g_new0 (GKeyFileGroup, 1);
   group->name = g_strdup (group_name);
