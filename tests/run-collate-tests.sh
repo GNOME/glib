065cbcb55f78632e69b2273270622a0ec5b17b68
Matthias Clasen <mclasen@redhat.com>
2006-12-24 21:45:39 +0000

    Silently skip tests if we can't set LC_COLLATE to en_US. (#336438)
    2006-12-24  Matthias Clasen  <mclasen@redhat.com>
    
            * tests/run-collate-tests.sh:
            * tests/unicode-collate.c: Silently skip tests if
            we can't set LC_COLLATE to en_US.  (#336438)

--- tests/run-collate-tests.sh
+++ tests/run-collate-tests.sh
@@ -13,9 +13,6 @@ echo_v ()
   fi
 }
 
-LC_ALL=en_US
-export LC_ALL
-
 error_out=/dev/null
 if [ "$1" = "-v" ]; then
   verbose=1
@@ -25,6 +22,9 @@ for I in ${srcdir:-.}/collate/*.in; do
   echo_v "Sorting $I"
   name=`basename $I .in`
   ./unicode-collate $I > collate.out
+  if [ $? -eq 2 ]; then
+    exit 0
+  fi   
   diff collate.out ${srcdir:-.}/collate/$name.unicode || 
     fail "unexpected error when using g_utf8_collate() on $I"
   ./unicode-collate --key $I > collate.out
