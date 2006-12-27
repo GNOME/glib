d1be55cccbd6565fc814d0ca1b0ef47a64c78ae6
Matthias Clasen <mclasen@redhat.com>
2006-12-27 05:12:47 +0000

    Don't modify the current locale. (#389300)
    2006-12-27  Matthias Clasen  <mclasen@redhat.com>
    
            * glib/gunicollate.c (g_utf8_collate_key): Don't modify
            the current locale.  (#389300)

--- glib/gunicollate.c
+++ glib/gunicollate.c
@@ -213,8 +213,6 @@ g_utf8_collate_key (const gchar *str,
 
   str_norm = _g_utf8_normalize_wc (str, len, G_NORMALIZE_ALL_COMPOSE);
 
-  setlocale (LC_COLLATE, "");
-
   xfrm_len = wcsxfrm (NULL, (wchar_t *)str_norm, 0);
   result_wc = g_new (wchar_t, xfrm_len + 1);
   wcsxfrm (result_wc, (wchar_t *)str_norm, xfrm_len + 1);
