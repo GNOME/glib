74ab9212a011d30a60ec173bd68bd600daf1f639
Tor Lillqvist <tml@novell.com>
2007-01-15 13:41:51 +0000

    Fix mistake in my last commit.
    2007-01-15  Tor Lillqvist  <tml@novell.com>
    
    	* glib/gmain.h (struct _GPollFD): Fix mistake in my last commit.
    
    
    svn path=/trunk/; revision=5271

--- glib/gmain.h
+++ glib/gmain.h
@@ -120,7 +120,7 @@ typedef gint	(*GPollFunc)	(GPollFD *ufds,
 
 struct _GPollFD
 {
-#ifdef G_OS_WIN32 && GLIB_SIZEOF_VOID_P == 8
+#if defined (G_OS_WIN32) && GLIB_SIZEOF_VOID_P == 8
   gint64	fd;
 #else
   gint		fd;
