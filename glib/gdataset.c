f50d64c6ad05eb18bb318c578abc6f63b5d2ba7a
Behdad Esfahbod <behdad@gnome.org>
2007-06-17 07:43:34 +0000

    Accept NULL and return GQuark value of zero. (#446859)
    2007-06-17  Behdad Esfahbod  <behdad@gnome.org>
    
            * glib/gdataset.c (g_quark_from_string),
            (g_quark_from_static_string): Accept NULL and return GQuark
            value of zero. (#446859)
    
    
    svn path=/trunk/; revision=5572

--- glib/gdataset.c
+++ glib/gdataset.c
@@ -635,7 +635,8 @@ g_quark_from_string (const gchar *string)
 {
   GQuark quark;
   
-  g_return_val_if_fail (string != NULL, 0);
+  if (!string)
+    return 0;
   
   G_LOCK (g_quark_global);
   quark = g_quark_from_string_internal (string, TRUE);
@@ -649,7 +650,8 @@ g_quark_from_static_string (const gchar *string)
 {
   GQuark quark;
   
-  g_return_val_if_fail (string != NULL, 0);
+  if (!string)
+    return 0;
   
   G_LOCK (g_quark_global);
   quark = g_quark_from_string_internal (string, FALSE);
