4630859b8193756f540e2e5ce65e71c76bae2232
Matthias Clasen <matthiasc@src.gnome.org>
2007-11-24 16:53:04 +0000

    2.14.4
    svn path=/branches/glib-2-14/; revision=5929

--- glib/gconvert.c
+++ glib/gconvert.c
@@ -228,7 +228,7 @@ iconv_cache_init (void)
 }
 
 
-/**
+/*
  * iconv_cache_bucket_new:
  * @key: cache key
  * @cd: iconv descriptor
@@ -263,7 +263,7 @@ iconv_cache_bucket_new (gchar *key, GIConv cd)
 }
 
 
-/**
+/*
  * iconv_cache_bucket_expire:
  * @node: cache bucket's node
  * @bucket: cache bucket
@@ -308,7 +308,7 @@ iconv_cache_bucket_expire (GList *node, struct _iconv_cache_bucket *bucket)
 }
 
 
-/**
+/*
  * iconv_cache_expire_unused:
  *
  * Expires as many unused cache buckets as it needs to in order to get
