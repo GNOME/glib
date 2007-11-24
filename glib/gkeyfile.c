46e8950f77b6a3256480a91f069a4f13391d5c55
Matthias Clasen <mclasen@redhat.com>
2007-11-24 06:00:09 +0000

    Set length out param in list-returning functions to 0 when returning NULL.
    2007-11-22  Matthias Clasen  <mclasen@redhat.com>
    
            * glib/gkeyfile.c: Set length out param in list-returning functions
            to 0 when returning NULL.  (#498728, Christian Persch)
    
    
    
    svn path=/branches/glib-2-14/; revision=5922

--- glib/gkeyfile.c
+++ glib/gkeyfile.c
@@ -1461,6 +1461,9 @@ g_key_file_get_string_list (GKeyFile     *key_file,
   g_return_val_if_fail (group_name != NULL, NULL);
   g_return_val_if_fail (key != NULL, NULL);
 
+  if (length)
+    *length = 0;
+
   value = g_key_file_get_value (key_file, group_name, key, &key_file_error);
 
   if (key_file_error)
@@ -1743,7 +1746,11 @@ g_key_file_get_locale_string_list (GKeyFile     *key_file,
     g_propagate_error (error, key_file_error);
   
   if (!value)
-    return NULL;
+    {
+      if (length)
+        *length = 0;
+      return NULL;
+    }
 
   if (value[strlen (value) - 1] == ';')
     value[strlen (value) - 1] = '\0';
@@ -1938,6 +1945,9 @@ g_key_file_get_boolean_list (GKeyFile     *key_file,
   g_return_val_if_fail (key_file != NULL, NULL);
   g_return_val_if_fail (group_name != NULL, NULL);
   g_return_val_if_fail (key != NULL, NULL);
+
+  if (length)
+    *length = 0;
 
   key_file_error = NULL;
 
@@ -2151,6 +2161,9 @@ g_key_file_get_integer_list (GKeyFile     *key_file,
   g_return_val_if_fail (group_name != NULL, NULL);
   g_return_val_if_fail (key != NULL, NULL);
 
+  if (length)
+    *length = 0;
+
   values = g_key_file_get_string_list (key_file, group_name, key,
 				       &num_ints, &key_file_error);
 
@@ -2359,6 +2372,9 @@ g_key_file_get_double_list  (GKeyFile     *key_file,
   g_return_val_if_fail (key_file != NULL, NULL);
   g_return_val_if_fail (group_name != NULL, NULL);
   g_return_val_if_fail (key != NULL, NULL);
+
+  if (length)
+    *length = 0;
 
   values = g_key_file_get_string_list (key_file, group_name, key,
                                        &num_doubles, &key_file_error);
@@ -3295,7 +3311,7 @@ g_key_file_is_key_name (const gchar *name)
     return FALSE;
 
   /* We accept spaces in the middle of keys to not break
-   * existing apps, but we don't tolerate initial of final
+   * existing apps, but we don't tolerate initial or final
    * spaces, which would lead to silent corruption when
    * rereading the file.
    */
