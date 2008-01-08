daeb6b8cf9173b093884d18e7f7a32cfa17307f3
Matthias Clasen <mclasen@redhat.com>
2008-01-08 01:08:22 +0000

    Don't set open_max to RLIM_INFINITY. (#495589, Tommi Komulainen)
    2008-01-06  Matthias Clasen  <mclasen@redhat.com>
    
            * glib/gspawn.c (fdwalk): Don't set open_max to
            RLIM_INFINITY.  (#495589, Tommi Komulainen)
    
    
    
    svn path=/branches/glib-2-14/; revision=6270

--- glib/gspawn.c
+++ glib/gspawn.c
@@ -936,7 +936,7 @@ fdwalk (int (*cb)(void *data, int fd), void *data)
   
 #ifdef HAVE_SYS_RESOURCE_H
       
-  if (getrlimit(RLIMIT_NOFILE, &rl) == 0)
+  if (getrlimit(RLIMIT_NOFILE, &rl) == 0 && rl.rlim_max != RLIM_INFINITY)
       open_max = rl.rlim_max;
   else
 #endif
