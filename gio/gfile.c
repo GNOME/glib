79ad411eb56cf05dd31e2213fb231f970dc3f48c
Alexander Larsson <alexl@redhat.com>
2008-03-06 10:04:24 +0000

    Add g_return_val_if_fail check (#520700)
    2008-03-06  Alexander Larsson  <alexl@redhat.com>
    
    	* gfile.c (g_file_query_exists):
    	Add g_return_val_if_fail check (#520700)
    
    
    svn path=/trunk/; revision=6633

--- gio/gfile.c
+++ gio/gfile.c
@@ -928,7 +928,9 @@ g_file_query_exists (GFile *file,
 		     GCancellable *cancellable)
 {
   GFileInfo *info;
-
+  
+  g_return_val_if_fail (G_IS_FILE(file), FALSE);
+  
   info = g_file_query_info (file, G_FILE_ATTRIBUTE_STANDARD_TYPE,
 			    G_FILE_QUERY_INFO_NONE,
 			    cancellable, NULL);
