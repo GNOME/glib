c6e52984495d26e76832fcbb650a2c78d37d7238
Alexander Larsson <alexl@redhat.com>
2008-03-14 09:28:17 +0000

    Add required includes for f_fstypename member check.
    2008-03-14  Alexander Larsson  <alexl@redhat.com>
    
            * configure.in:
    	Add required includes for f_fstypename member check.
    
    2008-03-14  Alexander Larsson  <alexl@redhat.com>
    
            * glocalfile.c:
            (g_local_file_query_filesystem_info):
    	Use right define name for f_fstypename member check
    
    
    svn path=/branches/glib-2-16/; revision=6704

--- gio/glocalfile.c
+++ gio/glocalfile.c
@@ -1001,7 +1001,7 @@ g_local_file_query_filesystem_info (GFile         *file,
 #endif
     }
 #ifdef USE_STATFS
-#if defined(HAVE_STRUCT_STATFS_FS_TYPENAME)
+#if defined(HAVE_STRUCT_STATFS_F_FSTYPENAME)
   fstype = g_strdup(statfs_buffer.f_fstypename);
 #else
   fstype = get_fs_type (statfs_buffer.f_type);
