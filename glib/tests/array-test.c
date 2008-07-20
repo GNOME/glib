d0008f007073f9d75207d00c4c22812b04911f71
Matthias Clasen <matthiasc@src.gnome.org>
2008-07-20 02:09:05 +0000

    Move another test
    svn path=/trunk/; revision=7209

--- glib/tests/array-test.c
+++ glib/tests/array-test.c
@@ -40,52 +40,64 @@ sum_up (gpointer data,
   *sum += GPOINTER_TO_INT (data);
 }
 
-int
-main (int   argc,
-      char *argv[])
+static void
+array_append (void)
 {
-  gint i;
   GArray *garray;
-  GPtrArray *gparray;
-  GByteArray *gbarray;
-  gint sum = 0;
+  gint i;
 
-  /* array tests */
   garray = g_array_new (FALSE, FALSE, sizeof (gint));
   for (i = 0; i < 10000; i++)
     g_array_append_val (garray, i);
 
   for (i = 0; i < 10000; i++)
-    g_assert (g_array_index (garray, gint, i) == i);
+    g_assert_cmpint (g_array_index (garray, gint, i), ==, i);
 
   g_array_free (garray, TRUE);
+}
+
+static void
+array_prepend (void)
+{
+  GArray *garray;
+  gint i;
 
   garray = g_array_new (FALSE, FALSE, sizeof (gint));
   for (i = 0; i < 100; i++)
     g_array_prepend_val (garray, i);
 
   for (i = 0; i < 100; i++)
-    g_assert (g_array_index (garray, gint, i) == (100 - i - 1));
+    g_assert_cmpint (g_array_index (garray, gint, i), ==, (100 - i - 1));
 
   g_array_free (garray, TRUE);
+}
 
-  /* pointer arrays */
+static void
+pointer_array_add (void)
+{
+  GPtrArray *gparray;
+  gint i;
+  gint sum = 0;
+
   gparray = g_ptr_array_new ();
   for (i = 0; i < 10000; i++)
     g_ptr_array_add (gparray, GINT_TO_POINTER (i));
 
   for (i = 0; i < 10000; i++)
-    if (g_ptr_array_index (gparray, i) != GINT_TO_POINTER (i))
-      g_print ("array fails: %p ( %p )\n",
-	       g_ptr_array_index (gparray, i),
-	       GINT_TO_POINTER (i));
+    g_assert (g_ptr_array_index (gparray, i) == GINT_TO_POINTER (i));
   
   g_ptr_array_foreach (gparray, sum_up, &sum);
   g_assert (sum == 49995000);
 
   g_ptr_array_free (gparray, TRUE);
+}
 
-  /* byte arrays */
+static void
+byte_array_append (void)
+{
+  GByteArray *gbarray;
+  gint i;
+
   gbarray = g_byte_array_new ();
   for (i = 0; i < 10000; i++)
     g_byte_array_append (gbarray, (guint8*) "abcd", 4);
@@ -99,7 +111,23 @@ main (int   argc,
     }
 
   g_byte_array_free (gbarray, TRUE);
+}
 
-  return 0;
+int
+main (int argc, char *argv[])
+{
+  g_test_init (&argc, &argv, NULL);
+
+  /* array tests */
+  g_test_add_func ("/array/append", array_append);
+  g_test_add_func ("/array/prepend", array_prepend);
+
+  /* pointer arrays */
+  g_test_add_func ("/pointerarray/add", pointer_array_add);
+
+  /* byte arrays */
+  g_test_add_func ("/bytearray/append", byte_array_append);
+
+  return g_test_run ();
 }
 
