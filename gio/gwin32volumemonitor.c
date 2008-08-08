671ac311d54825f3eef99947b3dfc04a0fa19f57
Tor Lillqvist <tml@novell.com>
2008-08-08 05:36:56 +0000

    Put questionable code that didn't actually do anything inside #if 0, and
    2008-08-08  Tor Lillqvist  <tml@novell.com>
    
    	* gwin32volumemonitor.c (get_connected_drives): Put questionable
    	code that didn't actually do anything inside #if 0, and add a
    	comment wondering what the code was supposed to do. This also gets
    	rid of a leftover debugging printout.
    
    
    svn path=/trunk/; revision=7321

--- gio/gwin32volumemonitor.c
+++ gio/gwin32volumemonitor.c
@@ -175,16 +175,32 @@ get_connected_drives (GVolumeMonitor *volume_monitor)
   
   monitor = G_WIN32_VOLUME_MONITOR (volume_monitor);
 
+#if 0
   find_handle = FindFirstVolumeW (wc_name, MAX_PATH);
   found = (find_handle != INVALID_HANDLE_VALUE);
   while (found)
     {
+      /* I don't know what this code is supposed to do; clearly it now
+       * does nothing, the returned GList is always NULL. But what was
+       * this code supposed to be a start of? The volume names that
+       * the FindFirstVolume/FindNextVolume loop iterates over returns
+       * device names like
+       *
+       *   \Device\HarddiskVolume1
+       *   \Device\HarddiskVolume2
+       *   \Device\CdRom0
+       *
+       * No DOS devices there, so I don't see the point with the
+       * QueryDosDevice call below. Probably this code is confusing volumes
+       * with something else that does contain the mapping from DOS devices
+       * to volumes.
+       */
       wchar_t wc_dev_name[MAX_PATH+1];
-      guint trailing = wcslen(wc_name) - 1;
+      guint trailing = wcslen (wc_name) - 1;
 
       /* remove trailing backslash and leading \\?\\ */
       wc_name[trailing] = L'\0';
-      if (QueryDosDeviceW(&wc_name[4], wc_dev_name, MAX_PATH))
+      if (QueryDosDeviceW (&wc_name[4], wc_dev_name, MAX_PATH))
         {
           gchar *name = g_utf16_to_utf8 (wc_dev_name, -1, NULL, NULL, NULL);
           g_print ("%s\n", name);
@@ -195,6 +211,7 @@ get_connected_drives (GVolumeMonitor *volume_monitor)
     }
   if (find_handle != INVALID_HANDLE_VALUE)
     FindVolumeClose (find_handle);
+#endif
 
   return list;
 }
