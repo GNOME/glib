fa7cafae5cfea880a1d6cfc82b000eecd34534e1
Ryan Lortie <desrt@desrt.ca>
2010-01-31 00:05:48 -0500

    GChecksum: move docs from tmpl to .c

--- glib/gchecksum.c
+++ glib/gchecksum.c
@@ -29,6 +29,28 @@
 
 #include "galias.h"
 
+/**
+ * SECTION: checksum
+ * @title: Data Checksums
+ * @short_description: Computes the checksum for data
+ *
+ * GLib provides a generic API for computing checksums (or "digests")
+ * for a sequence of arbitrary bytes, using various hashing algorithms
+ * like MD5, SHA-1 and SHA-256. Checksums are commonly used in various
+ * environments and specifications.
+ *
+ * GLib supports incremental checksums using the GChecksum data
+ * structure, by calling g_checksum_update() as long as there's data
+ * available and then using g_checksum_get_string() or
+ * g_checksum_get_digest() to compute the checksum and return it either
+ * as a string in hexadecimal form, or as a raw sequence of bytes. To
+ * compute the checksum for binary blobs and NUL-terminated strings in
+ * one go, use the convenience functions g_compute_checksum_for_data()
+ * and g_compute_checksum_for_string(), respectively.
+ *
+ * Support for checksums has been added in GLib 2.16
+ **/
+
 #define IS_VALID_TYPE(type)     ((type) >= G_CHECKSUM_MD5 && (type) <= G_CHECKSUM_SHA256)
 
 /* The fact that these are lower case characters is part of the ABI */
