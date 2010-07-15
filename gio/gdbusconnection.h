ca14ab7ac0d93b2ccd0977d905b19094e7317490
Ryan Lortie <desrt@desrt.ca>
2010-07-15 16:47:32 -0400

    Bug 624483: GDBusSubtreeEnumerateFunc clarification
    Clarify a couple of things in the docs:
    
      1) you must return flat names (no slashes)
    
      2) g_strfreev() will be called on the result
    
      3) a benefit of using the DISPATCH_TO_UNENUMERATED flag

--- gio/gdbusconnection.h
+++ gio/gdbusconnection.h
@@ -306,6 +306,16 @@ gboolean         g_dbus_connection_unregister_object          (GDBusConnection  
  *
  * The type of the @enumerate function in #GDBusSubtreeVTable.
  *
+ * This function is called when generating introspection data and also
+ * when preparing to dispatch incoming messages in the event that the
+ * %G_DBUS_SUBTREE_FLAGS_DISPATCH_TO_UNENUMERATED_NODES flag is not
+ * specified (ie: to verify that the object path is valid).
+ *
+ * Hierarchies are not supported; the items that you return should not
+ * contain the '/' character.
+ *
+ * The return value will be freed with g_strfreev().
+ *
  * Returns: A newly allocated array of strings for node names that are children of @object_path.
  *
  * Since: 2.26
