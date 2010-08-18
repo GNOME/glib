6c340c0b0312fc12f01ba1887e559fd36fcf28cb
Matthias Clasen <mclasen@redhat.com>
2010-08-18 12:07:01 -0400

    Make gunixcredentialsmessage.h standalone includable

--- gio/gunixcredentialsmessage.h
+++ gio/gunixcredentialsmessage.h
@@ -24,8 +24,7 @@
 #ifndef __G_UNIX_CREDENTIALS_MESSAGE_H__
 #define __G_UNIX_CREDENTIALS_MESSAGE_H__
 
-#include <gio/giotypes.h>
-#include <gio/gsocketcontrolmessage.h>
+#include <gio/gio.h>
 
 G_BEGIN_DECLS
 
