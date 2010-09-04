c5049d86f94cc7f943ee63c93bb01100e759769b
Matthias Clasen <mclasen@redhat.com>
2010-09-03 20:34:15 -0400

    Remove redundant header inclusions

--- glib/gstring.c
+++ glib/gstring.c
@@ -21,10 +21,10 @@
  * Modified by the GLib Team and others 1997-2000.  See the AUTHORS
  * file for a list of people on the GLib Team.  See the ChangeLog
  * files for a list of changes.  These files are distributed with
- * GLib at ftp://ftp.gtk.org/pub/gtk/. 
+ * GLib at ftp://ftp.gtk.org/pub/gtk/.
  */
 
-/* 
+/*
  * MT safe
  */
 
@@ -39,7 +39,8 @@
 #include <string.h>
 #include <ctype.h>
 
-#include "glib.h"
+#include "gstring.h"
+
 #include "gprintf.h"
 
 
