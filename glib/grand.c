92c577eeff6c42247a4eb3a74eb3f4894a143f9d
Matthias Clasen <mclasen@redhat.com>
2010-09-03 20:44:59 -0400

    Remove eexcessive header inclusions

--- glib/grand.c
+++ glib/grand.c
@@ -29,10 +29,10 @@
  * Modified by the GLib Team and others 1997-2000.  See the AUTHORS
  * file for a list of people on the GLib Team.  See the ChangeLog
  * files for a list of changes.  These files are distributed with
- * GLib at ftp://ftp.gtk.org/pub/gtk/.  
+ * GLib at ftp://ftp.gtk.org/pub/gtk/.
  */
 
-/* 
+/*
  * MT safe
  */
 
@@ -47,7 +47,11 @@
 #include <unistd.h>
 #endif
 
-#include "glib.h"
+#include "grand.h"
+
+#include "gmem.h"
+#include "gtestutils.h"
+#include "gthread.h"
 #include "gthreadprivate.h"
 
 #ifdef G_OS_WIN32
