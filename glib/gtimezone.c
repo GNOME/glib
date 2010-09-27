0aba35904c600c0b8ed0dbe37cad63e038336a36
Ryan Lortie <desrt@desrt.ca>
2010-09-27 11:25:31 -0400

    Remove some useless GTimeZone info from docs

--- glib/gtimezone.c
+++ glib/gtimezone.c
@@ -44,22 +44,6 @@
  * #GTimeZone is a structure that represents a time zone, at no
  * particular point in time.  It is refcounted and immutable.
  *
- * A time zone contains a number of intervals.  Each interval has
- * an abbreviation to describe it, an offet to UTC and a flag indicating
- * if the daylight savings time is in effect during that interval.  A
- * time zone always has at least one interval -- interval 0.
- *
- * Every UTC time is contained within exactly one interval, but a given
- * local time may be contained within zero, one or two intervals (due to
- * incontinuities associated with daylight savings time).
- *
- * An interval may refer to a specific period of time (eg: the duration
- * of daylight savings time during 2010) or it may refer to many periods
- * of time that share the same properties (eg: all periods of daylight
- * savings time).  It is also possible (usually for political reasons)
- * that some properties (like the abbreviation) change between intervals
- * without other properties changing.
- *
  * #GTimeZone is available since GLib 2.26.
  */
 
