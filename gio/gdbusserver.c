c06be2051541644938616e0da6f1a02b71bc56d5
Michael Kuhn <suraia@ikkoku.de>
2010-12-22 10:11:11 +0100

    Bug 637561 - Crash when using G_DBUS_SERVER_FLAGS_RUN_IN_THREAD
    https://bugzilla.gnome.org/show_bug.cgi?id=637561
    
    Signed-off-by: David Zeuthen <davidz@redhat.com>

--- gio/gdbusserver.c
+++ gio/gdbusserver.c
@@ -990,11 +990,16 @@ on_run (GSocketService    *service,
 
   if (server->flags & G_DBUS_SERVER_FLAGS_RUN_IN_THREAD)
     {
+      gboolean claimed;
+
+      claimed = FALSE;
       g_signal_emit (server,
                      _signals[NEW_CONNECTION_SIGNAL],
                      0,
-                     connection);
-      g_dbus_connection_start_message_processing (connection);
+                     connection,
+                     &claimed);
+      if (claimed)
+        g_dbus_connection_start_message_processing (connection);
       g_object_unref (connection);
     }
   else
