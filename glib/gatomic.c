349212211247781e7bec43b95cb57dddc1d0e5b1
Behdad Esfahbod <behdad@behdad.org>
2011-08-09 19:43:34 +0200

    Bug 652827 - glib-2.29.8 no longer builds with mingw.org's toolchain
    Check for Win32 atomic intrinsics.

--- glib/gatomic.c
+++ glib/gatomic.c
@@ -455,7 +455,7 @@ gsize
   return g_atomic_pointer_xor ((volatile gpointer *) atomic, val);
 }
 
-#elif defined (G_PLATFORM_WIN32)
+#elif defined (G_PLATFORM_WIN32) && defined(HAVE_WIN32_BUILTINS_FOR_ATOMIC_OPERATIONS)
 
 #include <windows.h>
 #if !defined(_M_AMD64) && !defined (_M_IA64) && !defined(_M_X64)
