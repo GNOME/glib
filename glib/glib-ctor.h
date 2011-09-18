8f74c927f609139c51b0a0c067ad63c60ac9139b
Ryan Lortie <desrt@desrt.ca>
2011-09-18 02:16:07 -0400

    Add glib-ctor functionality
    A pair of macros to define a constructor function (on compilers that
    support such a concept) and to ensure that the function is run exactly
    once (on compilers that lack such support).
    
    Presently only GCC is implemented.

---
+++ glib/glib-ctor.h
@@ -0,0 +1,25 @@
+#ifndef __GLIB_CTOR_H__
+
+#ifdef __GNUC__
+#define GLIB_CTOR(func) \
+  __attribute__((constructor)) static void func (void)
+#define GLIB_ENSURE_CTOR(func) G_STMT_START { } G_STMT_END
+#else
+/* could be vastly improved... */
+#define GLIB_CTOR(func) \
+  static GMutex   g__##func##_mutex = G_MUTEX_INIT;   \
+  static gboolean g__##func##_initialised;            \
+  static void func (void)
+#define GLIB_ENSURE_CTOR(func) \
+  G_STMT_START {                                      \
+    g_mutex_lock (&g__##func##_mutex);                \
+    if (!g__##func##_initialised)                     \
+      {                                               \
+        g__##func##_initialised = TRUE;               \
+        func ();                                      \
+      }                                               \
+    g_mutex_unlock (&g__##func##_mutex);              \
+  } G_STMT_END
+#endif
+
+#endif /* __GLIB_CTOR_H__ */
