cf9623767a721607cb972d300440e91dc8a57861
Matthias Clasen <mclasen@redhat.com>
2011-09-24 00:39:59 -0400

    Add a testcase for bug 659866
    Mere inclusion of glib headers should not require you to define
    any XYZ_SOURCE macros.

---
+++ glib/tests/include.c
@@ -0,0 +1,17 @@
+/* Test case for bug 659866 */
+
+#define _POSIX_C_SOURCE 0
+#undef _GNU_SOURCE
+#undef _XOPEN_SOURCE
+#include <pthread.h>
+#include <glib.h>
+
+int
+main (int argc, char *argv[])
+{
+  GRWLock lock;
+
+  g_rw_lock_init (&lock);
+
+  return 0;
+}
