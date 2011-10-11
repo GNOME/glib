0da0411a4a2f9efde4f8fc3d6bf504ee488c2e4a
Matthias Clasen <mclasen@redhat.com>
2011-10-11 13:06:58 -0400

    Add better deprecation macros
    Unlike G_GNUC_... macros, the new G_DEPRECATED[_FOR] are
    meant as abstractions that work with different compilers.
    Using a new name also lets us restrict it to 'must be placed
    before the declaration', which works with more compilers.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=661438

--- glib/gmacros.h
+++ glib/gmacros.h
@@ -287,4 +287,21 @@
 #define G_UNLIKELY(expr) (expr)
 #endif
 
+#if    __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 1)
+#define G_DEPRECATED __attribute__((__deprecated__))
+#elif defined(_MSC_VER) && (_MSC_VER >= 1300)
+#define G_GNUC_DEPRECATED __declspec(deprecated)
+#else
+#define G_DEPRECATED
+#endif
+
+#if    __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 5)
+#define G_DEPRECATED_FOR(f) __attribute__((deprecated("Use '" #f "' instead")))
+#elif defined(_MSC_FULL_VER) && (_MSC_FULL_VER > 140050320)
+#define G_DEPRECATED_FOR(f) __declspec(deprecated("is deprecated. Use '" #f "' instead"))
+#else
+#define G_DEPRECATED_FOR(f) G_DEPRECATED
+#endif
+
+
 #endif /* __G_MACROS_H__ */
