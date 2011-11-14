104f7353a7c86a8a34b1b284d1b7d453eacb0c74
Ryan Lortie <desrt@desrt.ca>
2011-11-14 17:15:58 +0100

    move GSettingsSchemaKey to gsettingsschema.c
    These functions no longer have anything to do with GSettings itself, so
    they should not be in that file anymore.
    
    GSettings still wants direct access to the GSettingsSchemaKey structure,
    so put that one in gsettingsschema-internal.h.

--- gio/gsettingsschema-internal.h
+++ gio/gsettingsschema-internal.h
@@ -24,6 +24,25 @@
 
 typedef struct _GSettingsSchema                             GSettingsSchema;
 
+typedef struct
+{
+  GSettingsSchema *schema;
+  const gchar *name;
+
+  guint is_flags : 1;
+  guint is_enum  : 1;
+
+  const guint32 *strinfo;
+  gsize strinfo_length;
+
+  const gchar *unparsed;
+  gchar lc_char;
+
+  const GVariantType *type;
+  GVariant *minimum, *maximum;
+  GVariant *default_value;
+} GSettingsSchemaKey;
+
 G_GNUC_INTERNAL
 GSettingsSchema *       g_settings_schema_new                           (const gchar      *name);
 G_GNUC_INTERNAL
@@ -48,5 +67,36 @@ const GQuark *          g_settings_schema_list                          (GSettin
 G_GNUC_INTERNAL
 const gchar *           g_settings_schema_get_string                    (GSettingsSchema  *schema,
                                                                          const gchar      *key);
+
+G_GNUC_INTERNAL
+void                    g_settings_schema_key_init                      (GSettingsSchemaKey *key,
+                                                                         GSettingsSchema    *schema,
+                                                                         const gchar        *name);
+G_GNUC_INTERNAL
+void                    g_settings_schema_key_clear                     (GSettingsSchemaKey *key);
+G_GNUC_INTERNAL
+gboolean                g_settings_schema_key_type_check                (GSettingsSchemaKey *key,
+                                                                         GVariant           *value);
+G_GNUC_INTERNAL
+gboolean                g_settings_schema_key_range_check               (GSettingsSchemaKey *key,
+                                                                         GVariant           *value);
+G_GNUC_INTERNAL
+GVariant *              g_settings_schema_key_range_fixup               (GSettingsSchemaKey *key,
+                                                                         GVariant           *value);
+G_GNUC_INTERNAL
+GVariant *              g_settings_schema_key_get_translated_default    (GSettingsSchemaKey *key);
+
+G_GNUC_INTERNAL
+gint                    g_settings_schema_key_to_enum                   (GSettingsSchemaKey *key,
+                                                                         GVariant           *value);
+G_GNUC_INTERNAL
+GVariant *              g_settings_schema_key_from_enum                 (GSettingsSchemaKey *key,
+                                                                         gint                value);
+G_GNUC_INTERNAL
+guint                   g_settings_schema_key_to_flags                  (GSettingsSchemaKey *key,
+                                                                         GVariant           *value);
+G_GNUC_INTERNAL
+GVariant *              g_settings_schema_key_from_flags                (GSettingsSchemaKey *key,
+                                                                         guint               value);
 
 #endif /* __G_SETTINGS_SCHEMA_INTERNAL_H__ */
