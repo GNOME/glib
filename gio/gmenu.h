9bebf15077e462f1c3e657df5d377b5ec6e806c3
Matthias Clasen <mclasen@redhat.com>
2011-11-27 01:16:39 -0500

    Reduce excessive width of gmenu.h

--- gio/gmenu.h
+++ gio/gmenu.h
@@ -26,105 +26,105 @@
 
 G_BEGIN_DECLS
 
-#define G_TYPE_MENU                                         (g_menu_get_type ())
-#define G_MENU(inst)                                        (G_TYPE_CHECK_INSTANCE_CAST ((inst),                     \
-                                                             G_TYPE_MENU, GMenu))
-#define G_IS_MENU(inst)                                     (G_TYPE_CHECK_INSTANCE_TYPE ((inst),                     \
-                                                             G_TYPE_MENU))
+#define G_TYPE_MENU          (g_menu_get_type ())
+#define G_MENU(inst)         (G_TYPE_CHECK_INSTANCE_CAST ((inst), \
+                              G_TYPE_MENU, GMenu))
+#define G_IS_MENU(inst)      (G_TYPE_CHECK_INSTANCE_TYPE ((inst), \
+                              G_TYPE_MENU))
 
-#define G_TYPE_MENU_ITEM                                    (g_menu_item_get_type ())
-#define G_MENU_ITEM(inst)                                   (G_TYPE_CHECK_INSTANCE_CAST ((inst),                     \
-                                                             G_TYPE_MENU_ITEM, GMenuItem))
-#define G_IS_MENU_ITEM(inst)                                (G_TYPE_CHECK_INSTANCE_TYPE ((inst),                     \
-                                                             G_TYPE_MENU_ITEM))
+#define G_TYPE_MENU_ITEM     (g_menu_item_get_type ())
+#define G_MENU_ITEM(inst)    (G_TYPE_CHECK_INSTANCE_CAST ((inst), \
+                              G_TYPE_MENU_ITEM, GMenuItem))
+#define G_IS_MENU_ITEM(inst) (G_TYPE_CHECK_INSTANCE_TYPE ((inst), \
+                              G_TYPE_MENU_ITEM))
 
-typedef struct _GMenuItem                                   GMenuItem;
-typedef struct _GMenu                                       GMenu;
+typedef struct _GMenuItem GMenuItem;
+typedef struct _GMenu     GMenu;
 
-GType                   g_menu_get_type                                 (void) G_GNUC_CONST;
-GMenu *                 g_menu_new                                      (void);
+GType       g_menu_get_type                         (void) G_GNUC_CONST;
+GMenu *     g_menu_new                              (void);
 
-void                    g_menu_freeze                                   (GMenu              *menu);
+void        g_menu_freeze                           (GMenu       *menu);
 
-void                    g_menu_insert_item                              (GMenu              *menu,
-                                                                         gint                position,
-                                                                         GMenuItem          *item);
-void                    g_menu_prepend_item                             (GMenu              *menu,
-                                                                         GMenuItem          *item);
-void                    g_menu_append_item                              (GMenu              *menu,
-                                                                         GMenuItem          *item);
-void                    g_menu_remove                                   (GMenu              *menu,
-                                                                         gint                position);
+void        g_menu_insert_item                      (GMenu       *menu,
+                                                     gint         position,
+                                                     GMenuItem   *item);
+void        g_menu_prepend_item                     (GMenu       *menu,
+                                                     GMenuItem   *item);
+void        g_menu_append_item                      (GMenu       *menu,
+                                                     GMenuItem   *item);
+void        g_menu_remove                           (GMenu       *menu,
+                                                     gint         position);
 
-void                    g_menu_insert                                   (GMenu              *menu,
-                                                                         gint                position,
-                                                                         const gchar        *label,
-                                                                         const gchar        *detailed_action);
-void                    g_menu_prepend                                  (GMenu              *menu,
-                                                                         const gchar        *label,
-                                                                         const gchar        *detailed_action);
-void                    g_menu_append                                   (GMenu              *menu,
-                                                                         const gchar        *label,
-                                                                         const gchar        *detailed_action);
+void        g_menu_insert                           (GMenu       *menu,
+                                                     gint         position,
+                                                     const gchar *label,
+                                                     const gchar *detailed_action);
+void        g_menu_prepend                          (GMenu       *menu,
+                                                     const gchar *label,
+                                                     const gchar *detailed_action);
+void        g_menu_append                           (GMenu       *menu,
+                                                     const gchar *label,
+                                                     const gchar *detailed_action);
 
-void                    g_menu_insert_section                           (GMenu              *menu,
-                                                                         gint                position,
-                                                                         const gchar        *label,
-                                                                         GMenuModel         *section);
-void                    g_menu_prepend_section                          (GMenu              *menu,
-                                                                         const gchar        *label,
-                                                                         GMenuModel         *section);
-void                    g_menu_append_section                           (GMenu              *menu,
-                                                                         const gchar        *label,
-                                                                         GMenuModel         *section);
+void        g_menu_insert_section                   (GMenu       *menu,
+                                                     gint         position,
+                                                     const gchar *label,
+                                                     GMenuModel  *section);
+void        g_menu_prepend_section                  (GMenu       *menu,
+                                                     const gchar *label,
+                                                     GMenuModel  *section);
+void        g_menu_append_section                   (GMenu       *menu,
+                                                     const gchar *label,
+                                                     GMenuModel  *section);
 
-void                    g_menu_insert_submenu                           (GMenu              *menu,
-                                                                         gint                position,
-                                                                         const gchar        *label,
-                                                                         GMenuModel         *submenu);
-void                    g_menu_prepend_submenu                          (GMenu              *menu,
-                                                                         const gchar        *label,
-                                                                         GMenuModel         *submenu);
-void                    g_menu_append_submenu                           (GMenu              *menu,
-                                                                         const gchar        *label,
-                                                                         GMenuModel         *submenu);
+void        g_menu_insert_submenu                   (GMenu       *menu,
+                                                     gint        position,
+                                                     const gchar *label,
+                                                     GMenuModel  *submenu);
+void        g_menu_prepend_submenu                  (GMenu       *menu,
+                                                     const gchar *label,
+                                                     GMenuModel  *submenu);
+void        g_menu_append_submenu                   (GMenu       *menu,
+                                                     const gchar *label,
+                                                     GMenuModel  *submenu);
 
 
-GType                   g_menu_item_get_type                            (void) G_GNUC_CONST;
-GMenuItem *             g_menu_item_new                                 (const gchar        *label,
-                                                                         const gchar        *detailed_action);
+GType       g_menu_item_get_type                    (void) G_GNUC_CONST;
+GMenuItem * g_menu_item_new                         (const gchar *label,
+                                                     const gchar *detailed_action);
 
-GMenuItem *             g_menu_item_new_submenu                         (const gchar        *label,
-                                                                         GMenuModel         *submenu);
+GMenuItem * g_menu_item_new_submenu                 (const gchar *label,
+                                                     GMenuModel  *submenu);
 
-GMenuItem *             g_menu_item_new_section                         (const gchar        *label,
-                                                                         GMenuModel         *section);
+GMenuItem * g_menu_item_new_section                 (const gchar *label,
+                                                     GMenuModel  *section);
 
-void                    g_menu_item_set_attribute_value                 (GMenuItem          *menu_item,
-                                                                         const char         *attribute,
-                                                                         GVariant           *value);
-void                    g_menu_item_set_attribute                       (GMenuItem          *menu_item,
-                                                                         const char         *attribute,
-                                                                         const gchar        *format_string,
-                                                                         ...);
-void                    g_menu_item_set_link                            (GMenuItem          *menu_item,
-                                                                         const char         *link,
-                                                                         GMenuModel         *model);
-void                    g_menu_item_set_label                           (GMenuItem          *menu_item,
-                                                                         const gchar        *label);
-void                    g_menu_item_set_submenu                         (GMenuItem          *menu_item,
-                                                                         GMenuModel         *submenu);
-void                    g_menu_item_set_section                         (GMenuItem          *menu_item,
-                                                                         GMenuModel         *section);
-void                    g_menu_item_set_action_and_target_value         (GMenuItem          *menu_item,
-                                                                         const gchar        *action,
-                                                                         GVariant           *target_value);
-void                    g_menu_item_set_action_and_target               (GMenuItem          *menu_item,
-                                                                         const gchar        *action,
-                                                                         const gchar        *format_string,
-                                                                         ...);
-void                    g_menu_item_set_detailed_action                 (GMenuItem          *menu_item,
-                                                                         const gchar        *detailed_action);
+void        g_menu_item_set_attribute_value         (GMenuItem   *menu_item,
+                                                     const gchar *attribute,
+                                                     GVariant    *value);
+void        g_menu_item_set_attribute               (GMenuItem   *menu_item,
+                                                     const gchar *attribute,
+                                                     const gchar *format_string,
+                                                     ...);
+void        g_menu_item_set_link                    (GMenuItem   *menu_item,
+                                                     const gchar *link,
+                                                     GMenuModel  *model);
+void        g_menu_item_set_label                   (GMenuItem   *menu_item,
+                                                     const gchar *label);
+void        g_menu_item_set_submenu                 (GMenuItem   *menu_item,
+                                                     GMenuModel  *submenu);
+void        g_menu_item_set_section                 (GMenuItem   *menu_item,
+                                                     GMenuModel  *section);
+void        g_menu_item_set_action_and_target_value (GMenuItem   *menu_item,
+                                                     const gchar *action,
+                                                     GVariant    *target_value);
+void        g_menu_item_set_action_and_target       (GMenuItem   *menu_item,
+                                                     const gchar *action,
+                                                     const gchar *format_string,
+                                                     ...);
+void        g_menu_item_set_detailed_action         (GMenuItem   *menu_item,
+                                                     const gchar *detailed_action);
 G_END_DECLS
 
 #endif /* __G_MENU_H__ */
