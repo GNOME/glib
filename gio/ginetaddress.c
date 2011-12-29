c71fc7477ed3ebe513432cba21d69ca99d7eb81c
Dan Winship <danw@gnome.org>
2011-12-29 09:18:53 -0500

    ginetaddress.c: fix an incorrect enum name in the docs

--- gio/ginetaddress.c
+++ gio/ginetaddress.c
@@ -443,8 +443,8 @@ g_inet_address_new_from_string (const gchar *string)
  * @family: the address family of @bytes
  *
  * Creates a new #GInetAddress from the given @family and @bytes.
- * @bytes should be 4 bytes for %G_INET_ADDRESS_IPV4 and 16 bytes for
- * %G_INET_ADDRESS_IPV6.
+ * @bytes should be 4 bytes for %G_SOCKET_FAMILY_IPV4 and 16 bytes for
+ * %G_SOCKET_FAMILY_IPV6.
  *
  * Returns: a new #GInetAddress corresponding to @family and @bytes.
  *
