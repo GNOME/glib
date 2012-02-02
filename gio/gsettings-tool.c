GNOME/glib

commit 8852d4e9a08e7fbc9df7fb99a54b112f5049ee19

Fix a refcounting error
'new' is created floating, therefore it is consumed by
g_settings_set, and unreffing it after that call is not right.

gio/gsettings-tool.c
@@ -457,6 +457,7 @@ gsettings_set (GSettings   *settings,
 
   existing = g_settings_get_value (settings, key);
   type = g_variant_get_type (existing);
+  g_variant_unref (existing);
 
   new = g_variant_parse (type, value, NULL, NULL, &error);
 
@@ -503,8 +504,6 @@ gsettings_set (GSettings   *settings,
     }
 
   g_settings_set_value (settings, key, new);
-  g_variant_unref (existing);
-  g_variant_unref (new);
 
   g_settings_sync ();
 

commit 387ed239e236cad1c3a54d08a34194c36bb59dba

gsettings tool: fix a memory error
8852d4e9a08e7fbc9df7fb99a54b112f5049ee19 introduced a memory error by
taking the type of a GVariant, freeing the GVariant and using the type
after the free.

This delays the free until after we've used the type.

https://bugzilla.gnome.org/show_bug.cgi?id=669253

gio/gsettings-tool.c
@@ -457,7 +457,6 @@ gsettings_set (GSettings   *settings,
 
   existing = g_settings_get_value (settings, key);
   type = g_variant_get_type (existing);
-  g_variant_unref (existing);
 
   new = g_variant_parse (type, value, NULL, NULL, &error);
 
@@ -489,6 +488,9 @@ gsettings_set (GSettings   *settings,
       g_clear_error (&error);
       new = g_variant_new_string (value);
     }
+
+  /* we're done with 'type' now, so we can free 'existing' */
+  g_variant_unref (existing);
 
   if (new == NULL)
     {