1820dcacf13650ca8fa9b66ca8bcf9351ff9e84f
Colin Walters <walters@verbum.org>
2012-02-03 13:42:56 -0500

    Add Emacs mode lines to C sources

--- girepository/tools/compiler.c
+++ girepository/tools/compiler.c
@@ -1,4 +1,5 @@
-/* GObject introspection: Typelib compiler
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*-
+ * GObject introspection: Typelib compiler
  *
  * Copyright (C) 2005 Matthias Clasen
  *
