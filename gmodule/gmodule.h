069f4299900e3cc0ffa73539e7d41a781f98ce9b
Matthias Clasen <mclasen@redhat.com>
2012-07-13 17:37:37 -0400

    Fix GModule documentation a bit
    The documentation for g_module_make_resident was for some reason
    in the doc comment for g_module_name.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=679813

--- gmodule/gmodule.h
+++ gmodule/gmodule.h
@@ -52,9 +52,11 @@ typedef	struct _GModule			 GModule;
 typedef const gchar* (*GModuleCheckInit) (GModule	*module);
 typedef void	     (*GModuleUnload)	 (GModule	*module);
 
+#ifndef __GTK_DOC_IGNORE__
 #ifdef G_OS_WIN32
 #define g_module_open g_module_open_utf8
 #define g_module_name g_module_name_utf8
+#endif
 #endif
 
 /* return TRUE if dynamic module loading is supported */
