2360d04e50d744a6db42a0a24418ecd272b45a8e
Owen W. Taylor <otaylor@fishsoup.net>
2012-08-16 22:06:45 -0400

    Fix problems with CLEANFILES and automake-1.11.1
    Running with automake-1.11.1, a couple fixes are needed
    for CLEANFILES when gtk-doc is not installed.
    
    (Found with Amazon Linux AMI release 2012.03)
    
    https://bugzilla.gnome.org/show_bug.cgi?id=682067

--- autogen.sh
+++ autogen.sh
@@ -11,7 +11,10 @@ GTKDOCIZE=$(which gtkdocize 2>/dev/null)
 if test -z $GTKDOCIZE; then
         echo "You don't have gtk-doc installed, and thus won't be able to generate the documentation."
         rm -f gtk-doc.make
-        echo 'EXTRA_DIST =' > gtk-doc.make
+        cat > gtk-doc.make <<EOF
+EXTRA_DIST =
+CLEANFILES =
+EOF
 else
         gtkdocize || exit $?
 fi
