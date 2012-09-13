GNOME/glib

commit 6e4acf44b3a943906432a2bf55223ac107d8e0c2

gshell: Fix parsing of comments in command lines.
Fixes bug 562907

glib/gshell.c
@@ -520,6 +520,24 @@ tokenize_command_line (const gchar *command_line,
               /* FALL THRU */
               
             case '#':
+              if (p == command_line)
+	        { /* '#' was the first char */
+                  current_quote = *p;
+                  break;
+                }
+              switch(*(p-1))
+                {
+                  case ' ':
+                  case '\n':
+                  case '\0':
+                    current_quote = *p;
+                    break;
+                  default:
+                    ensure_token (&current_token);
+                    g_string_append_c (current_token, *p);
+		    break;
+                }
+              break;
             case '\\':
               current_quote = *p;
               break;

glib/tests/shell.c
@@ -59,6 +59,8 @@ static CmdlineTest cmdline_tests[] =
   { "foo \"yada yada \\$\\\"\"", 2, { "foo", "yada yada $\"", NULL }, -1 },
   { "foo \"c:\\\\\"", 2, { "foo", "c:\\", NULL }, -1 },
   { "foo # bla bla bla\n bar", 2, { "foo", "bar", NULL }, -1 },
+  { "foo a#b", 2, { "foo", "a#b", NULL }, -1 },
+  { "#foo", 0, { NULL }, -1 },
   { "foo bar \\", 0, { NULL }, G_SHELL_ERROR_BAD_QUOTING },
   { "foo 'bar baz", 0, { NULL }, G_SHELL_ERROR_BAD_QUOTING },
   { "foo '\"bar\" baz", 0, { NULL }, G_SHELL_ERROR_BAD_QUOTING },

commit c99acf51d0405500592c36b739faa6bfcdb8181a

Fix regression in g_shell_parse_argv()
The commit in 6e4acf44b3a943906432a2bf55223ac107d8e0c2 broke
the fallthrough case for '\\' when it changed the '#' case.

This caused issues like this:
  https://bugzilla.gnome.org/show_bug.cgi?id=683821

https://bugzilla.gnome.org/show_bug.cgi?id=562907

glib/gshell.c
@@ -518,7 +518,10 @@ tokenize_command_line (const gchar *command_line,
               g_string_append_c (current_token, *p);
 
               /* FALL THRU */
-              
+            case '\\':
+              current_quote = *p;
+              break;
+
             case '#':
               if (p == command_line)
 	        { /* '#' was the first char */
@@ -537,9 +540,6 @@ tokenize_command_line (const gchar *command_line,
                     g_string_append_c (current_token, *p);
 		    break;
                 }
-              break;
-            case '\\':
-              current_quote = *p;
               break;
 
             default:

glib/tests/shell.c
@@ -66,7 +66,8 @@ static CmdlineTest cmdline_tests[] =
   { "foo '\"bar\" baz", 0, { NULL }, G_SHELL_ERROR_BAD_QUOTING },
   { "", 0, { NULL }, G_SHELL_ERROR_EMPTY_STRING },
   { "  ", 0, { NULL }, G_SHELL_ERROR_EMPTY_STRING },
-  { "# foo bar", 0, { NULL }, G_SHELL_ERROR_EMPTY_STRING }
+  { "# foo bar", 0, { NULL }, G_SHELL_ERROR_EMPTY_STRING },
+  {"foo '/bar/summer'\\''09 tours.pdf'", 2, {"foo", "/bar/summer'09 tours.pdf", NULL}, -1}
 };
 
 static gboolean