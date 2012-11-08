8dd70a261df853d86f4944481dc6a18d4c8a62af
John Ralls <jralls@ceridwen.us>
2012-11-08 08:35:23 -0800

    Bug 686185: GDateTime Transcoding fails on OSX
    Better fix than d5df0a10f. Thanks to Christian Persch for pointing this
    out.

--- glib/tests/gdatetime.c
+++ glib/tests/gdatetime.c
@@ -885,7 +885,7 @@ test_non_utf8_printf (void)
       g_free (oldlocale);
       return;
     }
-  if (g_strcmp0 (g_get_codeset (), "UTF-8") == 0)
+  if (g_get_charset (NULL))
     {
       g_test_message ("locale ja_JP.eucjp may be available, but glib seems to think that it's equivalent to UTF-8, skipping non-UTF-8 tests.");
       g_test_message ("This is a known issue on Darwin");
