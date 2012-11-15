49db979922283364238a5c208671591f713ecd78
Michael Natterer <mitch@gimp.org>
2012-11-15 15:23:07 +0100

    Revert "gmain: Add private API to create Unix child watch that uses waitid()"
    This reverts commit 93bf37ce1507380f74d4cb4cab6640fc7d2eb7d1.

--- glib/glib-private.c
+++ glib/glib-private.c
@@ -41,8 +41,7 @@ glib__private__ (void)
     g_get_worker_context,
 
     g_check_setuid,
-    g_main_context_new_with_next_id,
-    g_child_watch_source_new_nowait
+    g_main_context_new_with_next_id
   };
 
   return &table;
