2fc0687bcb067411a740bdfe51a49728b25a14e3
Dan Winship <danw@gnome.org>
2012-11-24 15:47:45 -0500

    g_test_expect_message: fix two bugs in example code

--- glib/gmessages.c
+++ glib/gmessages.c
@@ -1082,8 +1082,8 @@ g_assert_warning (const char *log_domain,
  *    &ast; context is already owned by another thread.
  *    &ast;/
  *   g_test_expect_message (G_LOG_DOMAIN,
- *                          G_LOG_LEVEL_CRITICIAL,
- *                          "assertion.*acquired_context.*failed");
+ *                          G_LOG_LEVEL_CRITICAL,
+ *                          "assertion*acquired_context*failed");
  *   g_main_context_push_thread_default (bad_context);
  *   g_test_assert_expected_messages ();
  * ]|
