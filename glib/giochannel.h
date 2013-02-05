7788bb3a915cc0818186bb2c04a51e195579d13e
Matthias Clasen <mclasen@redhat.com>
2013-02-04 22:46:24 -0500

    Make G_IO_FLAG_IS_WRITEABLE and enum value
    Having it as a define was causing some problems for bindings.
    https://bugzilla.gnome.org/show_bug.cgi?id=657045

--- glib/giochannel.h
+++ glib/giochannel.h
@@ -89,14 +89,12 @@ typedef enum
   G_IO_FLAG_NONBLOCK = 1 << 1,
   G_IO_FLAG_IS_READABLE = 1 << 2,	/* Read only flag */
   G_IO_FLAG_IS_WRITABLE = 1 << 3,	/* Read only flag */
+  G_IO_FLAG_IS_WRITEABLE = 1 << 3,      /* Misspelling in 2.29.10 and earlier */
   G_IO_FLAG_IS_SEEKABLE = 1 << 4,	/* Read only flag */
   G_IO_FLAG_MASK = (1 << 5) - 1,
   G_IO_FLAG_GET_MASK = G_IO_FLAG_MASK,
   G_IO_FLAG_SET_MASK = G_IO_FLAG_APPEND | G_IO_FLAG_NONBLOCK
 } GIOFlags;
-
-/* Misspelling in enum in 2.29.10 and earlier */
-#define G_IO_FLAG_IS_WRITEABLE (G_IO_FLAG_IS_WRITABLE)
 
 struct _GIOChannel
 {
