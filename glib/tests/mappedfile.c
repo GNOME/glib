03086e0b0f96f607f72eb295a4541e1064f72458
Colin Walters <walters@verbum.org>
2013-05-02 11:39:11 -0400

    tests/mappedfile: Also handle ENOMEM
    The RHEL6.4 kernel gives me that instead of EINVAL.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=699485

--- glib/tests/mappedfile.c
+++ glib/tests/mappedfile.c
@@ -50,7 +50,8 @@ test_device (void)
   GMappedFile *file;
 
   file = g_mapped_file_new ("/dev/null", FALSE, &error);
-  g_assert_error (error, G_FILE_ERROR, G_FILE_ERROR_INVAL);
+  g_assert (g_error_matches (error, G_FILE_ERROR, G_FILE_ERROR_INVAL) ||
+            g_error_matches (error, G_FILE_ERROR, G_FILE_ERROR_NOMEM));
   g_assert (file == NULL);
   g_error_free (error);
 }
