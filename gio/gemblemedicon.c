91179be5a82a55c32ddc61821430aef0b0f3fa48
Matthias Clasen <mclasen@redhat.com>
2013-05-02 21:00:57 -0400

    Silently handle icon being NULL
    While an emblemed icon without a base icon is not very
    useful, no need to crash here.
    This was crashing the object finalization test in gtk.

--- gio/gemblemedicon.c
+++ gio/gemblemedicon.c
@@ -72,7 +72,7 @@ g_emblemed_icon_finalize (GObject *object)
 
   emblemed = G_EMBLEMED_ICON (object);
 
-  g_object_unref (emblemed->priv->icon);
+  g_clear_object (&emblemed->priv->icon);
   g_list_free_full (emblemed->priv->emblems, g_object_unref);
 
   (*G_OBJECT_CLASS (g_emblemed_icon_parent_class)->finalize) (object);
