GNOME/glib

commit 3b01cbe8a8a9ee2d1169f072624069877b91097c

Document restrictions on private names
With the new machinery, private structs must be named
TypeNamePrivate if TypeName is the name of the instance
struct.

gobject/gtype.h
@@ -1353,6 +1353,9 @@ guint     g_type_get_type_registration_serial (void);
  * instance data to the type. Furthermore, it defines a *_get_type() function. See G_DEFINE_TYPE_EXTENDED()
  * for an example.
  * 
+ * Note that private structs added with this macros must have a struct
+ * name of the form <repaceable>@TN</replaceable>Private.
+ *
  * Since: 2.38
  */
 #define G_DEFINE_TYPE_WITH_PRIVATE(TN, t_n, T_P)            G_DEFINE_TYPE_EXTENDED (TN, t_n, T_P, 0, G_ADD_PRIVATE (TN))
@@ -1574,6 +1577,9 @@ guint     g_type_get_type_registration_serial (void);
  *
  * Note that this macro can only be used together with the G_DEFINE_TYPE_*
  * macros, since it depends on variable names from those macros.
+ *
+ * Also note that private structs added with these macros must have a struct
+ * name of the form <repaceable>TypeName</replaceable>Private.
  *
  * Since: 2.38
  */

commit 023e3b31a5352460f415944e166d413c2c747886

Fix a typo
Introduced by 3b01cbe8a8a9ee2d1169f072624069877b91097c

gobject/gtype.h
@@ -1354,7 +1354,7 @@ guint     g_type_get_type_registration_serial (void);
  * for an example.
  * 
  * Note that private structs added with this macros must have a struct
- * name of the form <repaceable>@TN</replaceable>Private.
+ * name of the form <replaceable>@TN</replaceable>Private.
  *
  * Since: 2.38
  */
@@ -1579,7 +1579,7 @@ guint     g_type_get_type_registration_serial (void);
  * macros, since it depends on variable names from those macros.
  *
  * Also note that private structs added with these macros must have a struct
- * name of the form <repaceable>TypeName</replaceable>Private.
+ * name of the form <replaceable>TypeName</replaceable>Private.
  *
  * Since: 2.38
  */