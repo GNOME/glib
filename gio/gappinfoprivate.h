e55d33edc1336ddc6d5cdfa0e3003a69a5812d26
Ryan Lortie <desrt@desrt.ca>
2013-09-25 09:19:43 -0400

    Add GAppInfoMonitor
    This is a simple object that emits a "change" signal when the installed
    applications may have changed in some way.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=711556

---
+++ gio/gappinfoprivate.h
@@ -0,0 +1,28 @@
+/* GIO - GLib Input, Output and Streaming Library
+ *
+ * Copyright © 2013 Canonical Limited
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Author: Ryan Lortie <desrt@desrt.ca>
+ */
+
+#ifndef __G_APP_INFO_PRIVATE_H__
+#define __G_APP_INFO_PRIVATE_H__
+
+void g_app_info_monitor_fire (void);
+
+#endif /* __G_APP_INFO_PRIVATE_H__ */
