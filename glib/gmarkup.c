474d91566a7db52ee1776933ea5c4b6276b1280a
Ryan Lortie <desrt@desrt.ca>
2013-10-28 14:05:51 -0700

    GMarkup: add G_MARKUP_IGNORE_QUALIFIED
    Add a flag to GMarkupParserFlags to ignore qualified tags (along with
    their contents) and attributes.
    
    This will provide a nice way for some of our parsers (GDBus
    introspection, GSettings schema, etc) to ignore additional tags that
    users have added to their files, under a different namespace.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=665634

--- glib/gmarkup.c
+++ glib/gmarkup.c
@@ -1004,21 +1004,39 @@ static inline void
 emit_start_element (GMarkupParseContext  *context,
                     GError              **error)
 {
-  int i;
+  int i, j = 0;
   const gchar *start_name;
   const gchar **attr_names;
   const gchar **attr_values;
   GError *tmp_error;
 
+  /* In case we want to ignore qualified tags and we see that we have
+   * one here, we push a subparser.  This will ignore all tags inside of
+   * the qualified tag.
+   *
+   * We deal with the end of the subparser from emit_end_element.
+   */
+  if (context->flags & G_MARKUP_IGNORE_QUALIFIED && strchr (current_element (context), ':'))
+    {
+      static const GMarkupParser ignore_parser;
+      g_markup_parse_context_push (context, &ignore_parser, NULL);
+      return;
+    }
+
   attr_names = g_newa (const gchar *, context->cur_attr + 2);
   attr_values = g_newa (const gchar *, context->cur_attr + 2);
   for (i = 0; i < context->cur_attr + 1; i++)
     {
-      attr_names[i] = context->attr_names[i]->str;
-      attr_values[i] = context->attr_values[i]->str;
+      /* Possibly omit qualified attribute names from the list */
+      if ((context->flags & G_MARKUP_IGNORE_QUALIFIED) && strchr (context->attr_names[i]->str, ':'))
+        continue;
+
+      attr_names[j] = context->attr_names[i]->str;
+      attr_values[j] = context->attr_values[i]->str;
+      j++;
     }
-  attr_names[i] = NULL;
-  attr_values[i] = NULL;
+  attr_names[j] = NULL;
+  attr_values[j] = NULL;
 
   /* Call user callback for element start */
   tmp_error = NULL;
@@ -1050,6 +1068,14 @@ emit_end_element (GMarkupParseContext  *context,
   g_assert (context->tag_stack != NULL);
 
   possibly_finish_subparser (context);
+
+  /* We might have just returned from our ignore subparser */
+  if (context->flags & G_MARKUP_IGNORE_QUALIFIED && strchr (current_element (context), ':'))
+    {
+      g_markup_parse_context_pop (context);
+      pop_tag (context);
+      return;
+    }
 
   tmp_error = NULL;
   if (context->parser->end_element)
