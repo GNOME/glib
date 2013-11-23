f969f1fc25248d4f53c78568481205b21ef1446b
Matthias Clasen <mclasen@redhat.com>
2013-11-23 10:49:00 -0500

    Small documentation clarification
    https://bugzilla.gnome.org/show_bug.cgi?id=671557

--- glib/gkeyfile.c
+++ glib/gkeyfile.c
@@ -859,7 +859,7 @@ g_key_file_load_from_file (GKeyFile       *key_file,
  * g_key_file_load_from_data:
  * @key_file: an empty #GKeyFile struct
  * @data: key file loaded in memory
- * @length: the length of @data in bytes (or -1 if data is nul-terminated)
+ * @length: the length of @data in bytes (or (gsize)-1 if data is nul-terminated)
  * @flags: flags from #GKeyFileFlags
  * @error: return location for a #GError, or %NULL
  *
