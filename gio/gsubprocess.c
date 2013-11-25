03bf43e14aa3d5c22f9365a6c6137e3bb1f67b02
Colin Walters <walters@verbum.org>
2013-11-25 18:39:46 -0500

    gsubprocess: Document g_subprocess_get_identifier()

--- gio/gsubprocess.c
+++ gio/gsubprocess.c
@@ -720,6 +720,13 @@ g_subprocess_newv (const gchar * const  *argv,
                          NULL);
 }
 
+/**
+ * g_subprocess_get_identifier:
+ * @subprocess: a #GSubprocess
+ *
+ * On UNIX, returns the process ID as a decimal string.  On Windows,
+ * returns the result of GetProcessId() also as a string.
+ */
 const gchar *
 g_subprocess_get_identifier (GSubprocess *subprocess)
 {
