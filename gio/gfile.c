091e466095c47f60ef5b2b0c81459615169216cf
Matthias Scheler <tron@zhadum.de>
2013-12-25 13:57:56 -0500

    gio: Fix return of value from void function
    https://bugzilla.gnome.org/show_bug.cgi?id=721034

--- gio/gfile.c
+++ gio/gfile.c
@@ -7676,9 +7676,9 @@ g_file_measure_disk_usage_async (GFile                        *file,
   g_return_if_fail (G_IS_FILE (file));
   g_return_if_fail (cancellable == NULL || G_IS_CANCELLABLE (cancellable));
 
-  return G_FILE_GET_IFACE (file)->measure_disk_usage_async (file, flags, io_priority, cancellable,
-                                                            progress_callback, progress_data,
-                                                            callback, user_data);
+  G_FILE_GET_IFACE (file)->measure_disk_usage_async (file, flags, io_priority, cancellable,
+                                                     progress_callback, progress_data,
+                                                     callback, user_data);
 }
 
 /**
