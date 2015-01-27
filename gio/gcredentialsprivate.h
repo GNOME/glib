4c54b9fe31b2a9b2db3ccbce71ae486001cb8b5e
Peeter Must <karu.pruun@gmail.com>
2015-01-27 11:34:48 +0900

    gcredentials: Add support for DragonFly
    https://bugzilla.gnome.org/show_bug.cgi?id=743508

--- gio/gcredentialsprivate.h
+++ gio/gcredentialsprivate.h
@@ -31,7 +31,7 @@
 #define G_CREDENTIALS_SOCKET_GET_CREDENTIALS_SUPPORTED 1
 #define G_CREDENTIALS_SPOOFING_SUPPORTED 1
 
-#elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__GNU__)
+#elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__GNU__) || defined(__DragonFly__)
 #define G_CREDENTIALS_SUPPORTED 1
 #define G_CREDENTIALS_USE_FREEBSD_CMSGCRED 1
 #define G_CREDENTIALS_NATIVE_TYPE G_CREDENTIALS_TYPE_FREEBSD_CMSGCRED
