GNOME/glib

commit 126c685f4aa627a8190e09adfc8ab0733c14a6ea

GOptionContext: Improve help in simple cases
Only add [OPTION...] to the usage line if the context
has options. And shorten "Application Options" to just
"Options" if we don't have to differentiate from other
kinds of options.

glib/goption.c
@@ -854,8 +854,11 @@ g_option_context_get_help (GOptionContext *context,
         }
     }
 
-  g_string_append_printf (string, "%s\n  %s %s",
-                          _("Usage:"), g_get_prgname(), _("[OPTION...]"));
+  g_string_append_printf (string, "%s\n  %s", _("Usage:"), g_get_prgname());
+  if (context->help_enabled ||
+      context->main_group->n_entries > 0 ||
+      context->groups != NULL)
+    g_string_append_printf (string, " %s", _("[OPTION...]"));
 
   if (rest_description)
     {
@@ -1040,7 +1043,10 @@ g_option_context_get_help (GOptionContext *context,
     {
       list = context->groups;
 
-      g_string_append (string,  _("Application Options:"));
+      if (context->help_enabled || list)
+        g_string_append (string,  _("Application Options:"));
+      else
+        g_string_append (string, _("Options:"));
       g_string_append (string, "\n");
       if (context->main_group)
         for (i = 0; i < context->main_group->n_entries; i++)

commit 08a3f3f3d2190c7ff393ea13c5a310ba8a13a2e0

GOptionContext: Don't crash without main group
This was introduced in 126c685f4aa and caused e.g. gdbus
to crash when called without arguments.

https://bugzilla.gnome.org/show_bug.cgi?id=752210

glib/goption.c
@@ -854,9 +854,9 @@ g_option_context_get_help (GOptionContext *context,
         }
     }
 
-  g_string_append_printf (string, "%s\n  %s", _("Usage:"), g_get_prgname());
+  g_string_append_printf (string, "%s\n  %s", _("Usage:"), g_get_prgname ());
   if (context->help_enabled ||
-      context->main_group->n_entries > 0 ||
+      (context->main_group && context->main_group->n_entries > 0) ||
       context->groups != NULL)
     g_string_append_printf (string, " %s", _("[OPTION...]"));
 