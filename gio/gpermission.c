GNOME/glib

commit a8eedd00a726483cd29a2a5c9660c82438a5d57e

gio: fix a leftover GSimpleAsyncResult usage
And remove remaining unnecessary gsimpleasyncresult.h includes

gio/gpermission.c
@@ -24,7 +24,7 @@
 #include "gioerror.h"
 #include "gioenums.h"
 #include "gasyncresult.h"
-#include "gsimpleasyncresult.h"
+#include "gtask.h"
 #include "glibintl.h"
 
 
@@ -406,10 +406,11 @@ acquire_or_release_async (GPermission         *permission,
                           GAsyncReadyCallback  callback,
                           gpointer             user_data)
 {
-  g_simple_async_report_error_in_idle (G_OBJECT (permission),
-                                       callback, user_data,
-                                       G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED,
-                                       "Can't acquire or release permission");
+  g_task_report_new_error (permission,
+                           callback, user_data,
+                           NULL,
+                           G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED,
+                           "Can't acquire or release permission");
 }
 
 static gboolean

gio/gconverterinputstream.c
@@ -24,7 +24,6 @@
 
 #include "gconverterinputstream.h"
 #include "gpollableinputstream.h"
-#include "gsimpleasyncresult.h"
 #include "gcancellable.h"
 #include "gioenumtypes.h"
 #include "gioerror.h"

gio/gconverteroutputstream.c
@@ -24,7 +24,6 @@
 
 #include "gconverteroutputstream.h"
 #include "gpollableoutputstream.h"
-#include "gsimpleasyncresult.h"
 #include "gcancellable.h"
 #include "gioenumtypes.h"
 #include "gioerror.h"

gio/gdbusobjectmanagerclient.c
@@ -27,7 +27,6 @@
 #include "gioenumtypes.h"
 #include "ginitable.h"
 #include "gasyncresult.h"
-#include "gsimpleasyncresult.h"
 #include "gasyncinitable.h"
 #include "gdbusconnection.h"
 #include "gdbusutils.h"

gio/gdummytlsbackend.c
@@ -31,7 +31,6 @@
 #include "gtlsdatabase.h"
 #include "gtlsfiledatabase.h"
 #include "gtlsserverconnection.h"
-#include "gsimpleasyncresult.h"
 
 #include "giomodule.h"
 #include "giomodule-priv.h"

gio/gproxyresolver.c
@@ -29,7 +29,6 @@
 #include "gcancellable.h"
 #include "giomodule.h"
 #include "giomodule-priv.h"
-#include "gsimpleasyncresult.h"
 
 /**
  * SECTION:gproxyresolver

gio/gresolver.c
@@ -26,7 +26,6 @@
 #include "gnetworkingprivate.h"
 #include "gasyncresult.h"
 #include "ginetaddress.h"
-#include "gsimpleasyncresult.h"
 #include "gtask.h"
 #include "gsrvtarget.h"
 #include "gthreadedresolver.h"

gio/gunixinputstream.c
@@ -31,7 +31,6 @@
 #include <glib/gstdio.h>
 #include <glib/glib-unix.h>
 #include "gioerror.h"
-#include "gsimpleasyncresult.h"
 #include "gunixinputstream.h"
 #include "gcancellable.h"
 #include "gasynchelper.h"

commit 656494a7840cfb6c53c2cb9a27d82f58e2dd6f40

gpermission: fix async error returns
a8eedd00 broke the error return values from
g_permission_acquire/release_async() on GSimplePermission. Fix that.

gio/gpermission.c
@@ -418,8 +418,7 @@ acquire_or_release_finish (GPermission   *permission,
                            GAsyncResult  *result,
                            GError       **error)
 {
-  g_async_result_legacy_propagate_error (result, error);
-  return FALSE;
+  return g_task_propagate_boolean (G_TASK (result), error);
 }
 
 static void