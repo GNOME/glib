1dec512a66fddfd8b4b265231b00d4f918b16cef
Matthias Clasen <mclasen@redhat.com>
2015-09-01 10:21:26 -0400

    Revert "GSettings: delay backend subscription"
    This reverts commit 8ff5668a458344da22d30491e3ce726d861b3619.
    
    This change has had considerable fallout, and there was no
    follow-up to address it.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=733791

--- gio/gsettings-tool.c
+++ gio/gsettings-tool.c
@@ -412,8 +412,6 @@ value_changed (GSettings   *settings,
 static void
 gsettings_monitor (void)
 {
-  gchar **keys;
-
   if (global_key)
     {
       gchar *name;
@@ -423,17 +421,6 @@ gsettings_monitor (void)
     }
   else
     g_signal_connect (global_settings, "changed", G_CALLBACK (value_changed), NULL);
-
-  /* We have to read a value from GSettings before we start receiving
-   * signals...
-   *
-   * If the schema has zero keys then we won't be displaying any
-   * notifications anyway.
-   */
-  keys = g_settings_schema_list_keys (global_schema);
-  if (keys[0])
-    g_variant_unref (g_settings_get_value (global_settings, keys[0]));
-  g_strfreev (keys);
 
   for (;;)
     g_main_context_iteration (NULL, TRUE);
