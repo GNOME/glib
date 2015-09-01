4a09d0cf7a65cd08d4156eafd80e943712216d49
Ting-Wei Lan <lantw@src.gnome.org>
2015-09-02 01:19:40 +0800

    Fix return value error in g_list_store_sort

--- gio/gliststore.c
+++ gio/gliststore.c
@@ -334,7 +334,7 @@ g_list_store_sort (GListStore       *store,
   gint n_items;
 
   g_return_if_fail (G_IS_LIST_STORE (store));
-  g_return_val_if_fail (compare_func != NULL, 0);
+  g_return_if_fail (compare_func != NULL);
 
   g_sequence_sort (store->items, compare_func, user_data);
 
