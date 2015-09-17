8297ea8badef5ae5bdeab85b74780fd70de60247
Matthew Waters <matthew@centricular.com>
2015-09-17 20:38:10 +1000

    win32: fix incorrect specifier
    error: format ‘%u’ expects argument of type ‘unsigned int’, but argument 4 has
       type ‘gsize {aka long long unsigned int}’ [-Werror=format=]
    
    https://bugzilla.gnome.org/show_bug.cgi?id=755154

--- gio/gwin32appinfo.c
+++ gio/gwin32appinfo.c
@@ -3095,7 +3095,7 @@ link_handlers_to_registered_apps (void)
         }
     }
 
-  g_debug ("%u undefhandled extensions\n", unhandled_exts);
+  g_debug ("%" G_GSIZE_FORMAT "undefhandled extensions\n", unhandled_exts);
   unhandled_exts= 0;
   g_hash_table_iter_init (&sup_iter, extensions);
   while (g_hash_table_iter_next (&sup_iter,
@@ -3109,7 +3109,7 @@ link_handlers_to_registered_apps (void)
           unhandled_exts += 1;
         }
     }
-  g_debug ("%u undefhandled extensions\n", unhandled_exts);
+  g_debug ("%" G_GSIZE_FORMAT "undefhandled extensions\n", unhandled_exts);
 }
 
 static void
