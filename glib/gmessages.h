ab26dd54337544275ff8bb61eb227aed83a8ed80
Emmanuele Bassi <ebassi@gnome.org>
2015-09-22 13:10:28 +0100

    Revert use of the system_header GCC pragma
    This reverts commit 662bf991c08b16dea8a36026243b311f6cdb17f1. It is not
    a straight up revert because the old commit involved various long since
    removed ChangeLog files and we'd end up mudding the patch.
    
    The system_header GCC pragma is breaking warnings in the various
    g_return_* macros; GCC stopped warning when using a macro with a return
    value in a function that returns void, as well as when using a macro
    with no return value in a function that has a non-void return value.
    Suppressing this kind of warnings is not a good idea.
    
    Other compilers are unaffected, even ones like Clang with a GCC
    compatibility layer.
    
    Given the fact that the original commit was added 14 years ago as a
    workaround in the old days of GTK+ 1.2, I think it's safe to drop it.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=753310

--- glib/gmessages.h
+++ glib/gmessages.h
@@ -33,12 +33,6 @@
 #include <glib/gtypes.h>
 #include <glib/gmacros.h>
 
-/* Suppress warnings when GCC is in -pedantic mode and not -std=c99
- */
-#if (__GNUC__ >= 3 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 96))
-#pragma GCC system_header
-#endif
-
 G_BEGIN_DECLS
 
 /* calculate a string size, guaranteed to fit format + args.
