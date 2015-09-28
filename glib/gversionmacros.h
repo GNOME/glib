b36b4941a634af096d21f906caae25ef35161166
Nicolas Dufresne <nicolas.dufresne@collabora.com>
2015-09-28 19:35:30 -0400

    glib: Add 2.48 availibity macros
    https://bugzilla.gnome.org/show_bug.cgi?id=755766

--- glib/gversionmacros.h
+++ glib/gversionmacros.h
@@ -145,6 +145,16 @@
  */
 #define GLIB_VERSION_2_46       (G_ENCODE_VERSION (2, 46))
 
+/**
+ * GLIB_VERSION_2_48:
+ *
+ * A macro that evaluates to the 2.48 version of GLib, in a format
+ * that can be used by the C pre-processor.
+ *
+ * Since: 2.48
+ */
+#define GLIB_VERSION_2_48       (G_ENCODE_VERSION (2, 48))
+
 /* evaluates to the current stable version; for development cycles,
  * this means the next stable target
  */
@@ -389,5 +399,20 @@
 #else
 # define GLIB_AVAILABLE_IN_2_46                 _GLIB_EXTERN
 #endif
+
+#if GLIB_VERSION_MIN_REQUIRED >= GLIB_VERSION_2_48
+# define GLIB_DEPRECATED_IN_2_48                GLIB_DEPRECATED
+# define GLIB_DEPRECATED_IN_2_48_FOR(f)         GLIB_DEPRECATED_FOR(f)
+#else
+# define GLIB_DEPRECATED_IN_2_48                _GLIB_EXTERN
+# define GLIB_DEPRECATED_IN_2_48_FOR(f)         _GLIB_EXTERN
+#endif
+
+#if GLIB_VERSION_MAX_ALLOWED < GLIB_VERSION_2_48
+# define GLIB_AVAILABLE_IN_2_48                 GLIB_UNAVAILABLE(2, 48)
+#else
+# define GLIB_AVAILABLE_IN_2_48                 _GLIB_EXTERN
+#endif
+
 
 #endif /*  __G_VERSION_MACROS_H__ */
