8ed9e8c79ffed28b2e6ee533d3d184c0a748ed48
Dan Winship <danw@gnome.org>
2015-10-02 09:56:16 -0400

    Revert "gvalue: Improve _unset() documentation"
    This reverts commit 3c0d38d68b78a6ea9e6ca82012f011075a625a2b.

--- gobject/gvalue.c
+++ gobject/gvalue.c
@@ -257,9 +257,7 @@ g_value_reset (GValue *value)
  * Clears the current value in @value and "unsets" the type,
  * this releases all resources associated with this GValue.
  * An unset value is the same as an uninitialized (zero-filled)
- * #GValue structure. Note that you cannot call this method on
- * uninitialized structure, for that, g_value_clear() method can
- * be used.
+ * #GValue structure.
  */
 void
 g_value_unset (GValue *value)
