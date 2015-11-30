30359e740953b596d6c3d980dba36dd131681a1f
Allison Ryan Lortie <desrt@desrt.ca>
2015-11-30 10:13:46 -0500

    file monitors: reorder some code to avoid segfault
    We must initialise '->source' before we use fields inside of it.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=758823

--- gio/glocalfilemonitor.c
+++ gio/glocalfilemonitor.c
@@ -748,6 +748,9 @@ g_local_file_monitor_start (GLocalFileMonitor *local_monitor,
 
   g_assert (!local_monitor->source);
 
+  source = g_file_monitor_source_new (local_monitor, filename, is_directory, flags);
+  local_monitor->source = source; /* owns the ref */
+
   if (is_directory && !class->mount_notify && (flags & G_FILE_MONITOR_WATCH_MOUNTS))
     {
 #ifdef G_OS_WIN32
@@ -770,9 +773,6 @@ g_local_file_monitor_start (GLocalFileMonitor *local_monitor,
                                G_CALLBACK (g_local_file_monitor_mounts_changed), local_monitor, 0);
 #endif
     }
-
-  source = g_file_monitor_source_new (local_monitor, filename, is_directory, flags);
-  local_monitor->source = source; /* owns the ref */
 
   G_LOCAL_FILE_MONITOR_GET_CLASS (local_monitor)->start (local_monitor,
                                                          source->dirname, source->basename, source->filename,
