a1bcd1e45a3eea98dedcee68f67b458e95e9193d
Marc-André Lureau <marcandre.lureau@gmail.com>
2016-02-17 17:50:52 +0100

    win32: fix off-by-one length check
    Laszlo Ersek said: "The length check is off by one (in the safe direction); it
    should be (nchars >= 2). The processing should be active for the wide string
    L"\r\n" -- resulting in the empty wide string --, I believe."
    
    Reported-by: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Marc-André Lureau <marcandre.lureau@gmail.com>
    
    https://bugzilla.gnome.org/show_bug.cgi?id=762202

--- glib/gwin32.c
+++ glib/gwin32.c
@@ -203,7 +203,7 @@ g_win32_error_message (gint error)
     {
       nchars = wcslen (msg);
       
-      if (nchars > 2 && msg[nchars-1] == '\n' && msg[nchars-2] == '\r')
+      if (nchars >= 2 && msg[nchars-1] == '\n' && msg[nchars-2] == '\r')
 	msg[nchars-2] = '\0';
       
       retval = g_utf16_to_utf8 (msg, -1, NULL, NULL, NULL);
