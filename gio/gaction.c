7f60cbb701b11e66dc3d827100ed7a3a74a67e2e
Sebastian Geiger <sbastig@gmx.net>
2016-02-26 16:46:24 +0100

    gio: fix documentation of GAction

--- gio/gaction.c
+++ gio/gaction.c
@@ -54,7 +54,7 @@ G_DEFINE_INTERFACE (GAction, g_action, G_TYPE_OBJECT)
  * In all cases, the implementing class is responsible for storing the
  * name of the action, the parameter type, the enabled state, the
  * optional state type and the state and emitting the appropriate
- * signals when these change.  The implementor responsible for filtering
+ * signals when these change.  The implementor is responsible for filtering
  * calls to g_action_activate() and g_action_change_state() for type
  * safety and for the state being enabled.
  *
