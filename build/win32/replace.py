374a61f24541ae26f018f3bf1a14cc4337d0f463
Chun-wei Fan <fanchunwei@src.gnome.org>
2016-03-10 20:33:35 +0800

    MSVC builds: Add scripts to generate .pc files
    Add a generic script, pc_base.py, which can be utilized to obtain path
    info, along with user-input version info, which can be used to generate
    pkg-config .pc files.  Also enhance replace.py a bit so that it can also
    be used in the future to replace multiple items in a file in one shot.
    This is done to make building introspection files easier, as it depends
    much on the pkg-config .pc files to work.
    
    Update the project files so that when Python is available, we can generate
    the full, usable .pc files when we complete and 'install' the builds, and
    copy them to appropriate locations so that pkg-config can be set to find
    them easily.

--- build/win32/replace.py
+++ build/win32/replace.py
@@ -21,6 +21,16 @@ valid_actions = ['remove-prefix',
                  'replace-str',
                  'remove-str']
 
+def replace_multi(src, dest, replace_items):
+    with open(src, 'r') as s:
+        with open(dest, 'w') as d:
+            for line in s:
+                replace_dict = dict((re.escape(key), value) \
+                               for key, value in replace_items.items())
+                replace_pattern = re.compile("|".join(replace_dict.keys()))
+                d.write(replace_pattern.sub(lambda m: \
+                        replace_dict[re.escape(m.group(0))], line))
+
 def replace(src, dest, instring, outstring):
     with open(src, 'r') as s:
         with open(dest, 'w') as d:
