054f78880e90c3778d6c2020bb574909d9847a6a
Krzesimir Nowak <krzesimir@kinvolk.io>
2016-06-17 23:05:58 +0200

    Extend GError to store extra data in the key-value store
    Extra data can be used to store additional context of the failure.

--- glib/tests/error.c
+++ glib/tests/error.c
@@ -82,6 +82,110 @@ test_copy (void)
   g_error_free (copy);
 }
 
+typedef struct
+{
+  guint copy_calls;
+  guint free_calls;
+} ExtraDataStats;
+
+static gpointer
+extra_data_stats_fake_copy (gpointer data)
+{
+  ExtraDataStats *stats = data;
+
+  stats->copy_calls++;
+
+  return stats;
+}
+
+static void
+extra_data_stats_fake_free (gpointer data)
+{
+  ExtraDataStats *stats = data;
+
+  stats->free_calls++;
+}
+
+static void
+test_extra_data (void)
+{
+  GError *error;
+  GError *copy;
+  ExtraDataStats stats = { 0, 0 };
+  gboolean exists;
+  const gchar** keys;
+
+  error = g_error_new_literal (G_MARKUP_ERROR, G_MARKUP_ERROR_EMPTY, "foo");
+  copy = NULL;
+
+  /* check if we get an empty strings array when there are no extra
+   * data
+   */
+  keys = g_error_get_extra_data_keys (error);
+  g_assert_nonnull (keys);
+  g_assert_cmpuint (g_strv_length ((gchar**)keys), ==, 0);
+  g_free (g_steal_pointer (&keys));
+
+  /* check if extra data is stolen, so no copies happen */
+  g_add_error_extra_data (&error, "test", &stats, extra_data_stats_fake_copy, extra_data_stats_fake_free);
+  g_assert_cmpuint (stats.copy_calls, ==, 0);
+  g_assert_cmpuint (stats.free_calls, ==, 0);
+
+  /* check if extra data is freed when no error is passed */
+  g_add_error_extra_data (NULL, "test", &stats, extra_data_stats_fake_copy, extra_data_stats_fake_free);
+  g_assert_cmpuint (stats.copy_calls, ==, 0);
+  g_assert_cmpuint (stats.free_calls, ==, 1);
+
+  /* check if extra data is freed when being replaced */
+  g_add_error_extra_data (&error, "test", &stats, extra_data_stats_fake_copy, extra_data_stats_fake_free);
+  g_assert_cmpuint (stats.copy_calls, ==, 0);
+  g_assert_cmpuint (stats.free_calls, ==, 2);
+
+  /* add another extra data */
+  g_add_error_extra_data (&error, "test2", &stats, extra_data_stats_fake_copy, extra_data_stats_fake_free);
+  g_assert_cmpuint (stats.copy_calls, ==, 0);
+  g_assert_cmpuint (stats.free_calls, ==, 2);
+
+  /* check if copy func was called twice, once for "test" and once for
+   * "test2"
+   */
+  copy = g_error_copy (error);
+  g_assert_cmpuint (stats.copy_calls, ==, 2);
+  g_assert_cmpuint (stats.free_calls, ==, 2);
+
+  g_assert_nonnull (g_error_get_extra_data (copy, "test", &exists));
+  g_assert_true (exists);
+  g_assert_null (g_error_get_extra_data (copy, "foo", &exists));
+  g_assert_false (exists);
+  /* also possible to pass NULL for exists parameter */
+  g_assert_null (g_error_get_extra_data (copy, "bar", NULL));
+  g_assert_nonnull (g_error_get_extra_data (copy, "test", NULL));
+  /* ensure no copying happened in extra data getters */
+  g_assert_cmpuint (stats.copy_calls, ==, 2);
+  g_assert_cmpuint (stats.free_calls, ==, 2);
+
+  /* check if we get the keys we expect (test and test2) */
+  keys = g_error_get_extra_data_keys (copy);
+  g_assert_nonnull (keys);
+  g_assert_cmpuint (g_strv_length ((gchar**)keys), ==, 2);
+  g_assert_true (g_strv_contains (keys, "test"));
+  g_assert_true (g_strv_contains (keys, "test2"));
+  g_free (g_steal_pointer (&keys));
+  /* ensure no copying happened in extra data keys getter */
+  g_assert_cmpuint (stats.copy_calls, ==, 2);
+  g_assert_cmpuint (stats.free_calls, ==, 2);
+
+  /* check if extra data is freed when error is freed */
+  g_error_free (g_steal_pointer (&error));
+  g_assert_cmpuint (stats.copy_calls, ==, 2);
+  g_assert_cmpuint (stats.free_calls, ==, 4);
+
+  /* check if extra data is freed when error is freed */
+  g_clear_error (&copy);
+  g_assert_cmpuint (stats.copy_calls, ==, 2);
+  g_assert_cmpuint (stats.free_calls, ==, 6);
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -91,6 +195,7 @@ main (int argc, char *argv[])
   g_test_add_func ("/error/prefix", test_prefix);
   g_test_add_func ("/error/literal", test_literal);
   g_test_add_func ("/error/copy", test_copy);
+  g_test_add_func ("/error/extra-data", test_extra_data);
 
   return g_test_run ();
 }
