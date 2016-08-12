0e3f968a2e9d35b4c087128c9819f07ef9e4ce99
Philip Withnall <philip.withnall@collabora.co.uk>
2016-08-12 15:58:39 +0200

    gio: Fix use of Docbook in documentation comment
    This is literally so last year.

--- gio/ginetsocketaddress.c
+++ gio/ginetsocketaddress.c
@@ -396,7 +396,7 @@ g_inet_socket_address_new (GInetAddress *address,
  * Creates a new #GInetSocketAddress for @address and @port.
  *
  * If @address is an IPv6 address, it can also contain a scope ID
- * (separated from the address by a "<literal>%</literal>").
+ * (separated from the address by a `%`).
  *
  * Returns: a new #GInetSocketAddress, or %NULL if @address cannot be
  * parsed.
