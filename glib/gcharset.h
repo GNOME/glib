f60590534d375750389f34a40bd3d280748e17df
Sébastien Wilmet <swilmet@gnome.org>
2016-12-27 17:40:22 +0100

    License headers: "the Gnome Library" -> "this library"
    Harmonize a little the license headers. In most of the license headers
    in GLib, it is "this library".
    
    It is also what is explained at:
    https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html#SEC4
    "How to Apply These Terms to Your New Libraries"
    
    https://bugzilla.gnome.org/show_bug.cgi?id=776586

--- glib/gcharset.h
+++ glib/gcharset.h
@@ -12,9 +12,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with the Gnome Library; see the file COPYING.LIB.  If not,
- * see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __G_CHARSET_H__
--- glib/gthreadprivate.h
+++ glib/gthreadprivate.h
@@ -4,19 +4,18 @@
  *
  *  Copyright (C) 2003 Sebastian Wilhelmi
  *
- * The Gnome Library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
- * The Gnome Library is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with the Gnome Library; see the file COPYING.LIB.  If not,
- * see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __G_THREADPRIVATE_H__
