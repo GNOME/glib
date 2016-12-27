e603035d133b608eb1d208b5d14be6e05abc753d
Sébastien Wilmet <swilmet@gnome.org>
2016-12-27 17:29:04 +0100

    License headers: "The GLib Library" -> "This library"
    Harmonize a little the license headers. In most of the license headers
    in GLib, it is "This library".
    
    It is also what is explained at:
    https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html#SEC4
    "How to Apply These Terms to Your New Libraries"
    
    https://bugzilla.gnome.org/show_bug.cgi?id=776586

--- glib/gcharset.h
+++ glib/gcharset.h
@@ -2,12 +2,12 @@
  *
  *  Copyright (C) 2011 Red Hat, Inc.
  *
- * The GLib Library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
- * The GLib Library is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
