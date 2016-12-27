95cad9c3e0ba1d45ea8e1295505c50b6ac647a85
Sébastien Wilmet <swilmet@gnome.org>
2016-12-27 14:59:46 +0100

    glib/tests/: LGPLv2+ -> LGPLv2.1+
    There are other tests in glib/tests/ that are licensed under a BSD-style
    license, and other tests that don't have any license header.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=776504

--- glib/tests/642026.c
+++ glib/tests/642026.c
@@ -2,10 +2,10 @@
  * Author: Simon McVittie <simon.mcvittie@collabora.co.uk>
  * Copyright © 2011 Nokia Corporation
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * See the included COPYING file for more information.
  */
--- glib/tests/markup-collect.c
+++ glib/tests/markup-collect.c
@@ -1,10 +1,10 @@
 /* 
  * Copyright © 2007 Ryan Lortie
  * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  * 
  * See the included COPYING file for more information.
  */
