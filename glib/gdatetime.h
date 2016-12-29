6231b1abc9ac13f8766b79d148e83f169cd175bd
Sébastien Wilmet <swilmet@gnome.org>
2016-12-29 16:51:51 +0100

    License headers: replace current FSF address by a link to gnu.org
    Even if the address is correct, it's far more convenient to have a link
    to a website.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=776586

--- glib/gdatetime.h
+++ glib/gdatetime.h
@@ -12,10 +12,8 @@
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
  * License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
- * USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
  *
  * Authors: Christian Hergert <chris@dronelabs.com>
  *          Thiago Santos <thiago.sousa.santos@collabora.co.uk>
