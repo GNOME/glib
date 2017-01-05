f9faac7661fb84a1c7f998a76c7e89280a17b7b9
Sébastien Wilmet <swilmet@gnome.org>
2017-01-05 12:47:07 +0100

    glib/: LGPLv2+ -> LGPLv2.1+
    All glib/*.{c,h} files have been processed, as well as gtester-report.
    
    12 of those files are not licensed under LGPL:
    
    	gbsearcharray.h
    	gconstructor.h
    	glibintl.h
    	gmirroringtable.h
    	gscripttable.h
    	gtranslit-data.h
    	gunibreak.h
    	gunichartables.h
    	gunicomp.h
    	gunidecomp.h
    	valgrind.h
    	win_iconv.c
    
    Some of them are generated files, some are licensed under a BSD-style
    license and win_iconv.c is in the public domain.
    
    Sub-directories inside glib/:
    
    	deprecated/: processed in a previous commit
    	glib-mirroring-tab/: already LGPLv2.1+
    	gnulib/: not modified, the code is copied from gnulib
    	libcharset/: a copy
    	pcre/: a copy
    	tests/: processed in a previous commit
    
    https://bugzilla.gnome.org/show_bug.cgi?id=776504

--- glib/goption.h
+++ glib/goption.h
@@ -3,17 +3,17 @@
  *  Copyright (C) 2004  Anders Carlsson <andersca@gnome.org>
  *
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
+ * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
- * Library General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __G_OPTION_H__
