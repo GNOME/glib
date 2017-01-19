006a7d082bc3aacc8d73c150f279165f1525d52f
Ondrej Holy <oholy@redhat.com>
2017-01-19 16:13:51 +0100

    fileinfo: Add G_FILE_ATTRIBUTE_RECENT_MODIFIED attribute
    Add filesystem attribute to propagate time, when the metadata for the file
    in "recent:///" was last changed. This attribute is needed for sorting
    recent backend files in client applications.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=777507

--- gio/gfileinfo.h
+++ gio/gfileinfo.h
@@ -867,6 +867,17 @@ typedef struct _GFileInfoClass   GFileInfoClass;
  **/
 #define G_FILE_ATTRIBUTE_TRASH_DELETION_DATE "trash::deletion-date"  /* string */
 
+/**
+ * G_FILE_ATTRIBUTE_RECENT_MODIFIED:
+ *
+ * A key in the "recent" namespace for getting time, when the metadata for the
+ * file in `recent:///` was last changed. Corresponding #GFileAttributeType is
+ * %G_FILE_ATTRIBUTE_TYPE_INT64.
+ *
+ * Since: 2.52
+ **/
+#define G_FILE_ATTRIBUTE_RECENT_MODIFIED "recent::modified"          /* int64 (time_t) */
+
 GLIB_AVAILABLE_IN_ALL
 GType              g_file_info_get_type                  (void) G_GNUC_CONST;
 
