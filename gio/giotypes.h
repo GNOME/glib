09d936c528d7c534332f2d38d3e25178b6baaa4e
Debarshi Ray <debarshir@gnome.org>
2017-06-16 14:15:03 +0200

    docs: Explain how GAsyncReadyCallbacks are, and should be, invoked
    https://bugzilla.gnome.org/show_bug.cgi?id=783825

--- gio/giotypes.h
+++ gio/giotypes.h
@@ -264,7 +264,13 @@ typedef struct _GVolumeMonitor                GVolumeMonitor;
  * @user_data: user data passed to the callback.
  *
  * Type definition for a function that will be called back when an asynchronous
- * operation within GIO has been completed.
+ * operation within GIO has been completed. #GAsyncReadyCallback
+ * callbacks from #GTask are guaranteed to be invoked in a later
+ * iteration of the
+ * [thread-default main context][g-main-context-push-thread-default]
+ * where the #GTask was created. All other users of
+ * #GAsyncReadyCallback must likewise call it asynchronously in a
+ * later iteration of the main context.
  **/
 typedef void (*GAsyncReadyCallback) (GObject *source_object,
 				     GAsyncResult *res,
