94816e10d21ac545f9d0120a07b8c6d5dee0456d
Daniel Boles <dboles@src.gnome.org>
2017-08-03 11:30:20 +0100

    glib-compile-schemas: Fix typo in newly translatable string

--- gio/glib-compile-schemas.c
+++ gio/glib-compile-schemas.c
@@ -405,7 +405,7 @@ key_state_set_range (KeyState     *state,
     {
       g_set_error (error, G_MARKUP_ERROR,
                    G_MARKUP_ERROR_INVALID_CONTENT,
-                   _("<range> specified minimum is greater than maxmimum"));
+                   _("<range> specified minimum is greater than maximum"));
       return;
     }
 
