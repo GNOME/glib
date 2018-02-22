995f75cdd79d5e293d18b0d551fc3d9d9639a19e
Christoph Reiter <creiter@src.gnome.org>
2018-02-22 19:37:16 +0100

    gitlab-ci: Add 32bit MinGW jobs using MSYS2
    This builds glib using meson/ninja/ccache with mingw-w64 on a Windows
    machine.
    
    The CI scripts expect a gitlab runner to exist with the "win32" tag
    which uses the default "cmd" shell by default.
    
    Before running the tests pacman is invoked to update the system
    (potentially including bash etc, thus the extra step)
    Then a login shell is started with CHERE_INVOKING to not change the
    cwd and finally the test script is executed.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=793729

---
+++ .gitlab-ci/test-msys2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+export PATH="/c/msys64/$MSYSTEM/bin:$PATH"
+if [[ "$MSYSTEM" == "MINGW32" ]]; then
+    export MSYS2_ARCH="i686"
+else
+    export MSYS2_ARCH="x86_64"
+fi
+
+pacman --noconfirm -Suy
+
+pacman --noconfirm -S --needed \
+    base-devel \
+    mingw-w64-$MSYS2_ARCH-ccache \
+    mingw-w64-$MSYS2_ARCH-gettext \
+    mingw-w64-$MSYS2_ARCH-libffi \
+    mingw-w64-$MSYS2_ARCH-meson \
+    mingw-w64-$MSYS2_ARCH-pcre \
+    mingw-w64-$MSYS2_ARCH-python3 \
+    mingw-w64-$MSYS2_ARCH-toolchain \
+    mingw-w64-$MSYS2_ARCH-zlib
+
+mkdir -p _ccache
+export CCACHE_BASEDIR="$(pwd)"
+export CCACHE_DIR="${CCACHE_BASEDIR}/_ccache"
+
+# FIXME: Add --werror
+meson --buildtype debug _build
+cd _build
+ninja
+
+# FIXME: fix the test suite
+meson test || true
