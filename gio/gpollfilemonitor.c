ba4a9538e14e8ba0ea037cab5f4b23aa47272a4c
Ting-Wei Lan <lantw@src.gnome.org>
2018-03-20 21:58:20 +0800

    gpollfilemonitor: Fix use-after-free caused by leaking GSource
    https://bugzilla.gnome.org/show_bug.cgi?id=794528

--- gio/gpollfilemonitor.c
+++ gio/gpollfilemonitor.c
@@ -50,7 +50,9 @@ g_poll_file_monitor_finalize (GObject* object)
   
   poll_monitor = G_POLL_FILE_MONITOR (object);
 
+  g_poll_file_monitor_cancel (G_FILE_MONITOR (poll_monitor));
   g_object_unref (poll_monitor->file);
+  g_clear_object (&poll_monitor->last_info);
 
   G_OBJECT_CLASS (g_poll_file_monitor_parent_class)->finalize (object);
 }
