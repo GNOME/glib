9ba24986bbfce41a22b23b82b3bb4c5f7b6c03f9
Christoph Reiter <creiter@src.gnome.org>
2018-05-24 19:18:25 +0200

    ci: Add a VS2017 job
    Thanks to Xavier Claessens for figuring out what is needed.

---
+++ .gitlab-ci/test-msvc.bat
@@ -0,0 +1,11 @@
+@echo on
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+@echo on
+
+meson _build || goto :error
+cd _build
+ninja || goto :error
+
+goto :EOF
+:error
+exit /b 1
