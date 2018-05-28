75950d70c9e7bacdf7c828d7aa0a85c94ed617fa
Bastien Nocera <hadess@hadess.net>
2018-05-28 17:13:53 +0200

    gio: Deprecate G_VOLUME_IDENTIFIER_KIND_HAL_UDI
    HAL itself has long been deprecated.

--- gio/gvolume.h
+++ gio/gvolume.h
@@ -30,12 +30,16 @@
 
 G_BEGIN_DECLS
 
+#ifndef G_DISABLE_DEPRECATED
 /**
  * G_VOLUME_IDENTIFIER_KIND_HAL_UDI:
  *
  * The string used to obtain a Hal UDI with g_volume_get_identifier().
+ *
+ * Deprecated: 2.58: Do not use, HAL is deprecated.
  */
 #define G_VOLUME_IDENTIFIER_KIND_HAL_UDI "hal-udi"
+#endif /* G_DISABLE_DEPRECATED */
 
 /**
  * G_VOLUME_IDENTIFIER_KIND_UNIX_DEVICE:
