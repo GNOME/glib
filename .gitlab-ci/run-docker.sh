ca2fc30dea08268fbeb36c395f8a0c394fe0b712
Xavier Claessens <xavier.claessens@collabora.com>
2018-06-22 11:24:24 -0400

    ci: Add "--push" argument to run-docker.sh help script
    Also there is no need to run docker as root, the user just needs to be
    in the "docker" group.

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -4,8 +4,14 @@ set -e
 
 TAG="registry.gitlab.gnome.org/gnome/glib/master:v4"
 
-sudo docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
+docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
     --file "Dockerfile" .
-sudo docker run --rm \
-    --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
-    --tty --interactive "${TAG}" bash
+
+if [ "$1" = "--push" ]; then
+  docker login registry.gitlab.gnome.org
+  docker push $TAG
+else
+  docker run --rm \
+      --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
+      --tty --interactive "${TAG}" bash
+fi
