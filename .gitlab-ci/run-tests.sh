15af47fd5decf2c0f56614e3fb68aacbffa9bc50
Philip Withnall <withnall@endlessm.com>
2018-06-26 17:23:09 +0100

    ci: Add valgrind memcheck support on Fedora
    Add a separate CI job which runs memcheck on the unit tests. This is
    done as a separate job from the main build, since we don’t want it to
    interact with code coverage at all.
    
    Currently, failure of this job is ignored. Issue #333 will eventually
    fix that.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    
    Fixes: #487

--- .gitlab-ci/run-tests.sh
+++ .gitlab-ci/run-tests.sh
@@ -5,7 +5,8 @@ set +e
 meson test \
         -C _build \
         --timeout-multiplier ${MESON_TEST_TIMEOUT_MULTIPLIER} \
-        --no-suite flaky
+        --no-suite flaky \
+        "$@"
 
 exit_code=$?
 
@@ -13,6 +14,6 @@ python3 .gitlab-ci/meson-junit-report.py \
         --project-name=glib \
         --job-id "${CI_JOB_NAME}" \
         --output "_build/${CI_JOB_NAME}-report.xml" \
-        _build/meson-logs/testlog.json
+        _build/meson-logs/testlog*.json
 
 exit $exit_code
