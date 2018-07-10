e57fe623cb5359fc53438df2095e58d5b9959f88
Philip Withnall <withnall@endlessm.com>
2018-07-10 16:14:12 +0200

    tests: Fix error reporting on prlimit() call failure
    prlimit() returns its error code in errno, not as a return value.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    
    https://gitlab.gnome.org/GNOME/glib/issues/1044

--- glib/tests/thread.c
+++ glib/tests/thread.c
@@ -146,8 +146,8 @@ test_thread4 (void)
   getrlimit (RLIMIT_NPROC, &nl);
   nl.rlim_cur = 1;
 
-  if ((ret = prlimit (getpid(), RLIMIT_NPROC, &nl, &ol)) != 0)
-    g_error ("prlimit failed: %s", g_strerror (ret));
+  if ((ret = prlimit (getpid (), RLIMIT_NPROC, &nl, &ol)) != 0)
+    g_error ("prlimit failed: %s", g_strerror (errno));
 
   error = NULL;
   thread = g_thread_try_new ("a", thread1_func, NULL, &error);
@@ -156,7 +156,7 @@ test_thread4 (void)
   g_error_free (error);
 
   if ((ret = prlimit (getpid (), RLIMIT_NPROC, &ol, NULL)) != 0)
-    g_error ("resetting RLIMIT_NPROC failed: %s", g_strerror (ret));
+    g_error ("resetting RLIMIT_NPROC failed: %s", g_strerror (errno));
 #endif
 }
 
