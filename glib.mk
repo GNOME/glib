5840b44499a0545db6e926ce5b475d197ad90464
Simon McVittie <smcv@collabora.com>
2018-09-24 23:02:02 +0100

    Generate gtester-report from .in file
    Previously, it was installed unmodified by the Meson build system.
    
    We don't need to define @bindir@, because gtester-report never
    actually referred to it. We also don't need a definition of
    GTESTER_REPORT for use by GLib itself, because its last use was removed
    in 2013 (https://bugzilla.gnome.org/show_bug.cgi?id=709995).
    
    Fixes: https://gitlab.gnome.org/GNOME/glib/issues/1544
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- glib.mk
+++ glib.mk
@@ -3,7 +3,6 @@
 #GTESTER = gtester 			# for non-GLIB packages
 #GTESTER_REPORT = gtester-report        # for non-GLIB packages
 GTESTER = $(top_builddir)/glib/gtester			# for the GLIB package
-GTESTER_REPORT = $(top_builddir)/glib/gtester-report	# for the GLIB package
 NULL =
 
 # initialize variables for unconditional += appending
