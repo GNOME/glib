0ba3dabfc821938a0e702be237d4f13b4c294bb1
Xavier Claessens <xavier.claessens@collabora.com>
2018-09-26 10:56:10 -0400

    WIP: CI: Test static build on installed glib

---
+++ .gitlab-ci/fedora-test-installed.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+set -e
+
+prefix=$1
+
+# Verify there is no distro gio-2.0 installed, so we are sure we're going to
+# test the built one.
+if `pkg-config --exists gio-2.0`; then
+  echo "Docker image shouldn't have gio installed"
+  exit 1
+fi
+
+# Ensure we can static link and run a test app
+export PKG_CONFIG_PATH=$prefix/lib64/pkgconfig
+meson gio/tests/static-link _build_static_link
+ninja -C _build_static_link test
