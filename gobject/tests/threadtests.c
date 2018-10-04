48149d6bc762bfe4ed2f99926e8f4da508b9994d
Benjamin Otte <otte@redhat.com>
2018-10-04 13:02:18 +0200

    Suggest braces around empty body in ‘if’ statements

--- gobject/tests/threadtests.c
+++ gobject/tests/threadtests.c
@@ -155,7 +155,7 @@ static void
 prop_tester_init (PropTester* t)
 {
   if (t->name == NULL)
-    ; /* neds unit test framework initialization: g_test_bug ("race initializing properties"); */
+    { } /* neds unit test framework initialization: g_test_bug ("race initializing properties"); */
 }
 static void
 prop_tester_set_property (GObject        *object,
