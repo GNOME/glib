25d474f0c7aa945f50251f440f0fdc780fa1947e
Tapasweni Pathak <tapaswenipathak@gmail.com>
2018-10-05 18:31:43 +0530

    gqueue: Add g_queue_clear_full API
    g_queue_clear_full API frees all elements in the queue
    and than calls g_queue_init() for completion.
    
    Closes https://gitlab.gnome.org/GNOME/glib/issues/1464.

--- glib/gqueue.h
+++ glib/gqueue.h
@@ -81,6 +81,9 @@ void     g_queue_init           (GQueue           *queue);
 GLIB_AVAILABLE_IN_ALL
 void     g_queue_clear          (GQueue           *queue);
 GLIB_AVAILABLE_IN_ALL
+void     g_queue_clear_full     (GQueue           *queue,
+				GDestroyNotify    free_func);
+GLIB_AVAILABLE_IN_ALL
 gboolean g_queue_is_empty       (GQueue           *queue);
 GLIB_AVAILABLE_IN_ALL
 guint    g_queue_get_length     (GQueue           *queue);
