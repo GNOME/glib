23700aa598db57a0f40b860cf96ce33aaf9b06d4
pdknsk <pdknsk@gmail.com>
2018-10-06 18:55:24 +0200

    Rework fuzz.h

--- fuzzing/fuzz.h
+++ fuzzing/fuzz.h
@@ -1,20 +1,21 @@
 #include "gio/gio.h"
 #include "glib/glib.h"
 
-#ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION // oss-fuzz
+int LLVMFuzzerTestOneInput (const unsigned char *data, size_t size);
+
+#ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION // set by oss-fuzz
 static GLogWriterOutput
 empty_logging_func (GLogLevelFlags log_level, const GLogField *fields,
                     gsize n_fields, gpointer user_data)
 {
   return G_LOG_WRITER_HANDLED;
 }
+#endif
 
 static void
 fuzz_set_logging_func (void)
 {
+#ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
   g_log_set_writer_func (empty_logging_func, NULL, NULL);
-}
-#else
-int LLVMFuzzerTestOneInput (const unsigned char *data, size_t size);
-static void fuzz_set_logging_func (void) {};
 #endif
+}
