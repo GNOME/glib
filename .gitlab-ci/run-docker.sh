042a80ac4c4229381ab4f43b23e41a0a0594ecb6
Xavier Claessens <xavier.claessens@collabora.com>
2018-10-09 11:24:42 -0400

    ci: Avoid downloading subprojects for each job

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-TAG="registry.gitlab.gnome.org/gnome/glib/master:v11"
+TAG="registry.gitlab.gnome.org/gnome/glib/master:v12"
 
 cd "$(dirname "$0")"
 docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
