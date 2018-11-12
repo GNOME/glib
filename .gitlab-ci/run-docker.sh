8890efbf8dc601c87bd2b18fc582e50c938054c6
Will Thompson <will@willthompson.co.uk>
2018-11-12 12:44:19 +0000

    ci: add bindfs and fuse to Fedora CI image
    Without `bindfs` and `fusermount`, all tests in
    g-file-info-filesystem-readonly.c are skipped.

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-TAG="registry.gitlab.gnome.org/gnome/glib/master:v9"
+TAG="registry.gitlab.gnome.org/gnome/glib/master:v10"
 
 docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
     --file "Dockerfile" .
