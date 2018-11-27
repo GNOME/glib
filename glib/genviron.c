44d7203d893f364eee4d5919673fd025a0c43a08
Peter Wu <peter@lekensteyn.nl>
2018-11-27 16:02:37 +0100

    genviron: make g_environ_* case-insensitive on Windows
    g_environ_getenv(env, "PATH") and g_environ_setenv(env, "PATH", newpath)
    did not have the intended effect on Windows due to the environment block
    containing "Path=". Make these functions case-insensitive for Windows.

--- glib/genviron.c
+++ glib/genviron.c
@@ -44,6 +44,17 @@
 #include "gquark.h"
 
 /* Environ array functions {{{1 */
+static gboolean
+g_environ_matches (const gchar *env, const gchar *variable, gint len)
+{
+#ifdef G_OS_WIN32
+    /* Like filesystem paths, environment variables are case-insensitive. */
+    return g_ascii_strncasecmp (env, variable, len) == 0 && env[len] == '=';
+#else
+    return strncmp (env, variable, len) == 0 && env[len] == '=';
+#endif
+}
+
 static gint
 g_environ_find (gchar       **envp,
                 const gchar  *variable)
@@ -57,8 +68,7 @@ g_environ_find (gchar       **envp,
 
   for (i = 0; envp[i]; i++)
     {
-      if (strncmp (envp[i], variable, len) == 0 &&
-          envp[i][len] == '=')
+      if (g_environ_matches (envp[i], variable, len))
         return i;
     }
 
@@ -166,7 +176,7 @@ g_environ_unsetenv_internal (gchar        **envp,
   e = f = envp;
   while (*e != NULL)
     {
-      if (strncmp (*e, variable, len) != 0 || (*e)[len] != '=')
+      if (!g_environ_matches (*e, variable, len))
         {
           *f = *e;
           f++;
