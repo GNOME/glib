931b219665cba4a7150c36e6b0f2d53b8c3f69fa
Patrick Griffis <pgriffis@igalia.com>
2018-12-11 10:10:10 -0500

    gsocketaddressenumerator: Make it an error to call next_async before finish
    It logically doesn't make sense to repeatedly call next_async()
    before the previous one has finished.

--- gio/gsocketaddressenumerator.c
+++ gio/gsocketaddressenumerator.c
@@ -120,6 +120,8 @@ g_socket_address_enumerator_real_next_async (GSocketAddressEnumerator *enumerato
  * Asynchronously retrieves the next #GSocketAddress from @enumerator
  * and then calls @callback, which must call
  * g_socket_address_enumerator_next_finish() to get the result.
+ *
+ * It is an error to call this multiple times before the previous callback has finished.
  */
 void
 g_socket_address_enumerator_next_async (GSocketAddressEnumerator *enumerator,
