c69a98057f47e8b50d8599996aa2fe91b6a8c56e
Tomasz Miąsko <tomasz.miasko@gmail.com>
2018-12-12 00:00:00 +0000

    tests: Avoid writing ever increasing sequence of null bytes to test logs

--- gobject/tests/taptestrunner.py
+++ gobject/tests/taptestrunner.py
@@ -121,6 +121,9 @@ class TAPTestResult(unittest.TestResult):
             self.print_raw("  ...\n")
           else:
             self.print_raw("# " + output.rstrip().replace("\n", "\n# ") + "\n")
+        # Truncate doesn't change the current stream position.
+        # Seek to the beginning to avoid extensions on subsequent writes.
+        log.seek(0)
         log.truncate(0)
 
   def addSuccess(self, test):
