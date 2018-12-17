5b42e8388c2f0dd1c889d807c60997d168c7f21f
Philip Withnall <withnall@endlessm.com>
2018-12-17 14:32:29 +0000

    goption: Fix an annotation on g_option_context_parse_strv()
    It’s `zero-terminated`, not `null-terminated` (yes, this is esoteric and
    hard to remember).
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

--- glib/goption.c
+++ glib/goption.c
@@ -2659,7 +2659,7 @@ g_option_context_get_description (GOptionContext *context)
 /**
  * g_option_context_parse_strv:
  * @context: a #GOptionContext
- * @arguments: (inout) (array null-terminated=1): a pointer to the
+ * @arguments: (inout) (array zero-terminated=1): a pointer to the
  *    command line arguments (which must be in UTF-8 on Windows)
  * @error: a return location for errors
  *
