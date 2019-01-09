9ba719603a718467cf16470e65d417a9c8a2e9bd
Philip Withnall <withnall@endlessm.com>
2019-01-09 10:47:24 +0000

    ci: Don’t run tests tagged as ‘flaky’ on the CI machines
    This effectively renders those tests useless (since realistically nobody
    runs tests locally), but it’s better than every other CI run failing for
    unrelated reasons. The idea is that the ‘flaky’ tag can be temporarily
    applied to a test while a problem is being investigated or fixed, and
    then removed later.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

--- .gitlab-ci/test-msys2.sh
+++ .gitlab-ci/test-msys2.sh
@@ -51,7 +51,7 @@ ninja
     --output-file "${DIR}/_coverage/${CI_JOB_NAME}-baseline.lcov"
 
 # FIXME: fix the test suite
-meson test --timeout-multiplier ${MESON_TEST_TIMEOUT_MULTIPLIER} || true
+meson test --timeout-multiplier ${MESON_TEST_TIMEOUT_MULTIPLIER} --no-suite flaky || true
 
 "${LCOV}" \
     --quiet \
