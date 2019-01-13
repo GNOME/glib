d8aefba9dfc34b46d2a50e3a9960d2ddf77fa277
Руслан Ижбулатов <lrn1986@gmail.com>
2019-01-13 11:21:04 +0000

    W32: Add a simple exception handler
    Install a Vectored Exception Handler. Its sole purpose is to catch
    some exceptions (access violations, stack overflows and illegal
    instructions - by default, but it can be made to catch any exception
    for which a code is known) and run a debugger in response.
    
    This allows W32 glib applications to be run without a debugger,
    but at the same time allows a debugger to be attached in case
    something happens.
    
    The short list of handleable exceptions is there to ensure that
    this handler won't run a debugger to "handle" utility exceptions,
    such as the one that is used to communicate thread names to a debugger.
    
    The handler is installed to be called last, and shouldn't interfere
    with any user-installed handlers.
    
    There's nothing fancy about the way it runs a debugger (it doesn't even
    support unicode in paths), and it deliberately avoids using glib code.

--- glib/gwin32.c
+++ glib/gwin32.c
@@ -1018,4 +1018,221 @@ g_console_win32_init (void)
     }
 }
 
+static void *WinVEH_handle = NULL;
+
+static gboolean
+subst_pid_and_event (char     *debugger,
+                     gsize     debugger_size,
+                     char     *cmdline,
+                     DWORD     pid,
+                     guintptr  event)
+{
+  gsize i, dbg_i;
+  gboolean replaced_pid = FALSE;
+  gboolean replaced_event = FALSE;
+  char pid_str[30] = {0};
+  gsize pid_str_len;
+  char event_str[30] = {0};
+  gsize event_str_len;
+  i = 0;
+  dbg_i = 0;
+  snprintf (pid_str, G_N_ELEMENTS (pid_str), "%lu", pid);
+  pid_str[29] = 0;
+  pid_str_len = strlen (pid_str);
+  snprintf (event_str, G_N_ELEMENTS (pid_str), "%Iu", event);
+  event_str[29] = 0;
+  event_str_len = strlen (event_str);
+
+  while (cmdline[i] != 0 && dbg_i < debugger_size)
+    {
+      if (cmdline[i] != '%')
+        {
+          debugger[dbg_i++] = cmdline[i++];
+          continue;
+        }
+      if (cmdline[i + 1] == 'p')
+        {
+          if (!replaced_pid)
+            {
+              gsize j = 0;
+              while (j < pid_str_len && dbg_i < debugger_size)
+                debugger[dbg_i++] = pid_str[j++];
+              replaced_pid = TRUE;
+              i += 2;
+            }
+        }
+      else if (cmdline[i + 1] == 'e')
+        {
+          if (!replaced_event)
+            {
+              gsize j = 0;
+              while (j < event_str_len && dbg_i < debugger_size)
+                debugger[dbg_i++] = event_str[j++];
+              replaced_pid = TRUE;
+              i += 2;
+            }
+        }
+      else
+        return FALSE;
+    }
+  if (dbg_i < debugger_size)
+    debugger[dbg_i] = 0;
+  else
+    return FALSE;
+
+  return TRUE;
+}
+
+/**
+ * Handles exceptions (useful for debugging).
+ * Issues a DebugBreak() call if the process is being debugged (not really
+ * useful - if the process is being debugged, this handler won't be invoked
+ * anyway). If it is not, runs a debugger from G_DEBUGGER env var,
+ * substituting first %p in it for PID, and the first %e for the event handle -
+ * that event should be set once the debugger attaches itself (otherwise the
+ * only way out of WaitForSingleObject() is to time out after 1 minute).
+ * For example, G_DEBUGGER can be set to the following command:
+ * gdb.exe -ex "attach %p" -ex "signal-event %e" -ex "bt" -ex "c"
+ * This will make GDB attach to the process, signal the event (GDB must be
+ * recent enough for the signal-event command to be available),
+ * show the backtrace and resume execution, which should make it catch
+ * the exception when Windows re-raises it again.
+ * The command line can't be longer than MAX_PATH (260 characters).
+ *
+ * This function will only stop (and run a debugger) on the following exceptions:
+ * * EXCEPTION_ACCESS_VIOLATION
+ * * EXCEPTION_STACK_OVERFLOW
+ * * EXCEPTION_ILLEGAL_INSTRUCTION
+ * To make it stop at other exceptions one should set the G_VEH_CATCH
+ * environment variable to a list of comma-separated hexademical numbers,
+ * where each number is the code of an exception that should be caught.
+ * This is done to prevent GLib from breaking when Windows uses
+ * exceptions to shuttle information (SetThreadName(), OutputDebugString())
+ * or for control flow.
+ *
+ * This function deliberately avoids calling any GLib code.
+ */
+static LONG __stdcall
+g_win32_veh_handler (PEXCEPTION_POINTERS ExceptionInfo)
+{
+  char                 debugger[MAX_PATH + 1];
+  char                *debugger_env = NULL;
+  char                *catch_list;
+  gboolean             catch = FALSE;
+  STARTUPINFO          si;
+  PROCESS_INFORMATION  pi;
+  HANDLE               event;
+  SECURITY_ATTRIBUTES  sa;
+
+  if (ExceptionInfo == NULL ||
+      ExceptionInfo->ExceptionRecord == NULL)
+    return EXCEPTION_CONTINUE_SEARCH;
+
+  switch (ExceptionInfo->ExceptionRecord->ExceptionCode)
+    {
+    case EXCEPTION_ACCESS_VIOLATION:
+    case EXCEPTION_STACK_OVERFLOW:
+    case EXCEPTION_ILLEGAL_INSTRUCTION:
+      break;
+    default:
+      catch_list = getenv ("G_VEH_CATCH");
+
+      while (!catch &&
+             catch_list != NULL &&
+             catch_list[0] != 0)
+        {
+          unsigned long  catch_code;
+          char          *end;
+          errno = 0;
+          catch_code = strtoul (catch_list, &end, 16);
+          if (errno != NO_ERROR)
+            break;
+          catch_list = end;
+          if (catch_list != NULL && catch_list[0] == ',')
+            catch_list++;
+          if (catch_code == ExceptionInfo->ExceptionRecord->ExceptionCode)
+            catch = TRUE;
+        }
+
+      if (catch)
+        break;
+
+      return EXCEPTION_CONTINUE_SEARCH;
+    }
+
+  if (IsDebuggerPresent ())
+    {
+      DebugBreak ();
+      return EXCEPTION_CONTINUE_EXECUTION;
+    }
+
+  fprintf (stderr,
+           "Exception code=0x%lx flags=0x%lx at 0x%p ",
+           ExceptionInfo->ExceptionRecord->ExceptionCode,
+           ExceptionInfo->ExceptionRecord->ExceptionFlags,
+           ExceptionInfo->ExceptionRecord->ExceptionAddress);
+  debugger_env = getenv ("G_DEBUGGER");
+
+  if (debugger_env == NULL)
+    return EXCEPTION_CONTINUE_SEARCH;
+
+  memset (&si, 0, sizeof (si));
+  memset (&pi, 0, sizeof (pi));
+  memset (&sa, 0, sizeof (sa));
+  si.cb = sizeof (si);
+  sa.nLength = sizeof (sa);
+  sa.bInheritHandle = TRUE;
+  event = CreateEvent (&sa, FALSE, FALSE, NULL);
+
+  if (!subst_pid_and_event (debugger, G_N_ELEMENTS (debugger),
+                            debugger_env, GetCurrentProcessId (),
+                            (guintptr) event))
+    {
+      CloseHandle (event);
+      return EXCEPTION_CONTINUE_SEARCH;
+    }
+
+  debugger[MAX_PATH] = '\0';
+  if (0 != CreateProcessA (NULL,
+                           debugger,
+                           NULL,
+                           NULL,
+                           TRUE,
+                           CREATE_NEW_CONSOLE,
+                           NULL,
+                           NULL,
+                           &si,
+                           &pi))
+    {
+      CloseHandle (pi.hProcess);
+      CloseHandle (pi.hThread);
+      WaitForSingleObject (event, 60000);
+    }
+
+  CloseHandle (event);
+
+  if (IsDebuggerPresent ())
+    return EXCEPTION_CONTINUE_EXECUTION;
+
+  return EXCEPTION_CONTINUE_SEARCH;
+}
+
+void
+g_crash_handler_win32_init ()
+{
+  if (WinVEH_handle != NULL)
+    return;
+
+  WinVEH_handle = AddVectoredExceptionHandler (0, &g_win32_veh_handler);
+}
+
+void
+g_crash_handler_win32_deinit ()
+{
+  if (WinVEH_handle != NULL)
+    RemoveVectoredExceptionHandler (WinVEH_handle);
+
+  WinVEH_handle = NULL;
+}
+
 #endif
