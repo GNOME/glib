69865e3f826d2176b6c0b7fc1842a2cd3551b919
Emmanuel Fleury <emmanuel.fleury@u-bordeaux.fr>
2019-01-31 18:43:59 +0100

    Fixing signedness in glib/giochannel.c
    In file included from glib/glibconfig.h:9,
                     from glib/gtypes.h:32,
                     from glib/gquark.h:32,
                     from glib/gerror.h:28,
                     from glib/gconvert.h:32,
                     from glib/giochannel.h:32,
                     from glib/giochannel.c:37:
    glib/giochannel.c: In function ‘g_io_channel_write_chars’:
    glib/gmacros.h:351:26: error: comparison of integer expressions of different signedness: ‘gsize’ {aka ‘long unsigned int’} and ‘gssize’ {aka ‘long int’} [-Werror=sign-compare]
     #define MIN(a, b)  (((a) < (b)) ? (a) : (b))
                              ^
    glib/giochannel.c:2285:31: note: in expansion of macro ‘MIN’
               gssize write_this = MIN (space_in_buf, count - wrote_bytes);
                                   ^~~
    glib/gmacros.h:351:41: error: operand of ?: changes signedness from ‘gssize’ {aka ‘long int’} to ‘gsize’ {aka ‘long unsigned int’} due to unsignedness of other operand [-Werror=sign-compare]
     #define MIN(a, b)  (((a) < (b)) ? (a) : (b))
                                             ^~~
    glib/giochannel.c:2285:31: note: in expansion of macro ‘MIN’
               gssize write_this = MIN (space_in_buf, count - wrote_bytes);
                                   ^~~
    glib/giochannel.c:2415:41: error: comparison of integer expressions of different signedness: ‘gssize’ {aka ‘long int’} and ‘gsize’ {aka ‘long unsigned int’} [-Werror=sign-compare]
                             g_assert (count == from_buf_len - from_buf_old_len);
                                             ^~
    glib/gmacros.h:455:25: note: in definition of macro ‘G_LIKELY’
     #define G_LIKELY(expr) (expr)
                             ^~~~
    glib/giochannel.c:2415:25: note: in expansion of macro ‘g_assert’
                             g_assert (count == from_buf_len - from_buf_old_len);
                             ^~~~~~~~

--- glib/giochannel.c
+++ glib/giochannel.c
@@ -2282,7 +2282,7 @@ g_io_channel_write_chars (GIOChannel   *channel,
 
       if (!channel->encoding)
         {
-          gssize write_this = MIN (space_in_buf, count - wrote_bytes);
+          gssize write_this = MIN (space_in_buf, (gsize) (count - wrote_bytes));
 
           g_string_append_len (channel->write_buf, buf, write_this);
           buf += write_this;
@@ -2412,7 +2412,7 @@ reconvert:
                          * less than a full character
                          */
 
-                        g_assert (count == from_buf_len - from_buf_old_len);
+                        g_assert (count == (gssize) (from_buf_len - from_buf_old_len));
 
                         channel->partial_write_buf[from_buf_len] = '\0';
 
