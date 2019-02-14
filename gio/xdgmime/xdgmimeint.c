1347044ef424cf04140a95b1013925ec56fbeee9
Emmanuel Fleury <emmanuel.fleury@u-bordeaux.fr>
2019-02-14 22:11:09 +0100

    Fix signedness problem in gio/xdgmime/xdgmimeint.c
    gio/xdgmime/xdgmimeint.c: In function ‘_xdg_binary_or_text_fallback’:
    gio/xdgmime/xdgmimeint.c:195:28: error: comparison of integer expressions of different signedness: ‘int’ and ‘size_t’ {aka ‘long unsigned int’} [-Werror=sign-compare]
       for (i = 0; i < 128 && i < len; ++i)
                                ^

--- gio/xdgmime/xdgmimeint.c
+++ gio/xdgmime/xdgmimeint.c
@@ -189,7 +189,7 @@ const char *
 _xdg_binary_or_text_fallback(const void *data, size_t len)
 {
   unsigned char *chardata;
-  int i;
+  size_t i;
 
   chardata = (unsigned char *) data;
   for (i = 0; i < 128 && i < len; ++i)
