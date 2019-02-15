44156c7b0184d55adaf8cb6dfa8eb0b48c9bcad3
Emmanuel Fleury <emmanuel.fleury@u-bordeaux.fr>
2019-02-15 21:34:16 +0100

    Fix signedness warning in gio/gsocketcontrolmessage.c
    gio/gsocketcontrolmessage.c: In function ‘g_socket_control_message_deserialize’:
    gio/gsocketcontrolmessage.c:189:17: error: comparison of integer expressions of different signedness: ‘int’ and ‘guint’ {aka ‘unsigned int’} [-Werror=sign-compare]
       for (i = 0; i < n_message_types; i++)

--- gio/gsocketcontrolmessage.c
+++ gio/gsocketcontrolmessage.c
@@ -175,7 +175,7 @@ g_socket_control_message_deserialize (int      level,
   GSocketControlMessage *message;
   GType *message_types;
   guint n_message_types;
-  int i;
+  guint i;
 
   /* Ensure we know about the built in types */
 #ifndef G_OS_WIN32
