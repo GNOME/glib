5df0337f5291aa0e9574e4930ba5d424c25e24b2
Руслан Ижбулатов <lrn1986@gmail.com>
2019-03-16 15:58:16 +0000

    GIO: Convert ENOTSOCK to G_IO_ERROR_INVALID_ARGUMENT
    instead of using a generic G_IO_ERROR_FAILED error code.
    This is in line with what W32 part of the code is doing with WSAENOTSOCK.
    
    This fix will break two tests in libsoup, which were written following
    the implementation and thus expect G_IO_ERROR_FAILED when attempting to
    do stuff with no-longer-valid socket descriptors.

--- gio/gioerror.c
+++ gio/gioerror.c
@@ -272,6 +272,12 @@ g_io_error_from_errno (gint err_no)
       break;
 #endif
 
+#ifdef ENOTSOCK
+    case ENOTSOCK:
+      return G_IO_ERROR_INVALID_ARGUMENT;
+      break;
+#endif
+
     default:
       return G_IO_ERROR_FAILED;
       break;
