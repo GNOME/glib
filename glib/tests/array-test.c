0411055144ea8807132aba09edd65ffb1ada17dd
Emmanuel Fleury <emmanuel.fleury@u-bordeaux.fr>
2019-03-19 12:43:10 +0100

    Fix signedness warnings in glib/tests/array-test.c
    glib/tests/array-test.c: In function ‘array_remove_index’:
    glib/tests/array-test.c:338:17: error: comparison of integer expressions of different signedness: ‘gint’ {aka ‘int’} and ‘guint’ {aka ‘unsigned int’} [-Werror=sign-compare]
       for (i = 0; i < garray->len; i++)
                     ^
    glib/tests/array-test.c: In function ‘array_remove_index_fast’:
    glib/tests/array-test.c:375:17: error: comparison of integer expressions of different signedness: ‘gint’ {aka ‘int’} and ‘guint’ {aka ‘unsigned int’} [-Werror=sign-compare]
       for (i = 0; i < garray->len; i++)
                     ^
    glib/tests/array-test.c: In function ‘array_remove_range’:
    glib/tests/array-test.c:412:17: error: comparison of integer expressions of different signedness: ‘gint’ {aka ‘int’} and ‘guint’ {aka ‘unsigned int’} [-Werror=sign-compare]
       for (i = 0; i < garray->len; i++)
                     ^
    glib/tests/array-test.c: In function ‘array_sort’:
    glib/tests/array-test.c:501:17: error: comparison of integer expressions of different signedness: ‘gint’ {aka ‘int’} and ‘guint’ {aka ‘unsigned int’} [-Werror=sign-compare]
       for (i = 0; i < garray->len; i++)
                     ^
    glib/tests/array-test.c: In function ‘array_sort_with_data’:
    glib/tests/array-test.c:533:17: error: comparison of integer expressions of different signedness: ‘gint’ {aka ‘int’} and ‘guint’ {aka ‘unsigned int’} [-Werror=sign-compare]
       for (i = 0; i < garray->len; i++)
                     ^
    glib/tests/array-test.c: In function ‘byte_array_sort’:
    glib/tests/array-test.c:1136:17: error: comparison of integer expressions of different signedness: ‘gint’ {aka ‘int’} and ‘guint’ {aka ‘unsigned int’} [-Werror=sign-compare]
       for (i = 0; i < gbarray->len; i++)
                     ^
    glib/tests/array-test.c: In function ‘byte_array_sort_with_data’:
    glib/tests/array-test.c:1164:17: error: comparison of integer expressions of different signedness: ‘gint’ {aka ‘int’} and ‘guint’ {aka ‘unsigned int’} [-Werror=sign-compare]
       for (i = 0; i < gbarray->len; i++)
                     ^

--- glib/tests/array-test.c
+++ glib/tests/array-test.c
@@ -316,7 +316,7 @@ array_remove_index (gconstpointer test_data)
 {
   const ArrayTestData *config = test_data;
   GArray *garray;
-  gint i;
+  guint i;
   gint prev, cur;
 
   garray = g_array_new (config->zero_terminated, config->clear_, sizeof (gint));
@@ -353,7 +353,7 @@ array_remove_index_fast (gconstpointer test_data)
 {
   const ArrayTestData *config = test_data;
   GArray *garray;
-  gint i;
+  guint i;
   gint prev, cur;
 
   garray = g_array_new (config->zero_terminated, config->clear_, sizeof (gint));
@@ -393,7 +393,7 @@ array_remove_range (gconstpointer test_data)
 {
   const ArrayTestData *config = test_data;
   GArray *garray;
-  gint i;
+  guint i;
   gint prev, cur;
 
   garray = g_array_new (config->zero_terminated, config->clear_, sizeof (gint));
@@ -483,7 +483,7 @@ array_sort (gconstpointer test_data)
 {
   const ArrayTestData *config = test_data;
   GArray *garray;
-  gint i;
+  guint i;
   gint prev, cur;
 
   garray = g_array_new (config->zero_terminated, config->clear_, sizeof (gint));
@@ -515,7 +515,7 @@ array_sort_with_data (gconstpointer test_data)
 {
   const ArrayTestData *config = test_data;
   GArray *garray;
-  gint i;
+  guint i;
   gint prev, cur;
 
   garray = g_array_new (config->zero_terminated, config->clear_, sizeof (gint));
@@ -1119,7 +1119,7 @@ static void
 byte_array_sort (void)
 {
   GByteArray *gbarray;
-  gint i;
+  guint i;
   guint8 val;
   guint8 prev, cur;
 
@@ -1147,7 +1147,7 @@ static void
 byte_array_sort_with_data (void)
 {
   GByteArray *gbarray;
-  gint i;
+  guint i;
   guint8 val;
   guint8 prev, cur;
 
