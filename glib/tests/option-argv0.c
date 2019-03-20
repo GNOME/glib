815c62182f0f58dfade1bf768e019e854e8e6b1a
Руслан Ижбулатов <lrn1986@gmail.com>
2019-03-20 21:19:29 +0000

    Check for /proc/self/cmdline
    Instead of hardcoding /proc/self/cmdline use for __linux__ only,
    do a configure-time test for it.
    
    Specifically, this enables /proc/self/cmdline use on Cygwin.
    
    The configure-time test is very primitive (just tests that the
    file exists and that it's possible to read more than one byte from it),
    relying on the testsuite for more extensive checks.
    
    The test in the testsuite is modified to always run, even on platforms
    where it isn't supposed to pass. If it fails there, the testing framework
    skips it. If the test unexpectedly passes, that is reported too.

--- glib/tests/option-argv0.c
+++ glib/tests/option-argv0.c
@@ -21,13 +21,13 @@
  * Authors: Colin Walters <walters@verbum.org>
  */
 
+#include "config.h"
 #include <glib.h>
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-#if defined __linux || defined __OpenBSD__
 static void
 test_platform_argv0 (void)
 {
@@ -37,18 +37,55 @@ test_platform_argv0 (void)
     { { "test", 't', 0, G_OPTION_ARG_STRING, &arg, NULL, NULL },
       { NULL } };
   gboolean retval;
+  gboolean fatal_errors = TRUE;
+
+#if !defined HAVE_PROC_SELF_CMDLINE && !defined __OpenBSD__
+  fatal_errors = FALSE;
+#endif
 
   context = g_option_context_new (NULL);
   g_option_context_add_main_entries (context, entries, NULL);
   
   retval = g_option_context_parse (context, NULL, NULL, NULL);
-  g_assert (retval == TRUE);
-  g_assert (strcmp (g_get_prgname(), "option-argv0") == 0
-	    || strcmp (g_get_prgname (), "lt-option-argv0") == 0);
+  if (fatal_errors)
+    {
+      g_assert (retval == TRUE);
+      g_assert (g_strcmp0 (g_get_prgname(), "option-argv0") == 0
+		|| g_strcmp0 (g_get_prgname(), "lt-option-argv0") == 0
+#ifdef G_OS_WIN32
+                || g_strcmp0 (g_get_prgname(), "option-argv0.exe") == 0
+#endif
+               );
+    }
+  else
+    {
+      gboolean failed = FALSE;
+
+      if (!retval)
+        {
+          g_print ("g_option_context_parse() failed\n");
+          failed = TRUE;
+        }
+      else if (g_strcmp0 (g_get_prgname(), "option-argv0") != 0 &&
+               g_strcmp0 (g_get_prgname(), "lt-option-argv0") != 0 &&
+#ifdef G_OS_WIN32
+               g_strcmp0 (g_get_prgname(), "option-argv0.exe") != 0
+#else
+               TRUE
+#endif
+              )
+        {
+          g_print ("program name `%s' is neither `option-argv0', nor `lt-option-argv0'\n", g_get_prgname());
+          failed = TRUE;
+        }
+      else
+        g_print ("The test unexpectedly passed\n");
+      if (failed)
+        g_test_skip ("platform_get_argv0() is not implemented [correctly?] on this platform");
+    }
 
   g_option_context_free (context);
 }
-#endif
 
 int
 main (int   argc,
@@ -56,9 +93,7 @@ main (int   argc,
 {
   g_test_init (&argc, &argv, "no_g_set_prgname", NULL);
 
-#if defined __linux || defined __OpenBSD__
   g_test_add_func ("/option/argv0", test_platform_argv0);
-#endif
 
   return g_test_run ();
 }
