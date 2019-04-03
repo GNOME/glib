50b33a724e17fe813c8b1aea84fcd78154aa101c
Nirbheek Chauhan <nirbheek@centricular.com>
2019-04-04 05:12:35 +0530

    gconvert: Don't differentiate between GNU iconv and macOS iconv
    Both provide iconv_open, and in fact, we weren't using the difference
    anywhere in glib at all.

--- glib/gconvert.c
+++ glib/gconvert.c
@@ -52,14 +52,6 @@
 
 #include "glibintl.h"
 
-#if defined(USE_LIBICONV_GNU) && !defined (_LIBICONV_H)
-#error GNU libiconv in use but included iconv.h not from libiconv
-#endif
-#if !defined(USE_LIBICONV_GNU) && defined (_LIBICONV_H) \
-     && !defined (__APPLE_CC__) && !defined (__LP_64__)
-#error GNU libiconv not in use but included iconv.h is from libiconv
-#endif
-
 
 /**
  * SECTION:conversions
