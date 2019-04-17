484d8ccd53eca990d541810dac7856a1f96e3fa6
Emmanuel Fleury <emmanuel.fleury@u-bordeaux.fr>
2019-04-17 09:49:05 +0200

    Fix some documentation issue in glib/gstrfuncs.c
    Apparently, the documentation of g_strcanon() was not really cristal
    clear, so this new code sample try to make it clear the fact that we
    are working on the given string and not a copy. Moreover, it provides
    a way to keep the original string at once.
    
    Fix #29

--- glib/gstrfuncs.c
+++ glib/gstrfuncs.c
@@ -1988,8 +1988,15 @@ g_strncasecmp (const gchar *s1,
  * changed to the @new_delimiter character. Modifies @string in place,
  * and returns @string itself, not a copy. The return value is to
  * allow nesting such as
- * |[<!-- language="C" --> 
+ * |[<!-- language="C" -->
  *   g_ascii_strup (g_strdelimit (str, "abc", '?'))
+ * ]|
+ *
+ * In order to modify a copy, you may use `g_strdup()`:
+ * |[<!-- language="C" -->
+ *   reformatted = g_strdelimit (g_strdup (const_str), "abc", '?');
+ *   ...
+ *   g_free (reformatted);
  * ]|
  *
  * Returns: @string
@@ -2025,8 +2032,15 @@ g_strdelimit (gchar       *string,
  * replaces the character with @substitutor. Modifies @string in place,
  * and return @string itself, not a copy. The return value is to allow
  * nesting such as
- * |[<!-- language="C" --> 
+ * |[<!-- language="C" -->
  *   g_ascii_strup (g_strcanon (str, "abc", '?'))
+ * ]|
+ *
+ * In order to modify a copy, you may use `g_strdup()`:
+ * |[<!-- language="C" -->
+ *   reformatted = g_strcanon (g_strdup (const_str), "abc", '?');
+ *   ...
+ *   g_free (reformatted);
  * ]|
  *
  * Returns: @string
