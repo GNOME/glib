fe7e75bfcb05aed22f5679f76d7d1ee9b18acb1d
Philip Withnall <withnall@endlessm.com>
2019-04-25 11:23:26 +0100

    ci: Add checks for ‘TODO’ in merge requests
    Using the same approach as we have for code style checks (the
    `style-check-diff` CI job), check the diff for any banned keywords like
    ‘TODO’, and also check the commit messages.
    
    The keyword ‘TODO’ is often used by developers to indicate a part of a
    commit which needs further work, and hence which shouldn’t yet be merged.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    
    Fixes: #1551

---
+++ .gitlab-ci/check-todos.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+#
+# Copyright © 2019 Endless Mobile, Inc.
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+#
+# Original author: Philip Withnall
+
+"""
+Checks that a merge request doesn’t add any instances of the string ‘todo’
+(in uppercase), or similar keywords. It may remove instances of that keyword,
+or move them around, according to the logic of `git log -S`.
+"""
+
+import sys
+
+
+# We have to specify these keywords obscurely to avoid the script matching
+# itself. The keyword ‘fixme’ (in upper case) is explicitly allowed because
+# that’s conventionally used as a way of marking a workaround which needs to
+# be merged for now, but is to be grepped for and reverted or reworked later.
+BANNED_KEYWORDS = [
+    'TO' + 'DO',
+    'X' + 'XX',
+    'W' + 'IP',
+]
+
+
+def main():
+    parser = argparse.ArgumentParser(description='TODO')
+    parser.add_argument('commit range',
+                        help='SHA to diff from, or range of commits to diff')
+    args = parser.parse_args()
+
+    banned_words_seen = set()
+
+    # Check the log messages for banned words.
+    log_process = subprocess.run([
+        'git', 'log', '-U0', '--no-color', args.commit_range],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8',
+        check=True)
+    log_lines = log_process.stdout.strip().split('\n')
+
+    for line in log_lines:
+        for keyword in BANNED_KEYWORDS:
+            if keyword in line:
+                banned_words_seen.add(keyword)
+
+    # Check the diff for banned words.
+    diff_process = subprocess.run([
+        'git', 'diff', '-U0', '--no-color', args.commit_range],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8',
+        check=True)
+    diff_lines = diff_process.stdout.strip().split('\n')
+
+    for line in diff_lines:
+        if not line.startswith('+ '):
+            continue
+
+        for keyword in BANNED_KEYWORDS:
+            if keyword in line:
+                banned_words_seen.add(keyword)
+
+    if banned_keywords_seen:
+        print('Saw banned keywords in a commit diff or log message: {}. '
+              'This indicates the branch is a work in progress and should not '
+              'be merged in its current '
+              'form.'.format(', '.join(banned_keywords_seen)))
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
