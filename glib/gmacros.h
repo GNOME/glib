617face8ff3f216216ffc03100d3bc5ee1378f82
Philip Withnall <withnall@endlessm.com>
2019-04-26 13:08:21 +0100

    Revert "macros: Try to use the standard __func__ first in G_STRFUNC"
    This reverts commit 9f75cc9edf1d311ef4bacc3d686c7afa0c4dcbb2.
    
    It breaks usage of G_STRFUNC when compiling applications with
    `-Wpedantic` — `__func__` is not `#define`d, so G_STRFUNC was falling
    through to using `__FUNCTION__`, which raises a warning with
    `-Wpedantic`.
    
    Fun times.
    
    Fixes: #1755

--- glib/gmacros.h
+++ glib/gmacros.h
@@ -733,10 +733,10 @@
 #endif
 
 /* Provide a string identifying the current function, non-concatenatable */
-#if defined (__func__)
-#define G_STRFUNC     ((const char*) (__func__))
-#elif defined (__GNUC__) && defined (__cplusplus)
+#if defined (__GNUC__) && defined (__cplusplus)
 #define G_STRFUNC     ((const char*) (__PRETTY_FUNCTION__))
+#elif defined (__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
+#define G_STRFUNC     ((const char*) (__func__))
 #elif defined (__GNUC__) || (defined(_MSC_VER) && (_MSC_VER > 1300))
 #define G_STRFUNC     ((const char*) (__FUNCTION__))
 #else
