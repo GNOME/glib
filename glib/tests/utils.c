0135842f89bc1f76b76db2e909f8dbc881b50646
Patrick Storz <eduard.braun2@gmx.de>
2019-05-09 02:15:54 +0200

    Apply style fixes

--- glib/tests/utils.c
+++ glib/tests/utils.c
@@ -347,8 +347,8 @@ test_console_charset (void)
 #ifdef G_OS_WIN32
   /* set console output encoding to UTF-8;
      this should never match the 8-bit codepage returned by g_get_charset */
-  const unsigned int initial_cp = GetConsoleOutputCP();
-  SetConsoleOutputCP(CP_UTF8);
+  const unsigned int initial_cp = GetConsoleOutputCP ();
+  SetConsoleOutputCP (CP_UTF8);
 #endif
 
   g_get_charset (&c1);
@@ -361,7 +361,7 @@ test_console_charset (void)
 #endif
 
 #ifdef G_OS_WIN32
-    SetConsoleOutputCP(initial_cp);
+  SetConsoleOutputCP (initial_cp);
 #endif
 }
 
