da1afcb4b9ca85c2814b6cb6669e94f27755e868
Philip Withnall <withnall@endlessm.com>
2019-06-03 17:59:58 +0100

    WIP
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

--- .gitlab-ci/run-tests.sh
+++ .gitlab-ci/run-tests.sh
@@ -6,7 +6,7 @@ meson test \
         -C _build \
         --timeout-multiplier ${MESON_TEST_TIMEOUT_MULTIPLIER} \
         --no-suite flaky \
-        --gdb \
+        --wrapper gdb \
         contenttype
 
 exit_code=$?
