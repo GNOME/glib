4d2031440ec11dd7e647f7d262a40450b64c3270
Chun-wei Fan <fanchunwei@src.gnome.org>
2019-06-24 11:42:14 +0800

    gobject/tests/signals.c: Fix tests on Windows
    On Windows and possibly other platforms the '%p' printf modifier does
    not prefix printed values with '0x', so do not expect the warning
    message to contain the '0x' prefix for the handler pointer value.

--- gobject/tests/signals.c
+++ gobject/tests/signals.c
@@ -1267,7 +1267,7 @@ test_clear_signal_handler (void)
     {
       handler = g_random_int_range (0x01, 0xFF);
       g_test_expect_message (G_LOG_DOMAIN, G_LOG_LEVEL_WARNING,
-                             "*instance '0x* has no handler with id *'");
+                             "*instance '* has no handler with id *'");
       g_clear_signal_handler (&handler, test_obj);
       g_assert_cmpuint (handler, ==, 0);
       g_test_assert_expected_messages ();
