bf1f5fe3a8b003f006d7d417ab32939add1bacc6
Christoph Reiter <reiter.christoph@gmail.com>
2019-08-10 19:55:53 +0200

    win32: don't assume the format specifier for the stdlib printf/scanf like functions
    When using the mingw printf shims for C99 compat the msvc format specifiers don't work
    and the build fails.
    
    Ideally we would use glib functions which abstract this away, but in the error handler context
    we shouldn't call back into glib. And for scanf we don't have a glib wrapper.
    
    Instead call the "secure" versions provided by the win32 API (_snprintf_s/fprintf_s/sscanf_s)
    which mingw doesn't replace.

--- glib/gwin32-private.c
+++ glib/gwin32-private.c
@@ -40,10 +40,10 @@ _g_win32_subst_pid_and_event (char       *debugger,
   char event_str[STR_BUFFER_SIZE] = {0};
   gsize event_str_len;
 #undef STR_BUFFER_SIZE
-  snprintf (pid_str, G_N_ELEMENTS (pid_str), "%lu", pid);
+  _snprintf_s (pid_str, G_N_ELEMENTS (pid_str), "%lu", pid);
   pid_str[G_N_ELEMENTS (pid_str) - 1] = 0;
   pid_str_len = strlen (pid_str);
-  snprintf (event_str, G_N_ELEMENTS (pid_str), "%Iu", event);
+  _snprintf_s (event_str, G_N_ELEMENTS (pid_str), "%Iu", event);
   event_str[G_N_ELEMENTS (pid_str) - 1] = 0;
   event_str_len = strlen (event_str);
 
