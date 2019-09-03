fde012404e82b36108508125e5b973418d0d2d00
Emmanuel Fleury <emmanuel.fleury@u-bordeaux.fr>
2019-09-03 20:41:57 +0200

    Deprecate G_GNUC_NORETURN in favor of G_NORETURN
    Closes issue #994

--- glib/gmacros.h
+++ glib/gmacros.h
@@ -507,7 +507,7 @@
 #define G_GNUC_FORMAT( arg_idx )                \
   __attribute__((__format_arg__ (arg_idx)))
 #define G_GNUC_NORETURN                         \
-  __attribute__((__noreturn__))
+  __attribute__((__noreturn__)) GLIB_DEPRECATED_MACRO_IN_2_64_FOR(G_NORETURN)
 #define G_GNUC_CONST                            \
   __attribute__((__const__))
 #define G_GNUC_UNUSED                           \
@@ -928,27 +928,32 @@
  *
  * Since: 2.64
  */
-GLIB_AVAILABLE_MACRO_IN_2_64
 #if (3 <= __GNUC__ || (__GNUC__ == 2 && 8 <= __GNUC_MINOR__)) || (0x5110 <= __SUNPRO_C)
   /* For compatibility with G_NORETURN_FUNCPTR on clang, use
      __attribute__((__noreturn__)), not _Noreturn.  */
-# define G_NORETURN __attribute__ ((__noreturn__))
+# define G_NORETURN __attribute__ ((__noreturn__))      \
+  GLIB_AVAILABLE_MACRO_IN_2_64
 #elif 1200 <= _MSC_VER
   /* Use MSVC specific syntax.  */
-# define G_NORETURN __declspec (noreturn)
+# define G_NORETURN __declspec (noreturn)       \
+  GLIB_AVAILABLE_MACRO_IN_2_64
 #elif defined __cplusplus
   /* Use ISO C++11 syntax when the compiler supports it.  */
 # if (__cplusplus >= 201103 && !(__GNUC__ == 4 && __GNUC_MINOR__ == 7)) || (_MSC_VER >= 1900)
-#  define G_NORETURN [[noreturn]]
+#  define G_NORETURN [[noreturn]]               \
+  GLIB_AVAILABLE_MACRO_IN_2_64
 # else
-#  define G_NORETURN /* empty */
+#  define G_NORETURN /* empty */                \
+  GLIB_AVAILABLE_MACRO_IN_2_64
 # endif
 #else
   /* Use ISO C11 syntax when the compiler supports it.  */
 # if __STDC_VERSION__ >= 201112 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 7)
-#  define G_NORETURN _Noreturn
+#  define G_NORETURN _Noreturn                  \
+  GLIB_AVAILABLE_MACRO_IN_2_64
 # else
-#  define G_NORETURN /* empty */
+#  define G_NORETURN /* empty */                \
+  GLIB_AVAILABLE_MACRO_IN_2_64
 # endif
 #endif
 
@@ -967,11 +972,12 @@ GLIB_AVAILABLE_MACRO_IN_2_64
  *
  * Since: 2.64
  */
-GLIB_AVAILABLE_MACRO_IN_2_64
 #if (3 <= __GNUC__ || (__GNUC__ == 2 && 8 <= __GNUC_MINOR__)) || (0x5110 <= __SUNPRO_C)
-# define G_NORETURN_FUNCPTR __attribute__ ((__noreturn__))
+# define G_NORETURN_FUNCPTR __attribute__ ((__noreturn__))      \
+  GLIB_AVAILABLE_MACRO_IN_2_64
 #else
-# define G_NORETURN_FUNCPTR /* empty */
+# define G_NORETURN_FUNCPTR /* empty */         \
+  GLIB_AVAILABLE_MACRO_IN_2_64
 #endif
 
 /*
