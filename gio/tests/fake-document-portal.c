5a68945b18f9d74e49f215d3ea3e10c1aead5dd8
James Henstridge <james@jamesh.id.au>
2019-09-16 14:04:30 +0200

    gio/tests: add a fake implementation of the document portal

---
+++ gio/tests/fake-document-portal.c
@@ -0,0 +1,117 @@
+/* A stub implementation of xdg-document-portal covering enough to
+ * support g_document_portal_add_documents */
+
+#include "fake-document-portal-generated.h"
+
+static gboolean
+on_handle_get_mount_point (FakeDocuments         *object,
+                           GDBusMethodInvocation *invocation,
+                           gpointer               user_data)
+{
+  fake_documents_complete_get_mount_point (object,
+                                           invocation,
+                                           "/document-portal");
+  return TRUE;
+}
+
+static gboolean
+on_handle_add_full (FakeDocuments         *object,
+                    GDBusMethodInvocation *invocation,
+                    GVariant              *o_path_fds,
+                    guint                  flags,
+                    const gchar           *app_id,
+                    const gchar           *permissions,
+                    gpointer               user_data)
+{
+  const gchar **doc_ids = NULL;
+  GVariant *extra_out = NULL;
+  gsize length, i;
+
+  length = g_variant_get_size (o_path_fds);
+  doc_ids = g_new0(const gchar *, length);
+  for (i = 0; i < length; i++)
+    {
+      doc_ids[i] = "document-id";
+    }
+  extra_out = g_variant_new_array (G_VARIANT_TYPE ("{sv}"), NULL, 0);
+
+  fake_documents_complete_add_full (object,
+                                    invocation,
+                                    NULL,
+                                    doc_ids,
+                                    extra_out);
+
+  g_free (doc_ids);
+
+  return TRUE;
+}
+
+static void
+on_bus_acquired (GDBusConnection *connection,
+                 const gchar     *name,
+                 gpointer         user_data)
+{
+  FakeDocuments *interface;
+  GError *error = NULL;
+
+  g_print ("Acquired a message bus connection\n");
+
+  interface = fake_documents_skeleton_new ();
+  g_signal_connect (interface,
+                    "handle-get-mount-point",
+                    G_CALLBACK (on_handle_get_mount_point),
+                    NULL);
+  g_signal_connect (interface,
+                    "handle-add-full",
+                    G_CALLBACK (on_handle_add_full),
+                    NULL);
+
+  g_dbus_interface_skeleton_export (G_DBUS_INTERFACE_SKELETON (interface),
+                                    connection,
+                                    "/org/freedesktop/portal/documents",
+                                    &error);
+  g_assert_no_error (error);
+}
+
+static void
+on_name_acquired (GDBusConnection *connection,
+                  const gchar     *name,
+                  gpointer         user_data)
+{
+  g_print ("Acquired the name %s\n", name);
+}
+
+static void
+on_name_lost (GDBusConnection *connection,
+              const gchar     *name,
+              gpointer         user_data)
+{
+  g_print ("Lost the name %s\n", name);
+}
+
+
+gint
+main (gint argc, gchar *argv[])
+{
+  GMainLoop *loop;
+  guint id;
+
+  loop = g_main_loop_new (NULL, FALSE);
+
+  id = g_bus_own_name (G_BUS_TYPE_SESSION,
+                       "org.freedesktop.portal.Documents",
+                       G_BUS_NAME_OWNER_FLAGS_ALLOW_REPLACEMENT |
+                       G_BUS_NAME_OWNER_FLAGS_REPLACE,
+                       on_bus_acquired,
+                       on_name_acquired,
+                       on_name_lost,
+                       loop,
+                       NULL);
+
+  g_main_loop_run (loop);
+
+  g_bus_unown_name (id);
+  g_main_loop_unref (loop);
+
+  return 0;
+}
