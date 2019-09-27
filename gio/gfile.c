cb70f391805bd230842ab8319b472a80292bcedf
Philip Withnall <withnall@endlessm.com>
2019-09-27 17:25:56 +0100

    gfile: Use a more specific error message if symlinks are not supported
    The string is already translated in `GLocalFile`, so this doesn’t
    introduce a new translatable string.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

--- gio/gfile.c
+++ gio/gfile.c
@@ -4046,7 +4046,7 @@ g_file_make_symbolic_link (GFile         *file,
     {
       g_set_error_literal (error, G_IO_ERROR,
                            G_IO_ERROR_NOT_SUPPORTED,
-                           _("Operation not supported"));
+                           _("Symbolic links not supported"));
       return FALSE;
     }
 
