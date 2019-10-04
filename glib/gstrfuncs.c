3f73dad07460bd091711986f8819981c5f16b4e6
Daniel P. Berrangé <berrange@redhat.com>
2019-10-04 15:04:01 +0100

    glib: add parameter annotations for g_vasprintf and callers
    Document that g_vasprintf and g_strdup_printf are guaranteed to return a
    non-NULL string, unless the format string contains the locale sensitive
    conversions %lc or %ls.
    
    Further annotate that the output parameter for g_vasprintf and the
    format string for all functions must be non-NULL.
    
    Fixes #1622
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- glib/gstrfuncs.c
+++ glib/gstrfuncs.c
@@ -491,7 +491,7 @@ g_stpcpy (gchar       *dest,
 
 /**
  * g_strdup_vprintf:
- * @format: a standard printf() format string, but notice
+ * @format: (not nullable): a standard printf() format string, but notice
  *     [string precision pitfalls][string-precision]
  * @args: the list of parameters to insert into the format string
  *
@@ -499,6 +499,10 @@ g_stpcpy (gchar       *dest,
  * calculates the maximum space required and allocates memory to hold
  * the result. The returned string should be freed with g_free() when
  * no longer needed.
+ *
+ * The returned string is guaranteed to be non-NULL, unless @format
+ * contains `%lc` or `%ls` conversions, which can fail if no multibyte
+ * representation is available for the given character.
  *
  * See also g_vasprintf(), which offers the same functionality, but
  * additionally returns the length of the allocated string.
@@ -518,7 +522,7 @@ g_strdup_vprintf (const gchar *format,
 
 /**
  * g_strdup_printf:
- * @format: a standard printf() format string, but notice
+ * @format: (not nullable): a standard printf() format string, but notice
  *     [string precision pitfalls][string-precision]
  * @...: the parameters to insert into the format string
  *
@@ -526,6 +530,10 @@ g_strdup_vprintf (const gchar *format,
  * calculates the maximum space required and allocates memory to hold
  * the result. The returned string should be freed with g_free() when no
  * longer needed.
+ *
+ * The returned string is guaranteed to be non-NULL, unless @format
+ * contains `%lc` or `%ls` conversions, which can fail if no multibyte
+ * representation is available for the given character.
  *
  * Returns: a newly-allocated string holding the result
  */
