6b5dba4462d1dd66a059b61f725bfcae3e1060e8
Philip Withnall <withnall@endlessm.com>
2019-10-28 19:42:41 +0000

    tests: Isolate directories in gdbus-peer test
    So that the tests all end up using separate `.dbus-keyring` directories,
    and hence not racing to create and acquire lock files, use
    `G_TEST_OPTION_ISOLATE_DIRS` to ensure they all run in separate
    disposable directories.
    
    This has the added benefit of meaning they don’t touch the developer’s
    actual `$HOME` directory.
    
    This reduces the false-failure rate of `gdbus-peer` by a factor of 9 for
    me on my local machine.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    
    Fixes: #1912

--- gio/tests/gdbus-peer.c
+++ gio/tests/gdbus-peer.c
@@ -1922,7 +1922,7 @@ main (int   argc,
   gint ret;
   GDBusNodeInfo *introspection_data = NULL;
 
-  g_test_init (&argc, &argv, NULL);
+  g_test_init (&argc, &argv, G_TEST_OPTION_ISOLATE_DIRS, NULL);
 
   introspection_data = g_dbus_node_info_new_for_xml (test_interface_introspection_xml, NULL);
   g_assert (introspection_data != NULL);
