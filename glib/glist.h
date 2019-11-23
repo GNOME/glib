79549f6dfe5faf1cf65ebc66b8f41b02519e62dc
Ernestas Kulik <ekulik@redhat.com>
2019-11-23 17:41:54 +0100

    list: Add g_clear_list()
    Although not quite as often-occurring, this should help with constructs
    like this:
    
      if (list)
        {
          g_list_free_full (list, foo);
          list = NULL;
        }
    
    Closes https://gitlab.gnome.org/GNOME/glib/issues/1943

--- glib/glist.h
+++ glib/glist.h
@@ -147,6 +147,10 @@ GLIB_AVAILABLE_IN_ALL
 gpointer g_list_nth_data                (GList            *list,
 					 guint             n);
 
+GLIB_AVAILABLE_IN_2_64
+void     g_clear_list                   (GList            **list_ptr,
+                                         GDestroyNotify     destroy);
+
 
 #define g_list_previous(list)	        ((list) ? (((GList *)(list))->prev) : NULL)
 #define g_list_next(list)	        ((list) ? (((GList *)(list))->next) : NULL)
