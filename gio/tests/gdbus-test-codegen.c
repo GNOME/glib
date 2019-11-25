cbff136931add997119ae05adb6908df510e6781
Matthew Leeds <matthew.leeds@endlessm.com>
2019-11-25 11:51:13 -0800

    gdbus-codegen: Add call_flags and timeout_msec args
    Currently the code generated by gdbus-codegen uses
    G_DBUS_CALL_FLAGS_NONE in its D-Bus calls, which occur for each method
    defined by the input XML, and for proxy_set_property functions. This
    means that if the daemon which implements the methods checks for
    G_DBUS_FLAGS_ALLOW_INTERACTIVE_AUTHORIZATION and only does interactive
    authorization if that flag is present, users of the generated code have
    no way to cause the daemon to use interactive authorization (e.g. polkit
    dialogs).
    
    If we simply changed the generated code to always use
    G_DBUS_FLAGS_ALLOW_INTERACTIVE_AUTHORIZATION, its users would have no
    way to disallow interactive authorization (except for manually calling
    the D-Bus method themselves).
    
    So instead, this commit adds a GDBusCallFlags argument to method call
    functions. Since this is an API break which will require changes in
    projects using gdbus-codegen code, the change is conditional on the
    command line argument --glib-min-version having the value 2.64 or
    higher.
    
    The impetus for this change is that I'm changing accountsservice to
    properly respect G_DBUS_FLAGS_ALLOW_INTERACTIVE_AUTHORIZATION, and
    libaccountsservice uses generated code for D-Bus method calls. So
    these changes will allow libaccountsservice to continue allowing
    interactive authorization, and avoid breaking any users of it which
    expect that. See
    https://gitlab.freedesktop.org/accountsservice/accountsservice/merge_requests/46
    
    It might make sense to also let GDBusCallFlags be specified for property
    set operations, but that is not needed in the case of accountsservice,
    and would require significant work and breaking API in multiple places.
    
    Similarly, the generated code currently hard codes -1 as the timeout
    value when calling g_dbus_proxy_call*(). Add a timeout_msec argument so
    the user of the generated code can specify the timeout as well.
    
    Also, test this new API. In gio/tests/codegen.py we test that the new
    arguments are generated if and only of --glib-min-version is used with a
    value greater than or equal to 2.64, and in gio/tests/meson.build we
    test that the generated code with the new API can be linked against.

--- gio/tests/gdbus-test-codegen.c
+++ gio/tests/gdbus-test-codegen.c
@@ -25,7 +25,12 @@
 
 #include "gdbus-tests.h"
 
+#ifdef GLIB_MIN_VERSION_2_64
+#include "gdbus-test-codegen-generated-min-version-2-64.h"
+#else
 #include "gdbus-test-codegen-generated.h"
+#endif
+
 #include "gdbus-test-codegen-generated-interface-info.h"
 
 /* ---------------------------------------------------------------------------------------------------- */
@@ -878,6 +883,10 @@ check_bar_proxy (FooiGenBar *proxy,
                                                      "/a/path",
                                                      "asig",
                                                      "bytestring\xff",
+#ifdef GLIB_MIN_VERSION_2_64
+                                                     G_DBUS_CALL_FLAGS_NONE,
+                                                     -1,
+#endif
                                                      &ret_val_byte,
                                                      &ret_val_boolean,
                                                      &ret_val_int16,
@@ -913,6 +922,10 @@ check_bar_proxy (FooiGenBar *proxy,
                                                          array_of_objpaths,
                                                          array_of_signatures,
                                                          array_of_bytestrings,
+#ifdef GLIB_MIN_VERSION_2_64
+                                                         G_DBUS_CALL_FLAGS_NONE,
+                                                         -1,
+#endif
                                                          &ret_array_of_strings,
                                                          &ret_array_of_objpaths,
                                                          &ret_array_of_signatures,
@@ -946,7 +959,11 @@ check_bar_proxy (FooiGenBar *proxy,
    * unimplemented methods.
    */
   error = NULL;
+#ifdef GLIB_MIN_VERSION_2_64
+  ret = foo_igen_bar_call_unimplemented_method_sync (proxy, G_DBUS_CALL_FLAGS_NONE, -1, NULL /* GCancellable */, &error);
+#else
   ret = foo_igen_bar_call_unimplemented_method_sync (proxy, NULL /* GCancellable */, &error);
+#endif
   g_assert_error (error, G_DBUS_ERROR, G_DBUS_ERROR_UNKNOWN_METHOD);
   g_error_free (error);
   error = NULL;
@@ -957,7 +974,11 @@ check_bar_proxy (FooiGenBar *proxy,
                     G_CALLBACK (on_test_signal),
                     data);
   error = NULL;
+#ifdef GLIB_MIN_VERSION_2_64
+  ret = foo_igen_bar_call_request_signal_emission_sync (proxy, 0, G_DBUS_CALL_FLAGS_NONE, -1, NULL, &error);
+#else
   ret = foo_igen_bar_call_request_signal_emission_sync (proxy, 0, NULL, &error);
+#endif
   g_assert_no_error (error);
   g_assert (ret);
 
@@ -1011,7 +1032,11 @@ check_bar_proxy (FooiGenBar *proxy,
                     G_CALLBACK (on_g_properties_changed),
                     data);
   error = NULL;
+#ifdef GLIB_MIN_VERSION_2_64
+  ret = foo_igen_bar_call_request_multi_property_mods_sync (proxy, G_DBUS_CALL_FLAGS_NONE, -1, NULL, &error);
+#else
   ret = foo_igen_bar_call_request_multi_property_mods_sync (proxy, NULL, &error);
+#endif
   g_assert_no_error (error);
   g_assert (ret);
   g_main_loop_run (thread_loop);
@@ -1077,6 +1102,10 @@ check_bar_proxy (FooiGenBar *proxy,
    */
   error = NULL;
   foo_igen_bar_call_property_cancellation (proxy,
+#ifdef GLIB_MIN_VERSION_2_64
+                                           G_DBUS_CALL_FLAGS_NONE,
+                                           -1,
+#endif
                                            NULL, /* GCancellable */
                                            (GAsyncReadyCallback) on_property_cancellation_cb,
                                            data);
@@ -1159,6 +1188,10 @@ check_bat_proxy (FooiGenBat *proxy,
                                        g_variant_new_string ("a string"),
                                        g_variant_new_bytestring ("a bytestring\xff"),
                                        g_variant_new ("(i)", 4200),
+#ifdef GLIB_MIN_VERSION_2_64
+                                       G_DBUS_CALL_FLAGS_NONE,
+                                       -1,
+#endif
                                        &ret_i,
                                        &ret_s,
                                        &ret_ay,
@@ -1191,18 +1224,30 @@ check_authorize_proxy (FooiGenAuthorize *proxy,
   /* Check that g-authorize-method works as intended */
 
   error = NULL;
+#ifdef GLIB_MIN_VERSION_2_64
+  ret = foo_igen_authorize_call_check_not_authorized_sync (proxy, G_DBUS_CALL_FLAGS_NONE, -1, NULL, &error);
+#else
   ret = foo_igen_authorize_call_check_not_authorized_sync (proxy, NULL, &error);
+#endif
   g_assert_error (error, G_IO_ERROR, G_IO_ERROR_PERMISSION_DENIED);
   g_error_free (error);
   g_assert (!ret);
 
   error = NULL;
+#ifdef GLIB_MIN_VERSION_2_64
+  ret = foo_igen_authorize_call_check_authorized_sync (proxy, G_DBUS_CALL_FLAGS_NONE, -1, NULL, &error);
+#else
   ret = foo_igen_authorize_call_check_authorized_sync (proxy, NULL, &error);
+#endif
   g_assert_no_error (error);
   g_assert (ret);
 
   error = NULL;
+#ifdef GLIB_MIN_VERSION_2_64
+  ret = foo_igen_authorize_call_check_not_authorized_from_object_sync (proxy, G_DBUS_CALL_FLAGS_NONE, -1, NULL, &error);
+#else
   ret = foo_igen_authorize_call_check_not_authorized_from_object_sync (proxy, NULL, &error);
+#endif
   g_assert_error (error, G_IO_ERROR, G_IO_ERROR_PENDING);
   g_error_free (error);
   g_assert (!ret);
@@ -1219,7 +1264,11 @@ get_self_via_proxy (FooiGenMethodThreads *proxy_1)
   gpointer self;
 
   error = NULL;
+#ifdef GLIB_MIN_VERSION_2_64
+  ret = foo_igen_method_threads_call_get_self_sync (proxy_1, &self_str, G_DBUS_CALL_FLAGS_NONE, -1, NULL, &error);
+#else
   ret = foo_igen_method_threads_call_get_self_sync (proxy_1, &self_str, NULL, &error);
+#endif
   g_assert_no_error (error);
   g_assert (ret);
 
