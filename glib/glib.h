44332521d08d7dfa052bc882e439fae774e2a311
Philip Withnall <withnall@endlessm.com>
2019-12-11 11:41:39 +0000

    glib: Sort #includes in top-level header files
    This should shut the code style checker up every time someone adds a new
    `#include`.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

--- glib/glib.h
+++ glib/glib.h
@@ -80,8 +80,8 @@
 #include <glib/gslist.h>
 #include <glib/gspawn.h>
 #include <glib/gstrfuncs.h>
-#include <glib/gstring.h>
 #include <glib/gstringchunk.h>
+#include <glib/gstring.h>
 #include <glib/gtestutils.h>
 #include <glib/gthread.h>
 #include <glib/gthreadpool.h>
@@ -94,10 +94,11 @@
 #include <glib/gurifuncs.h>
 #include <glib/gutils.h>
 #include <glib/guuid.h>
-#include <glib/gvarianttype.h>
 #include <glib/gvariant.h>
+#include <glib/gvarianttype.h>
 #include <glib/gversion.h>
 #include <glib/gversionmacros.h>
+
 #ifdef G_PLATFORM_WIN32
 #include <glib/gwin32.h>
 #endif
