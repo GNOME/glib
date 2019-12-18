cdaf9186c0c78fb6f20edfe0a5b783a849df730b
Bastien Nocera <hadess@hadess.net>
2019-12-18 12:52:27 +0100

    docs: Add full code example for GMemoryMonitor

--- gio/gmemorymonitor.c
+++ gio/gmemorymonitor.c
@@ -50,6 +50,29 @@
  *
  * See #GMemoryMonitorWarningLevel for details on the various warning levels.
  *
+ * |[<!-- language="C" -->
+ * static void
+ * warning_cb (GMemoryMonitor *m, GMemoryMonitorWarningLevel level)
+ * {
+ *   g_debug ("Warning level: %d", level);
+ *   if (warning_level > G_MEMORY_MONITOR_WARNING_LEVEL_LOW)
+ *     drop_caches ();
+ * }
+ *
+ * static GMemoryMonitor *
+ * monitor_low_memory (void)
+ * {
+ *   GMemoryMonitor *m;
+ *   m = g_memory_monitor_dup_default ();
+ *   g_signal_connect (G_OBJECT (m), "low-memory-warning",
+ *                     G_CALLBACK (warning_cb), NULL);
+ *   return m;
+ * }
+ * ]|
+ *
+ * Don't forget to disconnect the #GMemoryMonitor::low-memory-warning
+ * signal, and unref the #GMemoryMonitor itself when exiting.
+ *
  * Since: 2.64
  */
 
