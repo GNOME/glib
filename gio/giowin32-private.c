ce2032c0c191c422790e13e835554a68c5ed1cc1
LRN <lrn1986@gmail.com>
2019-12-20 09:50:27 +0000

    Add a licensing header for giowin32-private.c

--- gio/giowin32-private.c
+++ gio/giowin32-private.c
@@ -1,3 +1,22 @@
+/* giowin32-private.c - private glib-gio functions for W32 GAppInfo
+ *
+ * Copyright 2019 Руслан Ижбулатов
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 static gssize
 g_utf16_len (const gunichar2 *str)
 {
