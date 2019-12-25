8f6e216827b3f1d879addfe4508bd5908beeb756
Krzesimir Nowak <qdlacz@gmail.com>
2019-12-25 13:07:32 +0100

    gerror: Add support for extended errors
    This commit adds a G_DEFINE_ERROR_TYPE macro and
    g_error_domain_register() functions to register extended error
    domains. This feature uses tagged quarks to avoid the overhead of
    locking when the domain is not an extended one (which is likely going
    to be a majority of cases).

--- glib/tests/error.c
+++ glib/tests/error.c
@@ -82,6 +82,92 @@ test_copy (void)
   g_error_free (copy);
 }
 
+typedef struct
+{
+  int copy_called;
+  int free_called;
+} TestErrorCheck;
+
+typedef struct
+{
+  GError error;
+
+  int foo;
+  TestErrorCheck *check;
+} TestErrorInstance;
+
+static void
+test_error_copy (const GError *src_error, GError *dest_error)
+{
+  const TestErrorInstance *src_test_error = (const TestErrorInstance *) src_error;
+  TestErrorInstance *dest_test_error = (TestErrorInstance *) dest_error;
+
+  dest_test_error->foo = src_test_error->foo;
+  dest_test_error->check = src_test_error->check;
+
+  dest_test_error->check->copy_called++;
+}
+
+static void
+test_error_cleanup (GError *error)
+{
+  TestErrorInstance *test_error = (TestErrorInstance *) error;
+
+  test_error->check->free_called++;
+}
+
+static TestErrorInstance *
+fill_test_error (GError *error, TestErrorCheck *check)
+{
+  TestErrorInstance *test_error = (TestErrorInstance *) error;
+
+  test_error->foo = 42;
+  test_error->check = check;
+
+  return test_error;
+}
+
+GQuark test_error_quark (void);
+#define TEST_ERROR_DOMAIN (test_error_quark ())
+G_DEFINE_ERROR_TYPE (TestError, test_error)
+
+static TestErrorInstance *
+test_error_new (TestErrorCheck *check)
+{
+  GError *error = g_error_new_literal (TEST_ERROR_DOMAIN, 0, "foo");
+
+  return fill_test_error (error, check);
+}
+
+static void
+test_extended (void)
+{
+  TestErrorCheck check = { 0, 0 };
+  TestErrorInstance *error = test_error_new (&check);
+  GError *copy;
+  TestErrorInstance *copy_test_error;
+
+  g_assert_cmpint (check.copy_called, ==, 0);
+  g_assert_cmpint (check.free_called, ==, 0);
+
+  g_assert_cmpuint (error->error.domain, ==, TEST_ERROR_DOMAIN);
+
+  copy = g_error_copy ((GError *) error);
+  g_assert_cmpint (check.copy_called, ==, 1);
+
+  g_assert_cmpuint (error->error.domain, ==, copy->domain);
+  g_assert_cmpint (error->error.code, ==, copy->code);
+  g_assert_cmpstr (error->error.message, ==, copy->message);
+
+  copy_test_error = (TestErrorInstance *) copy;
+  g_assert_cmpint (error->foo, ==, copy_test_error->foo);
+
+  g_error_free ((GError *) error);
+  g_error_free (copy);
+
+  g_assert_cmpint (check.free_called, ==, 2);
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -91,6 +177,7 @@ main (int argc, char *argv[])
   g_test_add_func ("/error/prefix", test_prefix);
   g_test_add_func ("/error/literal", test_literal);
   g_test_add_func ("/error/copy", test_copy);
+  g_test_add_func ("/error/extended", test_extended);
 
   return g_test_run ();
 }
