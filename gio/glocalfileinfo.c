467d03e566fbd42864cc1de83c23a2606c5523d8
Andre Miranda <andreldm@xfce.org>
2020-01-07 01:46:57 -0300

    glocalfileinfo: Add support for stx_btime
    Provide the Creation timestamp as G_FILE_ATTRIBUTE_TIME_CREATED on
    systems running Linux 4.15+ where stx_btime is available via the staxt
    syscall.
    
    Fixes #1970.

--- gio/glocalfileinfo.c
+++ gio/glocalfileinfo.c
@@ -1068,6 +1068,18 @@ set_info_from_stat (GFileInfo             *info,
     }
 }
 
+#ifdef HAVE_STATX
+
+static void
+set_info_from_statx (GFileInfo    *info,
+                     struct statx *statxbuf)
+{
+  _g_file_info_set_attribute_uint64_by_id (info, G_FILE_ATTRIBUTE_ID_TIME_CREATED, statxbuf->stx_btime.tv_sec);
+  _g_file_info_set_attribute_uint64_by_id (info, G_FILE_ATTRIBUTE_ID_TIME_CREATED_USEC, statxbuf->stx_btime.tv_nsec / 1000);
+}
+
+#endif
+
 #ifndef G_OS_WIN32
 
 static char *
@@ -1760,6 +1772,10 @@ _g_local_file_info_get (const char             *basename,
 #elif defined (G_OS_WIN32)
   GWin32PrivateStat statbuf2;
 #endif
+#ifdef HAVE_STATX
+  struct statx statxbuf;
+  int statx_flags;
+#endif
   int res;
   gboolean stat_ok;
   gboolean is_symlink, symlink_broken;
@@ -1850,7 +1866,21 @@ _g_local_file_info_get (const char             *basename,
     }
 
   if (stat_ok)
-    set_info_from_stat (info, &statbuf, attribute_matcher);
+    {
+      set_info_from_stat (info, &statbuf, attribute_matcher);
+
+#ifdef HAVE_STATX
+      statx_flags = AT_NO_AUTOMOUNT;
+
+      if (flags & G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS)
+        statx_flags |= AT_SYMLINK_NOFOLLOW;
+
+      res = statx (AT_FDCWD, path, statx_flags, STATX_BTIME, &statxbuf);
+
+      if (res == 0)
+        set_info_from_statx (info, &statxbuf);
+#endif
+    }
 
 #ifdef G_OS_UNIX
   if (stat_ok && _g_local_file_is_lost_found_dir (path, statbuf.st_dev))
