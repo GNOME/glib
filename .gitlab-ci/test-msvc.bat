f116b9c05fc70ba9a79c676ceec8a49c3867363b
Xavier Claessens <xavier.claessens@collabora.com>
2020-01-12 22:57:43 -0500

    ci: Test static build with MSVC

--- .gitlab-ci/test-msvc.bat
+++ .gitlab-ci/test-msvc.bat
@@ -6,7 +6,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary
 
 :: FIXME: make warnings fatal
 pip3 install --upgrade --user meson==0.49.2  || goto :error
-meson _build || goto :error
+meson %MESON_OPTIONS% _build || goto :error
 ninja -C _build || goto :error
 
 :: FIXME: dont ignore test errors
