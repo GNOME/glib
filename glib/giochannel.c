c8811da56a6628d9df8a9fa0051c42e6bff94cd6
Philip Withnall <withnall@endlessm.com>
2020-01-14 19:55:36 +0000

    giochannel: Fix read_line() when nul is not a line terminator
    And add a test for it.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    
    Fixes: #2002

--- glib/giochannel.c
+++ glib/giochannel.c
@@ -1669,8 +1669,16 @@ g_io_channel_read_line (GIOChannel  *channel,
 
   if (status == G_IO_STATUS_NORMAL)
     {
+      gchar *line;
+
+      /* Copy the read bytes (including any embedded nuls) and nul-terminate.
+       * `USE_BUF (channel)->str` is guaranteed to be nul-terminated as it’s a
+       * #GString, so it’s safe to call g_memdup() with +1 length to allocate
+       * a nul-terminator. */
       g_assert (USE_BUF (channel));
-      *str_return = g_strndup (USE_BUF (channel)->str, got_length);
+      line = g_memdup (USE_BUF (channel)->str, got_length + 1);
+      line[got_length] = '\0';
+      *str_return = g_steal_pointer (&line);
       g_string_erase (USE_BUF (channel), 0, got_length);
     }
   else
