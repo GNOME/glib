748a7e9a7577587af0fb479a75fb7ce3619b8bbe
Philip Withnall <withnall@endlessm.com>
2020-02-17 17:42:54 +0000

    gparam: Clarify that parameter names must be non-empty
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

--- gobject/gparam.c
+++ gobject/gparam.c
@@ -41,8 +41,8 @@
  *
  * ## Parameter names # {#canonical-parameter-names}
  *
- * A property name consists of segments consisting of ASCII letters and
- * digits, separated by either the `-` or `_` character. The first
+ * A property name consists of one or more segments consisting of ASCII letters
+ * and digits, separated by either the `-` or `_` character. The first
  * character of a property name must be a letter. These are the same rules as
  * for signal naming (see g_signal_new()).
  *
