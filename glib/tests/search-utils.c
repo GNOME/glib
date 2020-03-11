d918e96b8783812f66ba1a2b360132a952d82951
nightuser <22158-nightuser@users.noreply.gitlab.gnome.org>
2020-03-11 16:52:35 +0300

    gstrutils: Set locale explicitly for search-utils test
    The test checks `g_str_match_string()` function, which performs matches
    based on user's locale. For this reason, some tests may fail, e.g., see
    issue #868.
    
    Now we explicitly set locale for each test, with C locale as a fallback
    when the locale is not available.

--- glib/tests/search-utils.c
+++ glib/tests/search-utils.c
@@ -7,8 +7,19 @@ typedef struct
 {
   const gchar *string;
   const gchar *prefix;
+  const gchar *locale;
   gboolean should_match;
 } SearchTest;
+
+static void
+try_setlocale (const gchar *locale)
+{
+  if (!setlocale (LC_ALL, locale))
+    {
+      setlocale (LC_ALL, "C");
+      g_info ("Cannot set locale %s, reverting to C", locale);
+    }
+}
 
 static void
 test_search (void)
@@ -16,46 +27,52 @@ test_search (void)
   SearchTest tests[] =
     {
       /* Test word separators and case */
-      { "Hello World", "he", TRUE },
-      { "Hello World", "wo", TRUE },
-      { "Hello World", "lo", FALSE },
-      { "Hello World", "ld", FALSE },
-      { "Hello-World", "wo", TRUE },
-      { "HelloWorld", "wo", FALSE },
+      { "Hello World", "he", "C", TRUE },
+      { "Hello World", "wo", "C", TRUE },
+      { "Hello World", "lo", "C", FALSE },
+      { "Hello World", "ld", "C", FALSE },
+      { "Hello-World", "wo", "C", TRUE },
+      { "HelloWorld", "wo", "C", FALSE },
 
       /* Test composed chars (accentued letters) */
-      { "Jörgen", "jor", TRUE },
-      { "Gaëtan", "gaetan", TRUE },
-      { "élève", "ele", TRUE },
-      { "Azais", "AzaÏs", FALSE },
-      { "AzaÏs", "Azais", TRUE },
+      { "Jörgen", "jor", "sv_SE.UTF-8", TRUE },
+      { "Gaëtan", "gaetan", "fr_FR.UTF-8", TRUE },
+      { "élève", "ele", "fr_FR.UTF-8", TRUE },
+      { "Azais", "AzaÏs", "fr_FR.UTF-8", FALSE },
+      { "AzaÏs", "Azais", "fr_FR.UTF-8", TRUE },
 
       /* Test decomposed chars, they looks the same, but are actually
        * composed of multiple unicodes */
-      { "Jorgen", "Jör", FALSE },
-      { "Jörgen", "jor", TRUE },
+      { "Jorgen", "Jör", "sv_SE.UTF-8", FALSE },
+      { "Jörgen", "jor", "sv_SE.UTF-8", TRUE },
 
       /* Turkish special case */
-      { "İstanbul", "ist", TRUE },
-      { "Diyarbakır", "diyarbakir", TRUE },
+      { "İstanbul", "ist", "tr_TR.UTF-8", TRUE },
+      { "Diyarbakır", "diyarbakir", "tr_TR.UTF-8", TRUE },
 
       /* Multi words */
-      { "Xavier Claessens", "Xav Cla", TRUE },
-      { "Xavier Claessens", "Cla Xav", TRUE },
-      { "Foo Bar Baz", "   b  ", TRUE },
-      { "Foo Bar Baz", "bar bazz", FALSE },
+      { "Xavier Claessens", "Xav Cla", "C", TRUE },
+      { "Xavier Claessens", "Cla Xav", "C", TRUE },
+      { "Foo Bar Baz", "   b  ", "C", TRUE },
+      { "Foo Bar Baz", "bar bazz", "C", FALSE },
 
-      { NULL, NULL, FALSE }
+      { NULL, NULL, NULL, FALSE }
     };
   guint i;
-
-  setlocale(LC_ALL, "");
+  gchar *user_locale;
 
   g_debug ("Started");
+
+  setlocale (LC_ALL, "");
+  user_locale = setlocale (LC_ALL, NULL);
+  g_debug ("Current user locale: %s", user_locale);
+
   for (i = 0; tests[i].string != NULL; i ++)
     {
       gboolean match;
       gboolean ok;
+
+      try_setlocale (tests[i].locale);
 
       match = g_str_match_string (tests[i].prefix, tests[i].string, TRUE);
       ok = (match == tests[i].should_match);
