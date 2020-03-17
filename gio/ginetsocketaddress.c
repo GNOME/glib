1dfdb750732b8d26fbe74f663728c7374576cedf
Phil Clayton <phil.clayton@veonix.com>
2020-03-17 00:22:36 +0000

    ginetaddress, ginetsocketaddress: Add missing (nullable) annotations
    In both modules, new_from_string() may return NULL.

--- gio/ginetsocketaddress.c
+++ gio/ginetsocketaddress.c
@@ -398,8 +398,8 @@ g_inet_socket_address_new (GInetAddress *address,
  * If @address is an IPv6 address, it can also contain a scope ID
  * (separated from the address by a `%`).
  *
- * Returns: a new #GInetSocketAddress, or %NULL if @address cannot be
- * parsed.
+ * Returns: (nullable) (transfer full): a new #GInetSocketAddress,
+ * or %NULL if @address cannot be parsed.
  *
  * Since: 2.40
  */
