a924a92592cda34cb0b2dd90cd07310d1dac9676
Charles <barto.charlie+code@gmail.com>
2020-04-21 22:55:09 -0700

    use Meson 0.54.0 on windows/msvc
    Also stop pip installing meson under msys, since that wasn't doing anything
    (the meson from msys2's repos comes before the pip one in the path it seems)

--- .gitlab-ci/test-msys2.sh
+++ .gitlab-ci/test-msys2.sh
@@ -36,8 +36,6 @@ CCACHE_BASEDIR="$(pwd)"
 CCACHE_DIR="${CCACHE_BASEDIR}/_ccache"
 export CCACHE_BASEDIR CCACHE_DIR
 
-pip3 install --upgrade --user meson==0.49.2
-
 PATH="$HOME/.local/bin:$PATH"
 CFLAGS="-coverage -ftest-coverage -fprofile-arcs"
 DIR="$(pwd)"
