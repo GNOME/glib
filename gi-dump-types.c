da7e635b719aa220b19d8d088b7f3b56eaaad5a8
Gisle Vanem <gisle.vanem@gmail.com>
2020-05-14 19:25:11 +0000

    [Win32] Fix gi-dump-types.c to build on Windows

--- gi-dump-types.c
+++ gi-dump-types.c
@@ -1,16 +1,24 @@
 /* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
 #include "gdump.c"
-#include <gio/gunixoutputstream.h>
+#ifdef G_OS_WIN32
+  #include <gio/gwin32outputstream.h>
+#else
+  #include <gio/gunixoutputstream.h>
+#endif
 
 int
 main (int    argc,
       char **argv)
 {
   int i;
-  GOutputStream *stdout;
+  GOutputStream *Stdout;
   GModule *self;
 
-  stdout = g_unix_output_stream_new (1, FALSE);
+#if defined(G_OS_WIN32)
+  Stdout = g_win32_output_stream_new (1, FALSE);
+#else
+  Stdout = g_output_stream (1, FALSE);
+#endif
 
   self = g_module_open (NULL, 0);
 
@@ -26,7 +34,7 @@ main (int    argc,
 	  g_clear_error (&error);
 	}
       else
-	dump_type (type, argv[i], stdout);
+	dump_type (type, argv[i], Stdout);
     }
 
   return 0;
