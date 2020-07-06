3f72a959253162df68da346577f89f01cb31109a
Marc-André Lureau <marcandre.lureau@redhat.com>
2020-07-06 19:38:40 +0400

    uri: make g_uri_parse_params() take an error
    This should be more future-proof.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- fuzzing/fuzz_uri_parse_params.c
+++ fuzzing/fuzz_uri_parse_params.c
@@ -3,6 +3,7 @@
 int
 LLVMFuzzerTestOneInput (const unsigned char *data, size_t size)
 {
+  GError *error = NULL;
   GHashTable *parsed_params = NULL;
 
   fuzz_set_logging_func ();
@@ -10,10 +11,17 @@ LLVMFuzzerTestOneInput (const unsigned char *data, size_t size)
   if (size > G_MAXSSIZE)
     return 0;
 
-  parsed_params = g_uri_parse_params ((const gchar *) data, (gssize) size, "&", G_URI_PARAMS_NONE);
+  parsed_params = g_uri_parse_params ((const gchar *) data, (gssize) size,
+                                      "&", G_URI_PARAMS_NONE, &error);
   if (parsed_params == NULL)
-    return 0;
+    {
+      g_assert (error);
+      g_clear_error (&error);
+      return 0;
+    }
 
+
+  g_assert_no_error (error);
   g_hash_table_unref (parsed_params);
 
   return 0;
