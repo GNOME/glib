49e04cc03e8d7cfcf0e03d3b05552e3198583773
holin <holin@iki.fi>
2020-07-06 17:13:12 -0700

    gmain: fix fetching timebase on non-Intel Darwin
    Co-authored-by: Misty De Meo <mistydemeo@gmail.com>

--- glib/gmain.c
+++ glib/gmain.c
@@ -2903,50 +2903,41 @@ g_get_monotonic_time (void)
   return 0;
 }
 #elif defined(HAVE_MACH_MACH_TIME_H) /* Mac OS */
+#ifdef HAVE_UINT128_T
+#define MACH_TIME_T __uint128_t
+#else
+#define MACH_TIME_T guint64
+#endif
+
 gint64
 g_get_monotonic_time (void)
 {
-  static mach_timebase_info_data_t timebase_info;
+  mach_timebase_info_data_t timebase_info;
+  guint64 val;
 
-  if (timebase_info.denom == 0)
+  /* we get nanoseconds from mach_absolute_time() using timebase_info */
+  mach_timebase_info (&timebase_info);
+  val = mach_absolute_time ();
+
+  if (timebase_info.numer != timebase_info.denom)
     {
-      /* This is a fraction that we must use to scale
-       * mach_absolute_time() by in order to reach nanoseconds.
-       *
-       * We've only ever observed this to be 1/1, but maybe it could be
-       * 1000/1 if mach time is microseconds already, or 1/1000 if
-       * picoseconds.  Try to deal nicely with that.
-       */
-      mach_timebase_info (&timebase_info);
+      MACH_TIME_T t_high, t_low;
+      MACH_TIME_T result_high, result_low;
 
-      /* We actually want microseconds... */
-      if (timebase_info.numer % 1000 == 0)
-        timebase_info.numer /= 1000;
-      else
-        timebase_info.denom *= 1000;
-
-      /* We want to make the numer 1 to avoid having to multiply... */
-      if (timebase_info.denom % timebase_info.numer == 0)
-        {
-          timebase_info.denom /= timebase_info.numer;
-          timebase_info.numer = 1;
-        }
-      else
-        {
-          /* We could just multiply by timebase_info.numer below, but why
-           * bother for a case that may never actually exist...
-           *
-           * Plus -- performing the multiplication would risk integer
-           * overflow.  If we ever actually end up in this situation, we
-           * should more carefully evaluate the correct course of action.
-           */
-          mach_timebase_info (&timebase_info); /* Get a fresh copy for a better message */
-          g_error ("Got weird mach timebase info of %d/%d.  Please file a bug against GLib.",
-                   timebase_info.numer, timebase_info.denom);
-        }
+      /* 64 bit x 32 bit / 32 bit with 96-bit intermediate 
+       * algorithm lifted from qemu */
+      t_low = (val & 0xffffffffLL) * (MACH_TIME_T) timebase_info.numer;
+      t_high = (val >> 32) * (MACH_TIME_T) timebase_info.numer;
+      t_high += (t_low >> 32);
+      result_high = t_high / (MACH_TIME_T) timebase_info.denom;
+      result_low = (((t_high % (MACH_TIME_T) timebase_info.denom) << 32) +
+                    (t_low & 0xffffffff)) /
+                   (MACH_TIME_T) timebase_info.denom;
+      val = ((result_high << 32) | result_low);
     }
 
-  return mach_absolute_time () / timebase_info.denom;
+  /* nanoseconds to microseconds */
+  return val / 1000;
 }
 #else
 gint64
