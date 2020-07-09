7e4709c294dc3f3f89a628d87f3d6da44d284b9f
Philip Withnall <withnall@endlessm.com>
2020-07-09 11:31:55 +0100

    WIP
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

--- .gitlab-ci/show-execution-environment.sh
+++ .gitlab-ci/show-execution-environment.sh
@@ -9,3 +9,11 @@ setpriv --dump || :
 ulimit -a || :
 cat /proc/self/status || :
 cat /proc/self/mountinfo || :
+
+
+cat /proc/loadavg
+nproc
+ip -s link ls
+
+wget http://localhost/metrics
+wget https://localhost/metrics
