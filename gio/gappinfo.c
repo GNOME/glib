75000b036df0e24d6000c999d653e63a56965926
Matthias Clasen <mclasen@redhat.com>
2020-07-27 19:47:47 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- gio/gappinfo.c
+++ gio/gappinfo.c
@@ -278,7 +278,7 @@ g_app_info_default_init (GAppInfoInterface *iface)
    * In #GAppInfos created from [desktop files](https://specifications.freedesktop.org/desktop-entry-spec/latest/),
    * this is the (inverted) value of the MimeType key.
    *
-   * May be %NULL, depending on how the GAppInfo has been constructed.
+   * May be %NULL, depending on how the #GAppInfo has been constructed.
    *
    * Since: 2.66
    */
