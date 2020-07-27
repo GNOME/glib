2301d75a94058771c87632e83453e834d1805d35
Matthias Clasen <mclasen@redhat.com>
2020-07-27 19:48:31 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- gio/gappinfo.c
+++ gio/gappinfo.c
@@ -151,7 +151,7 @@ g_app_info_default_init (GAppInfoInterface *iface)
    * In #GAppInfos created from [desktop files](https://specifications.freedesktop.org/desktop-entry-spec/latest/),
    * this is the value of the X-GNOME-FullName key, falling back to the Name key.
    *
-   * May be %NULL, depending on how the GAppInfo has been constructed.
+   * May be %NULL, depending on how the #GAppInfo has been constructed.
    *
    * Since: 2.66
    */
