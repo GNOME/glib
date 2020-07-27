e09594f500822a26058c355598e4ba6cf21a164c
Matthias Clasen <mclasen@redhat.com>
2020-07-27 19:48:04 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- gio/gappinfo.c
+++ gio/gappinfo.c
@@ -216,7 +216,7 @@ g_app_info_default_init (GAppInfoInterface *iface)
    *
    * The icon for the application.
    *
-   * May be %NULL, depending on how the GAppInfo has been constructed.
+   * May be %NULL, depending on how the #GAppInfo has been constructed.
    *
    * Since: 2.66
    */
