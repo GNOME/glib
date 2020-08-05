GNOME/glib

commit 20ae4b46d476915484ace6c982cfc99cd2602cac

uri: do not add ipv6 brackets on non-ip host
The heuristic is a bit too agressive, as we may have hostname with
%-encoded ':' (as shown in GVfs URI tests).

Add an extra test to check :-decoding as well.

Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

glib/tests/uri.c
@@ -669,6 +669,9 @@ static const UriAbsoluteTest absolute_tests[] = {
   { "http://:@host",
     { "http", ":", "host", -1, "", NULL, NULL }
   },
+  { "scheme://foo%3Abar._webdav._tcp.local",
+    { "scheme", NULL, "foo:bar._webdav._tcp.local", -1, "", NULL, NULL}
+  },
 
   /* IPv6 scope ID parsing (both correct and incorrect) */
   { "http://[fe80::dead:beef%em1]/",
@@ -1409,6 +1412,10 @@ test_uri_join (void)
                               "::192.9.5.5", 9876, "/path", "query", "fragment");
   g_assert_cmpstr (uri, ==,
                    "scheme://user%01:pass%02;authparams%03@[::192.9.5.5]:9876/path?query#fragment");
+  g_free (uri);
+
+  uri = g_uri_join (G_URI_FLAGS_NONE, "scheme", NULL, "foo:bar._webdav._tcp.local", -1, "", NULL, NULL);
+  g_assert_cmpstr (uri, ==, "scheme://foo%3Abar._webdav._tcp.local");
   g_free (uri);
 }
 

glib/guri.c
@@ -1383,7 +1383,7 @@ g_uri_join_internal (GUriFlags    flags,
           g_string_append_c (str, '@');
         }
 
-      if (strchr (host, ':'))
+      if (strchr (host, ':') && g_hostname_is_ip_address (host))
         {
           g_string_append_c (str, '[');
           if (encoded)

commit 3bcc6fd39f2681f3ec64b795e4362aa034dec2f4

guri: add a test to check ipv6 with zoneid URI to string
The test was failing since commit 20ae4b46d ("uri: do not add ipv6
brackets on non-ip host").

Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

glib/tests/uri.c
@@ -915,7 +915,12 @@ test_uri_to_string (void)
   tostring = g_uri_to_string (uri);
   g_assert_cmpstr (tostring, ==, "scheme://userinfo@host:1234/path?query#fragment");
   g_free (tostring);
+  g_uri_unref (uri);
 
+  uri = g_uri_build (G_URI_FLAGS_NONE, "scheme", NULL, "fe80::dead:beef%em1", -1, "", NULL, NULL);
+  tostring = g_uri_to_string (uri);
+  g_assert_cmpstr (tostring, ==, "scheme://[fe80::dead:beef%25em1]");
+  g_free (tostring);
   g_uri_unref (uri);
 
   uri = g_uri_build_with_user (G_URI_FLAGS_NONE, "scheme", "user", "pass", "auth", "host", 1234,