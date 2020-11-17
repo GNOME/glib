e187d836ade9c368b34a2ad16491d93d35520751
Philip Withnall <pwithnall@endlessos.org>
2020-11-17 15:48:28 +0000

    ci: Disable formatting warnings in flake8
    We use `black` to handle formatting of Python code.
    
    From https://stackoverflow.com/questions/59241007/flake8-disable-all-formatting-rules
    
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>

--- .gitlab-ci/run-flake8.sh
+++ .gitlab-ci/run-flake8.sh
@@ -2,5 +2,8 @@
 
 set -e
 
+# Disable formatting warnings in flake8, as we use `black` to handle that.
+formatting_warnings=E101,E111,E114,E115,E116,E117,E12,E13,E2,E3,E401,E5,E70,W1,W2,W3,W5
+
 # shellcheck disable=SC2046
-flake8 --max-line-length=88 $(git ls-files '*.py')
+flake8 --max-line-length=88 --ignore="$formatting_warnings" $(git ls-files '*.py')
