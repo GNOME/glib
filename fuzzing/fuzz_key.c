0c83f05be81a0044c43f74470eed54330492b742
Philip Withnall <pwithnall@endlessos.org>
2020-12-17 12:15:25 +0000

    fuzzing: Add more parsing flags to the GKeyFile test
    To increase the coverage.
    
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>

--- fuzzing/fuzz_key.c
+++ fuzzing/fuzz_key.c
@@ -1,16 +1,27 @@
 #include "fuzz.h"
 
-int
-LLVMFuzzerTestOneInput (const unsigned char *data, size_t size)
+static void
+test_parse (const gchar   *data,
+            size_t         size,
+            GKeyFileFlags  flags)
 {
   GKeyFile *key = NULL;
-
-  fuzz_set_logging_func ();
 
   key = g_key_file_new ();
   g_key_file_load_from_data (key, (const gchar*) data, size, G_KEY_FILE_NONE,
                              NULL);
 
   g_key_file_free (key);
+}
+
+int
+LLVMFuzzerTestOneInput (const unsigned char *data, size_t size)
+{
+  fuzz_set_logging_func ();
+
+  test_parse ((const gchar *) data, size, G_KEY_FILE_NONE);
+  test_parse ((const gchar *) data, size, G_KEY_FILE_KEEP_COMMENTS);
+  test_parse ((const gchar *) data, size, G_KEY_FILE_KEEP_TRANSLATIONS);
+
   return 0;
 }
