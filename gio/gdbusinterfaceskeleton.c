be8834340a2d928ece82025463ae23dee2c333d0
Philip Withnall <pwithnall@endlessos.org>
2021-02-04 13:37:56 +0000

    gio: Use g_memdup2() instead of g_memdup() in obvious places
    Convert all the call sites which use `g_memdup()`’s length argument
    trivially (for example, by passing a `sizeof()`), so that they use
    `g_memdup2()` instead.
    
    In almost all of these cases the use of `g_memdup()` would not have
    caused problems, but it will soon be deprecated, so best port away from
    it.
    
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>
    Helps: #2319

--- gio/gdbusinterfaceskeleton.c
+++ gio/gdbusinterfaceskeleton.c
@@ -28,6 +28,7 @@
 #include "gdbusmethodinvocation.h"
 #include "gdbusconnection.h"
 #include "gmarshal-internal.h"
+#include "gstrfuncsprivate.h"
 #include "gtask.h"
 #include "gioerror.h"
 
@@ -702,7 +703,7 @@ add_connection_locked (GDBusInterfaceSkeleton *interface_,
        * properly before building the hooked_vtable, so we create it
        * once at the last minute.
        */
-      interface_->priv->hooked_vtable = g_memdup (g_dbus_interface_skeleton_get_vtable (interface_), sizeof (GDBusInterfaceVTable));
+      interface_->priv->hooked_vtable = g_memdup2 (g_dbus_interface_skeleton_get_vtable (interface_), sizeof (GDBusInterfaceVTable));
       interface_->priv->hooked_vtable->method_call = skeleton_intercept_handle_method_call;
     }
 
