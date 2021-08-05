isabella232/gobject-introspection

Add "final" class attribute
A "final" class is a leaf node in a derivable type hierarchy, and cannot
be derived any further.

This matches the changes in libgobject that introduced G_TYPE_FLAG_FINAL
to the type flags.

girepository/gitypelib-internal.h
@@ -1034,10 +1034,11 @@ typedef struct {
 /**
  * ObjectBlob:
  * @blob_type: #BLOB_TYPE_OBJECT
- * @deprecated: TODO
- * @abstract: TODO
+ * @deprecated: whether the type is deprecated
+ * @abstract: whether the type can be instantiated
  * @fundamental: this object is not a GObject derived type, instead it's
  *   an additional fundamental type.
+ * @final: whether the type can be derived
  * @reserved: Reserved for future use.
  * @name: TODO
  * @gtype_name: String name of the associated #GType
@@ -1076,7 +1077,8 @@ typedef struct {
   guint16   deprecated   : 1;
   guint16   abstract     : 1;
   guint16   fundamental  : 1;
-  guint16   reserved     :13;
+  guint     final_       : 1;
+  guint16   reserved     :12;
   guint32   name;
 
   guint32   gtype_name;

GNOME/glib

gitypelib-internal.h
@@ -1034,10 +1034,11 @@ typedef struct {
 /**
  * ObjectBlob:
  * @blob_type: #BLOB_TYPE_OBJECT
- * @deprecated: TODO
- * @abstract: TODO
+ * @deprecated: whether the type is deprecated
+ * @abstract: whether the type can be instantiated
  * @fundamental: this object is not a GObject derived type, instead it's
  *   an additional fundamental type.
+ * @final: whether the type can be derived
  * @reserved: Reserved for future use.
  * @name: TODO
  * @gtype_name: String name of the associated #GType
@@ -1076,7 +1077,8 @@ typedef struct {
   guint16   deprecated   : 1;
   guint16   abstract     : 1;
   guint16   fundamental  : 1;
-  guint16   reserved     :13;
+  guint16   final_       : 1;
+  guint16   reserved     :12;
   guint32   name;
 
   guint32   gtype_name;