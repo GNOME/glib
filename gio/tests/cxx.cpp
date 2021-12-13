ae345e56c25e5b7452e3ce7c94611e7a71911047
Emmanuel Fleury <emmanuel.fleury@gmail.com>
2021-12-13 16:06:47 +0100

    Distribute cxx test tests/cxx-test.cpp to each module tests directory
    tests/cxx-test.cpp is removed and splitted into gio/tests/cxx.cpp,
    gmodule/tests/cxx.cpp and gobject/tests/cxx.cpp.
    
    Helps issue #1434

---
+++ gio/tests/cxx.cpp
@@ -0,0 +1,26 @@
+/* Copyright (C) 2001 Sebastian Wilhelmi <wilhelmi@google.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* A trivial C++ program to be compiled in C++ mode, which
+ * smoketests that the GIO headers are valid C++ headers. */
+
+#include <gio/gio.h>
+
+int
+main ()
+{
+  return 0;
+}
