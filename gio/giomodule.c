bb1b9d90ec2141c1690e222edc4b26427cd8a26c
Marc-André Lureau <marcandre.lureau@redhat.com>
2022-01-24 00:36:56 +0400

    gio/win32: add GMemoryMonitorWin32
    Windows has CreateMemoryResourceNotification() API:
    
    https://docs.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-creatememoryresourcenotification
    
    It only notifies whether "Available physical memory is running low."
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- gio/giomodule.c
+++ gio/giomodule.c
@@ -1080,6 +1080,7 @@ extern GType _g_network_monitor_nm_get_type (void);
 
 extern GType g_memory_monitor_dbus_get_type (void);
 extern GType g_memory_monitor_portal_get_type (void);
+extern GType g_memory_monitor_win32_get_type (void);
 extern GType g_power_profile_monitor_dbus_get_type (void);
 
 #ifdef G_OS_UNIX
@@ -1315,6 +1316,7 @@ _g_io_modules_ensure_loaded (void)
 #ifdef G_OS_WIN32
       g_type_ensure (g_win32_notification_backend_get_type ());
       g_type_ensure (_g_winhttp_vfs_get_type ());
+      g_type_ensure (g_memory_monitor_win32_get_type ());
 #endif
       g_type_ensure (_g_local_vfs_get_type ());
       g_type_ensure (_g_dummy_proxy_resolver_get_type ());
