425702df9c5590feca8e28e6948d8a3e8edcd649
Philip Withnall <pwithnall@endlessos.org>
2022-02-18 01:40:44 +0000

    gresource-tool: Fix unused-but-set-variable warn with G_DISABLE_ASSERT
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>

--- gio/gresource-tool.c
+++ gio/gresource-tool.c
@@ -180,7 +180,7 @@ elf_foreach_resource_section (Elf             *elf,
                               SectionCallback  callback,
                               gpointer         data)
 {
-  int ret;
+  int ret G_GNUC_UNUSED  /* when compiling with G_DISABLE_ASSERT */;
   size_t shstrndx, shnum;
   size_t scnidx;
   Elf_Scn *scn;
