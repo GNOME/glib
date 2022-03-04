23e935a34260e22656c2f74826430513cf83a9d4
Philip Withnall <pwithnall@endlessos.org>
2022-03-04 18:16:12 +0000

    Revert "gapplication: Expose zero-valued numbers in handle-local-options"
    This reverts commit 1ed67a9c44508dc70b4bbdb02ed91037643fdfd0.
    
    It turns out that including options, with their default values, in the
    `handle-local-options` signal, which weren’t set on the command line,
    breaks some applications.
    
    In particular, it breaks Inkscape, which is the application this commit
    was originally meant to fix (a different problem).
    
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>
    
    Fixes: #2611
    Breaks: #2329
    See: !1953

--- gio/gapplication.c
+++ gio/gapplication.c
@@ -434,7 +434,8 @@ g_application_pack_option_entries (GApplication *application,
           break;
 
         case G_OPTION_ARG_INT:
-          value = g_variant_new_int32 (*(gint32 *) entry->arg_data);
+          if (*(gint32 *) entry->arg_data)
+            value = g_variant_new_int32 (*(gint32 *) entry->arg_data);
           break;
 
         case G_OPTION_ARG_FILENAME:
@@ -453,11 +454,13 @@ g_application_pack_option_entries (GApplication *application,
           break;
 
         case G_OPTION_ARG_DOUBLE:
-          value = g_variant_new_double (*(gdouble *) entry->arg_data);
+          if (*(gdouble *) entry->arg_data)
+            value = g_variant_new_double (*(gdouble *) entry->arg_data);
           break;
 
         case G_OPTION_ARG_INT64:
-          value = g_variant_new_int64 (*(gint64 *) entry->arg_data);
+          if (*(gint64 *) entry->arg_data)
+            value = g_variant_new_int64 (*(gint64 *) entry->arg_data);
           break;
 
         default:
@@ -695,10 +698,6 @@ add_packed_option (GApplication *application,
  * need to handle these arguments for yourself because once they are
  * consumed, they will no longer be visible to the default handling
  * (which treats them as filenames to be opened).
- *
- * The dict includes options that have been explicitly specified on the parsed
- * commandline, as well as zero values for numeric options that were not
- * necessarily specified.
  *
  * It is important to use the proper GVariant format when retrieving
  * the options with g_variant_dict_lookup():
