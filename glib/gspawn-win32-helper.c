GNOME/glib

commit 674072b13976bad5a14d67473147cf2879bf6e0e

glib/win32: check if dup() failed in helper process
Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

glib/gspawn-win32-helper.c
@@ -153,6 +153,21 @@ protect_wargv (gint       argc,
   return argc;
 }
 
+static int
+checked_dup2 (int oldfd, int newfd, int report_fd, gboolean close_old)
+{
+  if (oldfd == newfd)
+    return newfd;
+
+  if (dup2 (oldfd, newfd) == -1)
+    write_err_and_exit (report_fd, CHILD_DUP_FAILED);
+
+  if (close_old)
+    close (oldfd);
+
+  return newfd;
+}
+
 #if (defined (_MSC_VER) && _MSC_VER >= 1400)
 /*
  * This is the (empty) invalid parameter handler
@@ -255,20 +270,12 @@ main (int ignored_argc, char **ignored_argv)
   else if (argv[ARG_STDIN][0] == 'z')
     {
       fd = open ("NUL:", O_RDONLY);
-      if (fd != 0)
-	{
-	  dup2 (fd, 0);
-	  close (fd);
-	}
+      checked_dup2 (fd, 0, child_err_report_fd, TRUE);
     }
   else
     {
       fd = atoi (argv[ARG_STDIN]);
-      if (fd != 0)
-	{
-	  dup2 (fd, 0);
-	  close (fd);
-	}
+      checked_dup2 (fd, 0, child_err_report_fd, TRUE);
     }
 
   if (argv[ARG_STDOUT][0] == '-')
@@ -276,42 +283,28 @@ main (int ignored_argc, char **ignored_argv)
   else if (argv[ARG_STDOUT][0] == 'z')
     {
       fd = open ("NUL:", O_WRONLY);
-      if (fd != 1)
-	{
-	  dup2 (fd, 1);
-	  close (fd);
-	}
+      checked_dup2 (fd, 1, child_err_report_fd, TRUE);
     }
   else
     {
       fd = atoi (argv[ARG_STDOUT]);
-      if (fd != 1)
-	{
-	  dup2 (fd, 1);
-	  close (fd);
-	}
+      checked_dup2 (fd, 1, child_err_report_fd, TRUE);
     }
 
   saved_stderr_fd = reopen_noninherited (dup (2), _O_WRONLY);
+  if (saved_stderr_fd == -1)
+    write_err_and_exit (child_err_report_fd, CHILD_DUP_FAILED);
   if (argv[ARG_STDERR][0] == '-')
     ; /* Nothing */
   else if (argv[ARG_STDERR][0] == 'z')
     {
       fd = open ("NUL:", O_WRONLY);
-      if (fd != 2)
-	{
-	  dup2 (fd, 2);
-	  close (fd);
-	}
+      checked_dup2 (fd, 2, child_err_report_fd, TRUE);
     }
   else
     {
       fd = atoi (argv[ARG_STDERR]);
-      if (fd != 2)
-	{
-	  dup2 (fd, 2);
-	  close (fd);
-	}
+      checked_dup2 (fd, 2, child_err_report_fd, TRUE);
     }
 
   /* argv[ARG_WORKING_DIRECTORY] is the directory in which to run the
@@ -337,6 +330,8 @@ main (int ignored_argc, char **ignored_argv)
    */
   child_err_report_fd = reopen_noninherited (child_err_report_fd, _O_WRONLY);
   helper_sync_fd = reopen_noninherited (helper_sync_fd, _O_RDONLY);
+  if (helper_sync_fd == -1)
+    write_err_and_exit (child_err_report_fd, CHILD_DUP_FAILED);
 
   /* argv[ARG_WAIT] is "w" to wait for the program to exit */
   if (argv[ARG_WAIT][0] == 'w')

glib/gspawn-win32.c
@@ -89,6 +89,7 @@ enum
   CHILD_CHDIR_FAILED,
   CHILD_SPAWN_FAILED,
   CHILD_SPAWN_NOENT,
+  CHILD_DUP_FAILED,
 };
 
 enum {
@@ -391,6 +392,11 @@ set_child_error (gintptr      report[2],
     case CHILD_SPAWN_NOENT:
       g_set_error (error, G_SPAWN_ERROR, G_SPAWN_ERROR_NOENT,
                    _("Failed to execute child process (%s)"),
+                   g_strerror (report[1]));
+      break;
+    case CHILD_DUP_FAILED:
+      g_set_error (error, G_SPAWN_ERROR, G_SPAWN_ERROR_FAILED,
+                   _("Failed to dup() in child process (%s)"),
                    g_strerror (report[1]));
       break;
     default:

commit f70dc8bcb46c3a724c12b311bf951ace889ce05b

glib/win32: fix spawn from GUI regression
In commit 674072b13 ("glib/win32: check if dup() failed in helper
process"), I happily added checks for dup() calls in the spawn helper.
However, I didn't realize that GUI applications do not necessarily have
a console attached, and will now fail spawning processes.

One way to check that is via a _fileno() call, as explained in:
https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/fileno?view=msvc-170#return-value

Fixes:
https://gitlab.gnome.org/GNOME/glib/-/issues/2628

Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

glib/gspawn-win32-helper.c
@@ -292,9 +292,13 @@ main (int ignored_argc, char **ignored_argv)
       checked_dup2 (fd, 1, child_err_report_fd);
     }
 
-  saved_stderr_fd = reopen_noninherited (dup (2), _O_WRONLY);
-  if (saved_stderr_fd == -1)
-    write_err_and_exit (child_err_report_fd, CHILD_DUP_FAILED);
+  /* GUI application do not necessarily have a stderr */
+  if (_fileno (stderr) == 2)
+    {
+      saved_stderr_fd = reopen_noninherited (dup (2), _O_WRONLY);
+      if (saved_stderr_fd == -1)
+        write_err_and_exit (child_err_report_fd, CHILD_DUP_FAILED);
+    }
 
   maxfd = MAX (saved_stderr_fd, maxfd);
   if (argv[ARG_STDERR][0] == '-')
@@ -354,8 +358,11 @@ main (int ignored_argc, char **ignored_argv)
   child_err_report_fd = checked_dup2 (child_err_report_fd, maxfd, child_err_report_fd);
   maxfd++;
   helper_sync_fd = checked_dup2 (helper_sync_fd, maxfd, child_err_report_fd);
-  maxfd++;
-  saved_stderr_fd = checked_dup2 (saved_stderr_fd, maxfd, child_err_report_fd);
+  if (saved_stderr_fd >= 0)
+    {
+      maxfd++;
+      saved_stderr_fd = checked_dup2 (saved_stderr_fd, maxfd, child_err_report_fd);
+    }
 
   {
     GHashTableIter iter;
@@ -381,7 +388,8 @@ main (int ignored_argc, char **ignored_argv)
 
   g_hash_table_add (fds, GINT_TO_POINTER (child_err_report_fd));
   g_hash_table_add (fds, GINT_TO_POINTER (helper_sync_fd));
-  g_hash_table_add (fds, GINT_TO_POINTER (saved_stderr_fd));
+  if (saved_stderr_fd >= 0)
+    g_hash_table_add (fds, GINT_TO_POINTER (saved_stderr_fd));
 
   /* argv[ARG_CLOSE_DESCRIPTORS] is "y" if file descriptors from 3
    *  upwards should be closed
@@ -430,7 +438,9 @@ main (int ignored_argc, char **ignored_argv)
    * Remove redirection so that they would go to original stderr
    * instead of being treated as part of stderr of child process.
    */
-  dup2 (saved_stderr_fd, 2);
+  if (saved_stderr_fd >= 0)
+    dup2 (saved_stderr_fd, 2);
+
   if (handle == -1 && saved_errno != 0)
     {
       int ec = (saved_errno == ENOENT)