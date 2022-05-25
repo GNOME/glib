a8058cb12ced8e825d03412eea8aa647472050e2
Marc-André Lureau <marcandre.lureau@redhat.com>
2022-05-25 15:27:08 +0200

    gitlab-ci: left-over from junit cleanup
    This should have been part of GNOME/glib!2686 (sorry!)
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- .gitlab-ci/run-tests.sh
+++ .gitlab-ci/run-tests.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-set +e
-
-case "$1" in
-  --log-file)
-    log_file="$2"
-    shift
-    shift
-    ;;
-  *)
-    log_file="_build/meson-logs/testlog.json"
-esac
-
 meson test \
         -C _build \
         --timeout-multiplier "${MESON_TEST_TIMEOUT_MULTIPLIER}" \
         --no-suite flaky \
         "$@"
-
-exit_code=$?
-
-exit $exit_code
