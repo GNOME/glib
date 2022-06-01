26409f19cd9460b460a100603147e571ef7ffbca
Philip Withnall <pwithnall@endlessos.org>
2022-06-01 12:44:23 +0100

    Add SPDX license headers for LGPL-2.1-or-later to various files
    These have all been added manually, as I’ve finished all the files which
    I can automatically detect.
    
    All the license headers in this commit are for LGPL-2.1-or-later, and
    all have been double-checked against the license paragraph in the file
    header.
    
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>
    
    Helps: #1415

--- glib/gdatetime.c
+++ glib/gdatetime.c
@@ -6,6 +6,8 @@
  * Copyright © 2010 Codethink Limited
  * Copyright © 2018 Tomasz Miąsko
  *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2.1 of the
--- gobject/gclosure.c
+++ gobject/gclosure.c
@@ -2,6 +2,8 @@
  * Copyright (C) 2000-2001 Red Hat, Inc.
  * Copyright (C) 2005 Imendio AB
  *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
