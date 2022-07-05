59fc26cbaad857ed6e4023e0f98682aef6846b90
Philip Withnall <pwithnall@endlessos.org>
2022-07-05 12:30:46 +0100

    gio: Add some missing license and copyright headers
    These headers have all been written manually, by looking through the git
    log for each file and noting the copyright of each significant
    contribution.
    
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>
    
    Helps: #1415

--- gio/tests/actions.c
+++ gio/tests/actions.c
@@ -1,3 +1,26 @@
+/*
+ * Copyright © 2010, 2011, 2013, 2014 Codethink Limited
+ * Copyright © 2010, 2011, 2012, 2013, 2015 Red Hat, Inc.
+ * Copyright © 2012 Pavel Vasin
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Ryan Lortie <desrt@desrt.ca>
+ */
+
 #include <gio/gio.h>
 #include <stdlib.h>
 #include <string.h>
