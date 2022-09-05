1eb1a47a50f31b2cea71cf8c94c8989727abb98c
Andy Holmes <andrew.g.r.holmes@gmail.com>
2022-09-04 17:29:39 -0700

    GActionGroup: Fix ownership transfer annotation for query_action()
    The `(transfer none)` behaviour for `parameter_type` and `state_type`
    parameters is implicit with the `const` attribute, but was incorrectly
    determined to be `(transfer full)` in the GIR.
    
    Add explicit `(transfer none)` annotations for these two parameters.

--- gio/gactiongroup.c
+++ gio/gactiongroup.c
@@ -741,8 +741,8 @@ g_action_group_action_state_changed (GActionGroup *action_group,
  * @action_group: a #GActionGroup
  * @action_name: the name of an action in the group
  * @enabled: (out): if the action is presently enabled
- * @parameter_type: (out) (optional): the parameter type, or %NULL if none needed
- * @state_type: (out) (optional): the state type, or %NULL if stateless
+ * @parameter_type: (out) (transfer none) (optional): the parameter type, or %NULL if none needed
+ * @state_type: (out) (transfer none) (optional): the state type, or %NULL if stateless
  * @state_hint: (out) (optional): the state hint, or %NULL if none
  * @state: (out) (optional): the current state, or %NULL if stateless
  *
