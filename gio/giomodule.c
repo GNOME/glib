3967d1da56d677d2c1291851fb21d4ef9afa6799
Marc-André Lureau <marcandre.lureau@redhat.com>
2022-10-18 10:52:00 +0400

    Revert "gio/module: fix leak when there is no cache"
    This reverts commit ad0fd6c5d9da1b1c00fdbc0cb3d1a9829e99faeb.
    
    The type system actually keeps a weak reference on the module/plugin.
    g_type_module_unuse() documentation is explicit "Once a #GTypeModule is
    initialized, it must exist forever."
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- gio/giomodule.c
+++ gio/giomodule.c
@@ -581,8 +581,10 @@ g_io_modules_scan_all_in_directory_with_scope (const char     *dirname,
 	      if (g_type_module_use (G_TYPE_MODULE (module)))
 		g_type_module_unuse (G_TYPE_MODULE (module)); /* Unload */
 	      else
-                g_printerr ("Failed to load module: %s\n", path);
-              g_object_unref (module);
+		{
+		  g_printerr ("Failed to load module: %s\n", path);
+		  g_object_unref (module);
+		}
 	    }
 
 	  g_free (path);
