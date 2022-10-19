8d5ae3ae83091363e459c1de31fe8426734ff8ee
Marco Trevisan (Treviño) <mail@3v1n0.net>
2022-10-19 21:34:48 +0200

    ci: Mark failing mingw tests as can-fail and do not ignore test failures

--- .gitlab-ci/test-msys2.sh
+++ .gitlab-ci/test-msys2.sh
@@ -50,8 +50,7 @@ lcov \
     --initial \
     --output-file "${DIR}/_coverage/${CI_JOB_NAME}-baseline.lcov"
 
-# FIXME: fix the test suite
-meson test --timeout-multiplier "${MESON_TEST_TIMEOUT_MULTIPLIER}" || true
+meson test --timeout-multiplier "${MESON_TEST_TIMEOUT_MULTIPLIER}"
 meson test --timeout-multiplier "${MESON_TEST_TIMEOUT_MULTIPLIER}" \
     --setup=unstable_tests --suite=failing --suite=flaky || true
 
