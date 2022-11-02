9ebb491d554b08d4817f1a5e55378e6027de148a
Philip Withnall <pwithnall@endlessos.org>
2022-11-02 10:05:52 +0000

    Revert "meson: Define G_OS_DARWIN when compiling under OSX or iOS"
    This reverts commit e85635daa07e9c669ded75c476d67d02aa85c7c1.
    
    See the previous revert for rationale.
    
    Fixes: #2802

--- glib/docs.c
+++ glib/docs.c
@@ -1998,17 +1998,6 @@
  */
 
 /**
- * G_OS_DARWIN:
- *
- * This macro is defined only on Apple operating systems (macOS or iOS).
- * So you can bracket Apple-specific code in `\#ifdef G_OS_DARWIN`.
- *
- * Note that %G_OS_UNIX is also set.
- *
- * Since: 2.76
- */
-
-/**
  * G_OS_WIN32:
  *
  * This macro is defined only on Windows. So you can bracket
