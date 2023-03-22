0a10851faa1e3a9563df03dd591e3ecc02c9fcfb
Philip Withnall <pwithnall@endlessos.org>
2023-03-22 14:36:57 +0000

    tests: Add copyright/licensing header to appmonitor tests
    This is put together through git archaeology:
    ```
    git log gio/tests/appmonitor.c
    ```
    
    The following commits were too trivial to have meaningful copyright:
     - 54047080e963b2d6c3f966340dcd9d788b73ac9c
     - 4e7d22e268a4e06beb1c09585a48288c31004da5
     - f2c1cfe8c770f73367a021044bcdda550348714c
     - f8f344923eba57ca13d82e53f3e7b82633179f43
     - 3ce00b29ec0bb240412506affb5c65bdb18c7ad3
     - 346836962521cce76f3cbd53259a985d3bef6a2a
     - e9d9edde824df1229bc5539610b98c98013e93aa
    
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>
    
    Helps: #1415

--- gio/tests/appmonitor.c
+++ gio/tests/appmonitor.c
@@ -1,3 +1,24 @@
+/* GLib testing framework examples and tests
+ *
+ * Copyright © 2013 Red Hat, Inc.
+ * Copyright © 2015, 2017, 2018 Endless Mobile, Inc.
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <gio/gio.h>
 #include <gstdio.h>
 
