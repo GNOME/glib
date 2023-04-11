a89b72389ae71acb2a1b9e061a474ff01b37ab58
badcel <33569-badcel@users.noreply.gitlab.gnome.org>
2023-04-11 20:40:00 +0200

    Explicitly mark size parameter as (in) The generated gir file marks the size parameter as "out" by default. This is wrong in the context of a caller allocated buffer with a given size. Explicitly marking the size parameter as (in) fixes the issue.

--- gio/gsocket.c
+++ gio/gsocket.c
@@ -3304,7 +3304,7 @@ g_socket_receive_with_timeout (GSocket       *socket,
  * @socket: a #GSocket
  * @buffer: (array length=size) (element-type guint8) (out caller-allocates):
  *     a buffer to read data into (which should be at least @size bytes long).
- * @size: the number of bytes you want to read from the socket
+ * @size: (in): the number of bytes you want to read from the socket
  * @cancellable: (nullable): a %GCancellable or %NULL
  * @error: #GError for error reporting, or %NULL to ignore.
  *
@@ -3354,7 +3354,7 @@ g_socket_receive (GSocket       *socket,
  * @socket: a #GSocket
  * @buffer: (array length=size) (element-type guint8) (out caller-allocates):
  *     a buffer to read data into (which should be at least @size bytes long).
- * @size: the number of bytes you want to read from the socket
+ * @size: (in): the number of bytes you want to read from the socket
  * @blocking: whether to do blocking or non-blocking I/O
  * @cancellable: (nullable): a %GCancellable or %NULL
  * @error: #GError for error reporting, or %NULL to ignore.
