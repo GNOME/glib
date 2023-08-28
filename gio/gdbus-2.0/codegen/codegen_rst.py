5f8af01b8b7d28be30d5f8e470cfecb1daa0319f
Emmanuele Bassi <ebassi@gnome.org>
2023-08-28 16:26:24 +0100

    Generate RST anchors for methods, signals, and properties
    Don't rely on the titles.

--- gio/gdbus-2.0/codegen/codegen_rst.py
+++ gio/gdbus-2.0/codegen/codegen_rst.py
@@ -120,11 +120,11 @@ class RstCodeGenerator:
     def _generate_section(self, title, name):
         """Generates a section with the given title."""
         res = [
+            f".. _{name} {title}:",
+            "",
             "-" * len(title),
             title,
             "-" * len(title),
-            "",
-            f".. {name} {title}:",
             "",
             "",
         ]
@@ -142,6 +142,8 @@ class RstCodeGenerator:
             else:
                 access = "readable"
             res += [
+                ".. _{title}:",
+                "",
                 title,
                 "^" * len(title),
                 "",
@@ -216,6 +218,8 @@ class RstCodeGenerator:
         for m in iface.methods:
             title = f"{iface.name}.{m.name}"
             res += [
+                ".. _{title}:",
+                "",
                 title,
                 "^" * len(title),
                 "",
@@ -292,6 +296,8 @@ class RstCodeGenerator:
         for s in iface.signals:
             title = f"{iface.name}::{s.name}"
             res += [
+                ".. _{title}:",
+                "",
                 title,
                 "^" * len(title),
                 "",
