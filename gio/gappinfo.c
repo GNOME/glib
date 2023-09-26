93788dfb07998e7806b82d7cc9602e6ae50121cf
Matthias Clasen <mclasen@redhat.com>
2023-09-25 21:34:01 -0400

    docs: Move the GAppInfoMonitor SECTION
    Move the contents to the struct docs.
    
    Helps: #3037

--- gio/gappinfo.c
+++ gio/gappinfo.c
@@ -1654,53 +1654,46 @@ g_app_launch_context_launch_failed (GAppLaunchContext *context,
 
 
 /**
- * SECTION:gappinfomonitor
- * @short_description: Monitor application information for changes
- *
- * #GAppInfoMonitor is a very simple object used for monitoring the app
- * info database for changes (newly installed or removed applications).
- *
- * Call g_app_info_monitor_get() to get a #GAppInfoMonitor and connect
- * to the #GAppInfoMonitor::changed signal. The signal will be emitted once when
- * the app info database changes, and will not be emitted again until after the
- * next call to g_app_info_get_all() or another `g_app_info_*()` function. This
- * is because monitoring the app info database for changes is expensive.
- *
- * The following functions will re-arm the #GAppInfoMonitor::changed signal so
- * it can be emitted again:
- *  - g_app_info_get_all()
- *  - g_app_info_get_all_for_type()
- *  - g_app_info_get_default_for_type()
- *  - g_app_info_get_fallback_for_type()
- *  - g_app_info_get_recommended_for_type()
- *  - g_desktop_app_info_get_implementations()
- *  - g_desktop_app_info_new()
- *  - g_desktop_app_info_new_from_filename()
- *  - g_desktop_app_info_new_from_keyfile()
- *  - g_desktop_app_info_search()
- *
- * In the usual case, applications should try to make note of the change
- * (doing things like invalidating caches) but not act on it.  In
- * particular, applications should avoid making calls to #GAppInfo APIs
- * in response to the change signal, deferring these until the time that
- * the updated data is actually required.  The exception to this case is when
- * application information is actually being displayed on the screen
- * (for example, during a search or when the list of all applications is shown).
- * The reason for this is that changes to the list of installed
- * applications often come in groups (like during system updates) and
- * rescanning the list on every change is pointless and expensive.
- *
- * Since: 2.40
- **/
-
-/**
  * GAppInfoMonitor:
  *
- * The only thing you can do with this is to get it via
- * g_app_info_monitor_get() and connect to the "changed" signal.
+ * `GAppInfoMonitor` monitors application information for changes.
+ *
+ * `GAppInfoMonitor` is a very simple object used for monitoring the app
+ * info database for changes (newly installed or removed applications).
+ *
+ * Call [func@Gio.AppInfoMonitor.get] to get a `GAppInfoMonitor` and connect
+ * to the [signal@Gio.AppInfoMonitor.changed] signal. The signal will be emitted once when
+ * the app info database changes, and will not be emitted again until after the
+ * next call to [func@Gio.AppInfo.get_all] or another `g_app_info_*()` function.
+ * This is because monitoring the app info database for changes is expensive.
+ *
+ * The following functions will re-arm the [signal@Gio.AppInfoMonitor.changed]
+ * signal so it can be emitted again:
+ *
+ *  - [method@Gio.AppInfo.get_all]
+ *  - [method@Gio.AppInfo.get_all_for_type]
+ *  - [method@Gio.AppInfo.get_default_for_type]
+ *  - [method@Gio.AppInfo.get_fallback_for_type]
+ *  - [method@Gio.AppInfo.get_recommended_for_type]
+ *  - [method@Gio.DesktopAppInfo.get_implementations]
+ *  - [method@Gio.DesktopAppInfo.new]
+ *  - [method@Gio.DesktopAppInfo.new_from_filename]
+ *  - [method@Gio.DesktopAppInfo.new_from_keyfile]
+ *  - [method@Gio.DesktopAppInfo.search]
+ *
+ * In the usual case, applications should try to make note of the change
+ * (doing things like invalidating caches) but not act on it. In
+ * particular, applications should avoid making calls to `GAppInfo` APIs
+ * in response to the change signal, deferring these until the time that
+ * the updated data is actually required. The exception to this case is when
+ * application information is actually being displayed on the screen
+ * (for example, during a search or when the list of all applications is shown).
+ * The reason for this is that changes to the list of installed applications
+ * often come in groups (like during system updates) and rescanning the list
+ * on every change is pointless and expensive.
  *
  * Since: 2.40
- **/
+ */
 
 typedef struct _GAppInfoMonitorClass GAppInfoMonitorClass;
 
