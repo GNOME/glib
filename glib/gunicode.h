9770d7ba314820d2ea731cc3d6720c41334cc9a2
Philip Withnall <pwithnall@endlessos.org>
2023-10-17 22:03:14 +0100

    gunicode: Fix a minor typo in a documentation comment
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>

--- glib/gunicode.h
+++ glib/gunicode.h
@@ -212,7 +212,7 @@ typedef enum
  *
  * These are the possible line break classifications.
  *
- * Since new unicode versions may add new types here, applications should be ready 
+ * Since new Unicode versions may add new types here, applications should be ready
  * to handle unknown values. They may be regarded as %G_UNICODE_BREAK_UNKNOWN.
  *
  * See [Unicode Line Breaking Algorithm](https://www.unicode.org/reports/tr14/).
