GNOME/glib

commit 0b114b26872da160126a9327503a87794a100abf

Add support for case modifiers to DateTime

glib/gdatetime.c
@@ -3048,8 +3048,10 @@ g_date_time_format_utf8 (GDateTime   *datetime,
   gunichar  c;
   gboolean  alt_digits = FALSE;
   gboolean  pad_set = FALSE;
+  gboolean mod_case = FALSE;
   gboolean  name_is_utf8;
   const gchar *pad = "";
+  const gchar *mod = "";
   const gchar *name;
   const gchar *tz;
 
@@ -3071,6 +3073,7 @@ g_date_time_format_utf8 (GDateTime   *datetime,
       colons = 0;
       alt_digits = FALSE;
       pad_set = FALSE;
+      mod_case = FALSE;
 
     next_mod:
       c = g_utf8_get_char (utf8_format);
@@ -3078,7 +3081,8 @@ g_date_time_format_utf8 (GDateTime   *datetime,
       switch (c)
 	{
 	case 'a':
-	  name = WEEKDAY_ABBR (datetime);
+          name = mod_case ? g_utf8_strup (WEEKDAY_ABBR (datetime), -1)
+                          : WEEKDAY_ABBR (datetime);
           if (g_strcmp0 (name, "") == 0)
             return FALSE;
 
@@ -3089,7 +3093,8 @@ g_date_time_format_utf8 (GDateTime   *datetime,
 
 	  break;
 	case 'A':
-	  name = WEEKDAY_FULL (datetime);
+          name = mod_case ? g_utf8_strup (WEEKDAY_FULL (datetime), -1)
+                          : WEEKDAY_FULL (datetime);
           if (g_strcmp0 (name, "") == 0)
             return FALSE;
 
@@ -3104,6 +3109,8 @@ g_date_time_format_utf8 (GDateTime   *datetime,
 			    : MONTH_ABBR_WITH_DAY (datetime);
           if (g_strcmp0 (name, "") == 0)
             return FALSE;
+          if (mod_case)
+            name = g_utf8_strup (name, -1);
 
           name_is_utf8 = locale_is_utf8 ||
             ((alt_digits && !MONTH_ABBR_STANDALONE_IS_LOCALE) ||
@@ -3118,6 +3125,8 @@ g_date_time_format_utf8 (GDateTime   *datetime,
 			    : MONTH_FULL_WITH_DAY (datetime);
           if (g_strcmp0 (name, "") == 0)
             return FALSE;
+          if (mod_case)
+            name = g_utf8_strup (name, -1);
 
           name_is_utf8 = locale_is_utf8 ||
             ((alt_digits && !MONTH_FULL_STANDALONE_IS_LOCALE) ||
@@ -3171,6 +3180,8 @@ g_date_time_format_utf8 (GDateTime   *datetime,
 			    : MONTH_ABBR_WITH_DAY (datetime);
           if (g_strcmp0 (name, "") == 0)
             return FALSE;
+          if (mod_case)
+            name = g_utf8_strup (name, -1);
 
           name_is_utf8 = locale_is_utf8 ||
             ((alt_digits && !MONTH_ABBR_STANDALONE_IS_LOCALE) ||
@@ -3215,11 +3226,15 @@ g_date_time_format_utf8 (GDateTime   *datetime,
 	  alt_digits = TRUE;
 	  goto next_mod;
 	case 'p':
-          if (!format_ampm (datetime, outstr, locale_is_utf8, TRUE))
+          if (!format_ampm (datetime, outstr, locale_is_utf8,
+                            mod_case && g_strcmp0 (mod, "#") == 0 ? FALSE
+                                                                  : TRUE))
             return FALSE;
           break;
 	case 'P':
-          if (!format_ampm (datetime, outstr, locale_is_utf8, FALSE))
+          if (!format_ampm (datetime, outstr, locale_is_utf8,
+                            mod_case && g_strcmp0 (mod, "^") == 0 ? TRUE
+                                                                  : FALSE))
             return FALSE;
 	  break;
 	case 'r':
@@ -3299,7 +3314,8 @@ g_date_time_format_utf8 (GDateTime   *datetime,
 	  }
 	  break;
 	case 'Z':
-	  tz = g_date_time_get_timezone_abbreviation (datetime);
+          tz = mod_case && g_strcmp0 (mod, "#") == 0 ? g_utf8_strdown (g_date_time_get_timezone_abbreviation (datetime), -1)
+                                                     : g_date_time_get_timezone_abbreviation (datetime);
           g_string_append (outstr, tz);
 	  break;
 	case '%':
@@ -3323,9 +3339,17 @@ g_date_time_format_utf8 (GDateTime   *datetime,
 	    return FALSE;
 	  colons++;
 	  goto next_mod;
-	default:
-	  return FALSE;
-	}
+        case '^':
+          mod_case = TRUE;
+          mod = "^";
+          goto next_mod;
+        case '#':
+          mod_case = TRUE;
+          mod = "#";
+          goto next_mod;
+        default:
+          return FALSE;
+        }
     }
 
   return TRUE;
@@ -3418,8 +3442,9 @@ g_date_time_format_utf8 (GDateTime   *datetime,
  * - `%%`: a literal `%` character
  *
  * Some conversion specifications can be modified by preceding the
- * conversion specifier by one or more modifier characters. The
- * following modifiers are supported for many of the numeric
+ * conversion specifier by one or more modifier characters.
+ *
+ * The following modifiers are supported for many of the numeric
  * conversions:
  *
  * - `O`: Use alternative numeric symbols, if the current locale supports those.
@@ -3429,6 +3454,13 @@ g_date_time_format_utf8 (GDateTime   *datetime,
  *   for the specifier.
  * - `0`: Pad a numeric result with zeros. This overrides the default padding
  *   for the specifier.
+ *
+ * The following modifiers are supported for many of the alphabetic conversions:
+ *
+ * - `^`: Use upper case if possible. This is a gnulib `strftime()` extension.
+ *   Since: 2.80
+ * - `#`: Use opposite case if possible. This is a gnulib `strftime()`
+ *   extension. Since: 2.80
  *
  * Additionally, when `O` is used with `B`, `b`, or `h`, it produces the alternative
  * form of a month name. The alternative form should be used when the month

glib/tests/gdatetime.c
@@ -1792,6 +1792,30 @@ test_modifiers (void)
   TEST_PRINTF_DATE (2009, 1, 21, "%-e", "21");
   TEST_PRINTF_DATE (2009, 1, 21, "%0e", "21");
 
+  TEST_PRINTF_DATE (2009, 1, 1, "%a", "Thu");
+  TEST_PRINTF_DATE (2009, 1, 1, "%^a", "THU");
+  TEST_PRINTF_DATE (2009, 1, 1, "%#a", "THU");
+
+  TEST_PRINTF_DATE (2009, 1, 1, "%A", "Thursday");
+  TEST_PRINTF_DATE (2009, 1, 1, "%^A", "THURSDAY");
+  TEST_PRINTF_DATE (2009, 1, 1, "%#A", "THURSDAY");
+
+  TEST_PRINTF_DATE (2009, 1, 1, "%b", "Jan");
+  TEST_PRINTF_DATE (2009, 1, 1, "%^b", "JAN");
+  TEST_PRINTF_DATE (2009, 1, 1, "%#b", "JAN");
+
+  TEST_PRINTF_DATE (2009, 1, 1, "%B", "January");
+  TEST_PRINTF_DATE (2009, 1, 1, "%^B", "JANUARY");
+  TEST_PRINTF_DATE (2009, 1, 1, "%#B", "JANUARY");
+
+  TEST_PRINTF_DATE (2009, 1, 1, "%h", "Jan");
+  TEST_PRINTF_DATE (2009, 1, 1, "%^h", "JAN");
+  TEST_PRINTF_DATE (2009, 1, 1, "%#h", "JAN");
+
+  TEST_PRINTF_DATE (2009, 1, 1, "%Z", "UTC");
+  TEST_PRINTF_DATE (2009, 1, 1, "%^Z", "UTC");
+  TEST_PRINTF_DATE (2009, 1, 1, "%#Z", "utc");
+
   TEST_PRINTF_TIME ( 1, 0, 0,  "%H", "01");
   TEST_PRINTF_TIME ( 1, 0, 0, "%_H", " 1");
   TEST_PRINTF_TIME ( 1, 0, 0, "%-H", "1");
@@ -1823,6 +1847,14 @@ test_modifiers (void)
   TEST_PRINTF_TIME (23, 0, 0, "%_l", "11");
   TEST_PRINTF_TIME (23, 0, 0, "%-l", "11");
   TEST_PRINTF_TIME (23, 0, 0, "%0l", "11");
+
+  TEST_PRINTF_TIME (1, 0, 0, "%p", "AM");
+  TEST_PRINTF_TIME (1, 0, 0, "%^p", "AM");
+  TEST_PRINTF_TIME (1, 0, 0, "%#p", "am");
+
+  TEST_PRINTF_TIME (1, 0, 0, "%P", "am");
+  TEST_PRINTF_TIME (1, 0, 0, "%^P", "AM");
+  TEST_PRINTF_TIME (1, 0, 0, "%#P", "am");
 
   oldlocale = g_strdup (setlocale (LC_ALL, NULL));
   setlocale (LC_ALL, "fa_IR.utf-8");

commit b1ae8fb85f22be7c17aee73bff006b6d8b9a96d9

gdatetime: Fix minor leaks from strup/strdown calls
These were accidentally introduced in commit 0b114b2687.

Signed-off-by: Philip Withnall <pwithnall@endlessos.org>

glib/gdatetime.c
@@ -2980,13 +2980,17 @@ g_date_time_format_locale (GDateTime   *datetime,
 static inline gboolean
 string_append (GString     *string,
                const gchar *s,
+               gboolean     do_strup,
                gboolean     s_is_utf8)
 {
   gchar *utf8;
   gsize  utf8_len;
+  char *tmp = NULL;
 
   if (s_is_utf8)
     {
+      if (do_strup)
+        s = tmp = g_utf8_strup (s, -1);
       g_string_append (string, s);
     }
   else
@@ -2994,9 +2998,17 @@ string_append (GString     *string,
       utf8 = _g_time_locale_to_utf8 (s, -1, NULL, &utf8_len, NULL);
       if (utf8 == NULL)
         return FALSE;
+      if (do_strup)
+        {
+          tmp = g_utf8_strup (utf8, utf8_len);
+          g_free (utf8);
+          utf8 = g_steal_pointer (&tmp);
+        }
       g_string_append_len (string, utf8, utf8_len);
       g_free (utf8);
     }
+
+  g_free (tmp);
 
   return TRUE;
 }
@@ -3021,6 +3033,7 @@ g_date_time_format_utf8 (GDateTime   *datetime,
   const gchar *mod = "";
   const gchar *name;
   const gchar *tz;
+  char *tmp = NULL;
 
   while (*utf8_format)
     {
@@ -3048,26 +3061,24 @@ g_date_time_format_utf8 (GDateTime   *datetime,
       switch (c)
 	{
 	case 'a':
-          name = mod_case ? g_utf8_strup (WEEKDAY_ABBR (datetime), -1)
-                          : WEEKDAY_ABBR (datetime);
+          name = WEEKDAY_ABBR (datetime);
           if (g_strcmp0 (name, "") == 0)
             return FALSE;
 
           name_is_utf8 = locale_is_utf8 || !WEEKDAY_ABBR_IS_LOCALE;
 
-          if (!string_append (outstr, name, name_is_utf8))
+          if (!string_append (outstr, name, mod_case, name_is_utf8))
             return FALSE;
 
 	  break;
 	case 'A':
-          name = mod_case ? g_utf8_strup (WEEKDAY_FULL (datetime), -1)
-                          : WEEKDAY_FULL (datetime);
+          name = WEEKDAY_FULL (datetime);
           if (g_strcmp0 (name, "") == 0)
             return FALSE;
 
           name_is_utf8 = locale_is_utf8 || !WEEKDAY_FULL_IS_LOCALE;
 
-          if (!string_append (outstr, name, name_is_utf8))
+          if (!string_append (outstr, name, mod_case, name_is_utf8))
             return FALSE;
 
 	  break;
@@ -3076,14 +3087,12 @@ g_date_time_format_utf8 (GDateTime   *datetime,
 			    : MONTH_ABBR_WITH_DAY (datetime);
           if (g_strcmp0 (name, "") == 0)
             return FALSE;
-          if (mod_case)
-            name = g_utf8_strup (name, -1);
 
           name_is_utf8 = locale_is_utf8 ||
             ((alt_digits && !MONTH_ABBR_STANDALONE_IS_LOCALE) ||
              (!alt_digits && !MONTH_ABBR_WITH_DAY_IS_LOCALE));
 
-          if (!string_append (outstr, name, name_is_utf8))
+          if (!string_append (outstr, name, mod_case, name_is_utf8))
             return FALSE;
 
 	  break;
@@ -3092,14 +3101,12 @@ g_date_time_format_utf8 (GDateTime   *datetime,
 			    : MONTH_FULL_WITH_DAY (datetime);
           if (g_strcmp0 (name, "") == 0)
             return FALSE;
-          if (mod_case)
-            name = g_utf8_strup (name, -1);
 
           name_is_utf8 = locale_is_utf8 ||
             ((alt_digits && !MONTH_FULL_STANDALONE_IS_LOCALE) ||
              (!alt_digits && !MONTH_FULL_WITH_DAY_IS_LOCALE));
 
-          if (!string_append (outstr, name, name_is_utf8))
+          if (!string_append (outstr, name, mod_case, name_is_utf8))
               return FALSE;
 
 	  break;
@@ -3147,14 +3154,12 @@ g_date_time_format_utf8 (GDateTime   *datetime,
 			    : MONTH_ABBR_WITH_DAY (datetime);
           if (g_strcmp0 (name, "") == 0)
             return FALSE;
-          if (mod_case)
-            name = g_utf8_strup (name, -1);
 
           name_is_utf8 = locale_is_utf8 ||
             ((alt_digits && !MONTH_ABBR_STANDALONE_IS_LOCALE) ||
              (!alt_digits && !MONTH_ABBR_WITH_DAY_IS_LOCALE));
 
-          if (!string_append (outstr, name, name_is_utf8))
+          if (!string_append (outstr, name, mod_case, name_is_utf8))
             return FALSE;
 
 	  break;
@@ -3281,9 +3286,11 @@ g_date_time_format_utf8 (GDateTime   *datetime,
 	  }
 	  break;
 	case 'Z':
-          tz = mod_case && g_strcmp0 (mod, "#") == 0 ? g_utf8_strdown (g_date_time_get_timezone_abbreviation (datetime), -1)
-                                                     : g_date_time_get_timezone_abbreviation (datetime);
+          tz = g_date_time_get_timezone_abbreviation (datetime);
+          if (mod_case && g_strcmp0 (mod, "#") == 0)
+            tz = tmp = g_utf8_strdown (tz, -1);
           g_string_append (outstr, tz);
+          g_free (tmp);
 	  break;
 	case '%':
 	  g_string_append_c (outstr, '%');