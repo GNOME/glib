824ae3f931c1534996a9f425fbb5f4fd284e1836
Philip Withnall <philip@tecnocode.co.uk>
2023-10-23 00:23:46 +0100

    docs: Move the GIcon SECTION
    Move it to the struct docs.
    
    Signed-off-by: Philip Withnall <philip@tecnocode.co.uk>
    
    Helps: #3037

--- docs/reference/gio/urlmap.js
+++ docs/reference/gio/urlmap.js
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 // SPDX-FileCopyrightText: 2023 Matthias Clasen
 var baseURLs = [
+    [ 'GdkPixbuf', 'https://docs.gtk.org/gdk-pixbuf/' ],
     [ 'GLib', 'https://docs.gtk.org/glib/' ],
     [ 'GModule', 'https://docs.gtk.org/gmodule/' ],
     [ 'GObject', 'https://docs.gtk.org/gobject/' ],
