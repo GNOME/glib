e30f8ac0a109992bc6320c015d45e66c7d11c1a3
Philip Withnall <philip@tecnocode.co.uk>
2023-10-25 17:11:08 +0100

    girepository: Add remaining license/copyright SPDX headers
    These are all custom-written, including a load of research in `git log`
    to find the copyright holders for `girepository/meson.build` and
    `girepository/gi-dump-types.c`.
    
    With this, `reuse lint` raises no complaints about the `girepository/`
    directory.
    
    Signed-off-by: Philip Withnall <philip@tecnocode.co.uk>
    
    Helps: #3155

--- girepository/gi-dump-types.c
+++ girepository/gi-dump-types.c
@@ -1,4 +1,28 @@
-/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*-
+ * GObject introspection: typelib validation, auxiliary functions
+ * related to the binary typelib format
+ *
+ * Copyright (C) 2011 Colin Walters
+ * Copyright (C) 2020 Gisle Vanem
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 #include "gdump.c"
 #ifdef G_OS_WIN32
   #include <windows.h>
