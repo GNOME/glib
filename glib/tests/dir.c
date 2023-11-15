3c42b9c3ee2cc1da1f6ffd63e9f4ed3f63f20ae2
Philip Withnall <pwithnall@gnome.org>
2023-11-15 16:09:44 +0000

    gdir: Add refcounting support and a boxed type
    This allows the methods for `GDir` to be introspected. While we don’t
    expect languages which use the introspection bindings to use `GDir`,
    full introspection support is necessary for the `GDir` documentation to
    be correctly built with gi-docgen.
    
    Signed-off-by: Philip Withnall <pwithnall@gnome.org>
    
    Helps: #3037

--- glib/tests/dir.c
+++ glib/tests/dir.c
@@ -41,6 +41,29 @@ test_dir_nonexisting (void)
   g_error_free (error);
 }
 
+static void
+test_dir_refcounting (void)
+{
+  GDir *dir;
+  GError *local_error = NULL;
+
+  g_test_summary ("Test refcounting interactions with g_dir_close()");
+
+  /* Try keeping the `GDir` struct alive after closing it. */
+  dir = g_dir_open (".", 0, &local_error);
+  g_assert_no_error (local_error);
+
+  g_dir_ref (dir);
+  g_dir_close (dir);
+  g_dir_unref (dir);
+
+  /* Test that dropping the last ref closes it. Any leak here should be caught
+   * when the test is run under valgrind. */
+  dir = g_dir_open (".", 0, &local_error);
+  g_assert_no_error (local_error);
+  g_dir_unref (dir);
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -48,6 +71,7 @@ main (int argc, char *argv[])
 
   g_test_add_func ("/dir/read", test_dir_read);
   g_test_add_func ("/dir/nonexisting", test_dir_nonexisting);
+  g_test_add_func ("/dir/refcounting", test_dir_refcounting);
 
   return g_test_run ();
 }
