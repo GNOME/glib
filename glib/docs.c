3957b47e59f696a936b20db98043155e73c6c277
Philip Withnall <pwithnall@gnome.org>
2023-11-23 13:31:16 +0000

    docs: Move the checkedmath SECTION
    Move it to a separate Markdown file.
    
    Signed-off-by: Philip Withnall <pwithnall@gnome.org>
    
    Helps: #3037

--- glib/docs.c
+++ glib/docs.c
@@ -1702,27 +1702,6 @@
  */
  
 /* Bounds-checked integer arithmetic {{{1 */
-/**
- * SECTION:checkedmath
- * @title: Bounds-checking integer arithmetic
- * @short_description: a set of helpers for performing checked integer arithmetic
- *
- * GLib offers a set of macros for doing additions and multiplications
- * of unsigned integers, with checks for overflows.
- *
- * The helpers all have three arguments.  A pointer to the destination
- * is always the first argument and the operands to the operation are
- * the other two.
- *
- * Following standard GLib convention, the helpers return %TRUE in case
- * of success (ie: no overflow).
- *
- * The helpers may be macros, normal functions or inlines.  They may be
- * implemented with inline assembly or compiler intrinsics where
- * available.
- *
- * Since: 2.48
- */
 
 /**
  * g_uint_checked_add
