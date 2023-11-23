877b0affb6289f79b82072eff4052607b13cbc5e
Philip Withnall <pwithnall@gnome.org>
2023-11-23 13:34:39 +0000

    docs: Move the numerical SECTION
    Move it to a separate Markdown file.
    
    Signed-off-by: Philip Withnall <pwithnall@gnome.org>
    
    Helps: #3037

--- glib/docs.c
+++ glib/docs.c
@@ -1807,23 +1807,6 @@
 /* Numerical Definitions {{{1 */
 
 /**
- * SECTION:numerical
- * @title: Numerical Definitions
- * @short_description: mathematical constants, and floating point decomposition
- *
- * GLib offers mathematical constants such as %G_PI for the value of pi;
- * many platforms have these in the C library, but some don't, the GLib
- * versions always exist.
- *
- * The #GFloatIEEE754 and #GDoubleIEEE754 unions are used to access the
- * sign, mantissa and exponent of IEEE floats and doubles. These unions are
- * defined as appropriate for a given platform. IEEE floats and doubles are
- * supported (used for storage) by at least Intel, PPC and Sparc. See
- * [IEEE 754-2008](http://en.wikipedia.org/wiki/IEEE_float)
- * for more information about IEEE number formats.
- */
-
-/**
  * G_IEEE754_FLOAT_BIAS:
  *
  * The bias by which exponents in single-precision floats are offset.
