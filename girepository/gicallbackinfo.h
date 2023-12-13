553e7cb02dd1b3d0be34d6db248918e79898544d
Philip Withnall <pwithnall@gnome.org>
2023-12-13 00:26:40 +0000

    gicallbackinfo: Port documentation to gi-docgen and update
    Review and update the documentation, making sure it’s complete,
    formatted in gi-docgen format, and has all appropriate GIR annotations
    and `Since:` lines.
    
    Signed-off-by: Philip Withnall <pwithnall@gnome.org>
    
    Helps: #3155

--- girepository/gicallbackinfo.h
+++ girepository/gicallbackinfo.h
@@ -32,10 +32,12 @@
 G_BEGIN_DECLS
 
 /**
- * GI_IS_CALLBACK_INFO
+ * GI_IS_CALLBACK_INFO:
  * @info: an info structure
  *
- * Checks if @info is a #GICallbackInfo or derived from it.
+ * Checks if @info is a [class@GIRepository.CallbackInfo] or derived from it.
+ *
+ * Since: 2.80
  */
 #define GI_IS_CALLBACK_INFO(info)					\
     (gi_base_info_get_info_type ((GIBaseInfo*) info) == GI_INFO_TYPE_CALLBACK)
