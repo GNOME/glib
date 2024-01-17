4d5b6599fc6b28bb734368f1eb179a7d5e82297c
Philip Withnall <pwithnall@gnome.org>
2024-01-17 14:32:23 +0000

    gibaseinfo: Allow parent types to be specified for derived types
    This doesn’t change the type hierarchy for now (i.e. it introduces no
    functional changes), but it will allow us to add some intermediate types
    into the `GIBaseInfo` hierarchy in an upcoming commit.
    
    Signed-off-by: Philip Withnall <pwithnall@gnome.org>
    Helps: #3216

--- girepository/gibaseinfo-private.h
+++ girepository/gibaseinfo-private.h
@@ -47,6 +47,7 @@ void            gi_base_info_init_types              (void);
 GType           gi_base_info_type_register_static    (const char     *type_name,
                                                       size_t          instance_size,
                                                       GClassInitFunc  class_init,
+                                                      GType           parent_type,
                                                       GTypeFlags      type_flags);
 
 G_END_DECLS
