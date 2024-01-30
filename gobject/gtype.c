f1d2919b795d84844f7191a338f7ed7b4d8baee0
Thomas Haller <thaller@redhat.com>
2024-01-30 10:40:07 +0100

    gobject: invoke g_object_weak_ref() one-by-one during destruction
    Previously, at two places (in g_object_real_dispose() and shortly before
    finalize()), we would call
    
        g_datalist_id_set_data (&object->qdata, quark_weak_notifies, NULL);
    
    This clears @quark_weak_notifies at once and then invokes all
    notifications.
    
    This means, if you were inside a notification and called
    g_object_weak_unref() on the object for *another* weak-reference, then
    an exception failed:
    
      GLib-GObject-FATAL-CRITICAL: g_object_weak_unref_cb: couldn't find weak ref 0x401320(0x16b9fe0)
    
    Granted, maybe inside a GWeakNotify you shouldn't call anything on
    where_the_object_was. However, unregistering things (like calling
    g_object_weak_unref() should still reasonably work.
    
    Instead, now remove each weak notification one by one and invoke it.
    
    https://gitlab.gnome.org/GNOME/glib/-/issues/1002

--- gobject/gtype.c
+++ gobject/gtype.c
@@ -5082,3 +5082,9 @@ g_type_ensure (GType type)
   if (G_UNLIKELY (type == (GType)-1))
     g_error ("can't happen");
 }
+
+void
+g_destroy_notify_assert_not_reached (gpointer object)
+{
+  g_return_if_reached ();
+}
