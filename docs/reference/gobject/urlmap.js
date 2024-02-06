4c178fa6230e9ee09ab97d0f0398672e84b20926
Philip Withnall <pwithnall@gnome.org>
2024-02-06 15:39:50 +0000

    build: Combine duplicate urlmap.js files
    Seems no point in keeping them separate. It doesn’t seem to matter if
    they contain entries which are unused for a particular docs build.
    
    Signed-off-by: Philip Withnall <pwithnall@gnome.org>

--- docs/reference/gobject/urlmap.js
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: LGPL-2.1-or-later
-// SPDX-FileCopyrightText: 2023 Matthias Clasen
-var baseURLs = [
-    [ 'GLib', 'https://docs.gtk.org/glib/' ],
-    [ 'GModule', 'https://docs.gtk.org/gmodule/' ],
-    [ 'GObject', 'https://docs.gtk.org/gobject/' ],
-    [ 'Gio', 'https://docs.gtk.org/gio/' ],
-    [ 'Gtk', 'https://docs.gtk.org/gtk4/' ],
-];
