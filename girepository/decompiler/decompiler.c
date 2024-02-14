GNOME/glib

commit 9ab84bc14ad4188c31bafdc758d62824accbb43a

girwriter: Stop using the singleton GIRepository
It’s soon going to disappear.

Signed-off-by: Philip Withnall <pwithnall@gnome.org>

Helps: #3155

girepository/girwriter.c
@@ -1335,10 +1335,10 @@ gi_ir_writer_write (const char *filename,
   FILE *ofile;
   size_t i, j;
   char **dependencies;
-  GIRepository *repository;
+  GIRepository *repository = NULL;
   Xml *xml;
 
-  repository = gi_repository_get_default ();
+  repository = gi_repository_new ();
 
   if (filename == NULL)
     ofile = stdout;
@@ -1462,4 +1462,6 @@ gi_ir_writer_write (const char *filename,
   xml_end_element (xml, "repository");
 
   xml_free (xml);
+
+  g_clear_object (&repository);
 }

commit f80e978f0040ad16d3fa7ac7a5a4c9f28cbc495c

girwriter: Take the GIRepository as a parameter
We certainly won't find the requested namespace in a newly-created
repository with no typelibs loaded.

Fixes: 9ab84bc1 "girwriter: Stop using the singleton GIRepository"
Signed-off-by: Simon McVittie <smcv@collabora.com>

girepository/decompiler/decompiler.c
@@ -118,7 +118,7 @@ main (int argc, char *argv[])
       if (namespace == NULL)
 	g_error ("failed to load typelib: %s", error->message);
 
-      gi_ir_writer_write (output, namespace, needs_prefix, show_all);
+      gi_ir_writer_write (repository, output, namespace, needs_prefix, show_all);
 
       /* when writing to stdout, stop after the first module */
       if (input[i + 1] && !output)

girepository/girwriter-private.h
@@ -24,10 +24,12 @@
 #pragma once
 
 #include <glib.h>
+#include <girepository.h>
 
 G_BEGIN_DECLS
 
-void gi_ir_writer_write (const char *filename,
+void gi_ir_writer_write (GIRepository *repository,
+                         const char *filename,
                          const char *ns,
                          gboolean    needs_prefix,
                          gboolean    show_all);

girepository/girwriter.c
@@ -1319,6 +1319,7 @@ write_union_info (const char *ns,
 
 /**
  * gi_ir_writer_write:
+ * @repository: repository containing @ns
  * @filename: (type filename): filename to write to
  * @ns: GIR namespace to write
  * @needs_prefix: if the filename needs prefixing
@@ -1330,7 +1331,8 @@ write_union_info (const char *ns,
  * Since: 2.80
  */
 void
-gi_ir_writer_write (const char *filename,
+gi_ir_writer_write (GIRepository *repository,
+                    const char *filename,
                     const char *ns,
                     gboolean    needs_prefix,
                     gboolean    show_all)
@@ -1338,10 +1340,7 @@ gi_ir_writer_write (const char *filename,
   FILE *ofile;
   size_t i, j;
   char **dependencies;
-  GIRepository *repository = NULL;
   Xml *xml;
-
-  repository = gi_repository_new ();
 
   if (filename == NULL)
     ofile = stdout;
@@ -1446,6 +1445,4 @@ gi_ir_writer_write (const char *filename,
   xml_end_element (xml, "repository");
 
   xml_free (xml);
-
-  g_clear_object (&repository);
 }