GNOME/glib

commit 9ae43169cfe007988769ee247c725f9db34a4d2e

gobject: fix race in toggle ref during g_object_ref()
Previously:

   1. old_val = atomic_add(&object->ref_count);
   2. if (old_val == 1 && OBJECT_HAS_TOGGLE_REF (object)) { toggle_notify() }

As old_val was 1, you might think that no other thread can have a valid
reference to object. However, that's not the case. For one, GWeakRef can
be used to create another strong reference. More easily, the single
reference can be shared between multiple threads (as long as the code
takes care that the object lives long enough).

That means, another thread can easily add and drop references (including
toggle references). All between step 1 and 2.

A race here might be hard to hit, and the effect might not be obviously
bad. However, consider old_val is 1 due to a normal reference, and
another thread adds a toggle ref between step 1. and 2. Then we would
notify a toggle from 1->2, although a newly added toggle ref is expected
to always start with a normal and a toggle reference. The first toggle
notification is expected to notify about the loss of other references, not
about getting a second reference.

To handle this properly, when we increase the reference count from 1 to
2, we must do so under a lock and check for the toggle notification.

As we now correctly track the toggle behavior, we can also assert in
toggle_refs_get_notify_unlocked() that n_toggle_refs agrees with the
number of references, that is, that the user did always match
g_object_add_toggle_ref() with g_object_remove_toggle_ref().

The downside is here too, that there is now a case (when increasing the
reference count from 1 to 2)  where we need to take the global lock.
That performance problem should be addresses by using per-object locks
instead of a global lock.

gobject/gobject.c
@@ -3587,32 +3587,12 @@ toggle_refs_get_notify_unlocked (GObject *object,
 
   if (tstackptr->n_toggle_refs != 1)
     {
-      /* There are multiple references. We won't notify.
-       *
-       * Note that the user MUST pair g_object_add_toggle_ref() with
-       * g_object_remove_toggle_ref(). In that case, having a
-       * "n_toggle_refs" larger than one, also means that we have multiple
-       * references. */
+      g_critical ("Unexpected number of toggle-refs. g_object_add_toggle_ref() must be paired with g_object_remove_toggle_ref()");
       return NULL;
     }
 
   *out_data = tstackptr->toggle_refs[0].data;
   return tstackptr->toggle_refs[0].notify;
-}
-
-static void
-toggle_refs_notify (GObject *object,
-		    gboolean is_last_ref)
-{
-  GToggleNotify notify;
-  gpointer data;
-
-  G_LOCK (toggle_refs_mutex);
-  notify = toggle_refs_get_notify_unlocked (object, &data);
-  G_UNLOCK (toggle_refs_mutex);
-
-  if (notify)
-    notify (data, object, is_last_ref);
 }
 
 /**
@@ -3781,19 +3761,49 @@ gpointer
 (g_object_ref) (gpointer _object)
 {
   GObject *object = _object;
-  gint old_val;
-  gboolean object_already_finalized;
+  GToggleNotify toggle_notify;
+  gpointer toggle_data;
+  gint old_ref;
 
   g_return_val_if_fail (G_IS_OBJECT (object), NULL);
 
-  old_val = g_atomic_int_add (&object->ref_count, 1);
-  object_already_finalized = (old_val <= 0);
-  g_return_val_if_fail (!object_already_finalized, NULL);
+  old_ref = g_atomic_int_get (&object->ref_count);
 
-  if (old_val == 1 && OBJECT_HAS_TOGGLE_REF (object))
-    toggle_refs_notify (object, FALSE);
+retry:
+  toggle_notify = NULL;
+  if (old_ref > 1 && old_ref < G_MAXINT)
+    {
+      /* Fast-path. We have apparently more than 1 references already. No
+       * special handling for toggle references, just increment the ref count. */
+      if (!g_atomic_int_compare_and_exchange_full ((int *) &object->ref_count,
+                                                   old_ref, old_ref + 1, &old_ref))
+        goto retry;
+    }
+  else if (old_ref == 1)
+    {
+      gboolean do_retry;
 
-  TRACE (GOBJECT_OBJECT_REF (object, G_TYPE_FROM_INSTANCE (object), old_val));
+      /* With ref count 1, check whether we need to emit a toggle notification. */
+      G_LOCK (toggle_refs_mutex);
+      toggle_notify = toggle_refs_get_notify_unlocked (object, &toggle_data);
+      do_retry = !g_atomic_int_compare_and_exchange_full ((int *) &object->ref_count,
+                                                          old_ref, old_ref + 1, &old_ref);
+      G_UNLOCK (toggle_refs_mutex);
+      if (do_retry)
+        goto retry;
+    }
+  else
+    {
+      gboolean object_already_finalized = TRUE;
+
+      g_return_val_if_fail (!object_already_finalized, NULL);
+      return NULL;
+    }
+
+  TRACE (GOBJECT_OBJECT_REF (object, G_TYPE_FROM_INSTANCE (object), old_ref));
+
+  if (toggle_notify)
+    toggle_notify (toggle_data, object, FALSE);
 
   return object;
 }

commit 1bb5661198cd81d73c60ff0583579edaf7f536a3

gobject: fix racy assertion for toggle-refs
We can only assert for having one toggle reference, after we confirmed
(under lock) that the ref count was in the toggle case.

Otherwise, if another thread refs/unrefs the object, we can hit a wrong
g_critical() assertion about

  if (tstackptr->n_toggle_refs != 1)
    {
      g_critical ("Unexpected number of toggle-refs. g_object_add_toggle_ref() must be paired with g_object_remove_toggle_ref()");

Fixes: 9ae43169cfe0 ('gobject: fix race in toggle ref during g_object_ref()')

gobject/gobject.c
@@ -4152,9 +4152,10 @@ retry:
 
       /* With ref count 1, check whether we need to emit a toggle notification. */
       object_bit_lock (object, OPTIONAL_BIT_LOCK_TOGGLE_REFS);
-      toggle_notify = toggle_refs_get_notify_unlocked (object, &toggle_data);
       do_retry = !g_atomic_int_compare_and_exchange_full ((int *) &object->ref_count,
                                                           old_ref, old_ref + 1, &old_ref);
+      if (!do_retry)
+        toggle_notify = toggle_refs_get_notify_unlocked (object, &toggle_data);
       object_bit_unlock (object, OPTIONAL_BIT_LOCK_TOGGLE_REFS);
       if (do_retry)
         goto retry;
@@ -4332,8 +4333,6 @@ retry_beginning:
 
       object_bit_lock (object, OPTIONAL_BIT_LOCK_TOGGLE_REFS);
 
-      toggle_notify = toggle_refs_get_notify_unlocked (object, &toggle_data);
-
       if (!g_atomic_int_compare_and_exchange_full ((int *) &object->ref_count,
                                                    old_ref, old_ref - 1, &old_ref))
         {
@@ -4341,6 +4340,7 @@ retry_beginning:
           goto retry_beginning;
         }
 
+      toggle_notify = toggle_refs_get_notify_unlocked (object, &toggle_data);
       object_bit_unlock (object, OPTIONAL_BIT_LOCK_TOGGLE_REFS);
 
       /* Beware: object might be a dangling pointer. */
@@ -4426,10 +4426,11 @@ retry_decrement:
        *
        * In that case, we need a lock to get the toggle notification. */
       object_bit_lock (object, OPTIONAL_BIT_LOCK_TOGGLE_REFS);
-      toggle_notify = toggle_refs_get_notify_unlocked (object, &toggle_data);
       do_retry = !g_atomic_int_compare_and_exchange_full ((int *) &object->ref_count,
                                                           old_ref, old_ref - 1,
                                                           &old_ref);
+      if (!do_retry)
+        toggle_notify = toggle_refs_get_notify_unlocked (object, &toggle_data);
       object_bit_unlock (object, OPTIONAL_BIT_LOCK_TOGGLE_REFS);
 
       if (do_retry)