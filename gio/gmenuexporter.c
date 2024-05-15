df2c5d925ac4b8f1708bafa5ac1d35acada05d55
Philip Withnall <pwithnall@gnome.org>
2024-05-15 12:26:36 +0100

    gmenuexporter: Fix a NULL pointer dereference on an error handling path
    This latent bug wasn’t triggered until commit 3f30ec86c (or its
    cherry-pick onto `glib-2-80`, 747e3af99, which was first released in
    2.80.1).
    
    That change means that `g_menu_exporter_free()` is now called on the
    registration failure path by `g_dbus_connection_register_object()`
    before it returns. The caller then tries to call `g_slice_free()` on the
    exporter again. The call to `g_menu_exporter_free()` tries to
    dereference/free members of the exporter which it expects to be
    initialised — but because this is happening in an error handling path,
    they are not initialised.
    
    If it were to get any further, the `g_slice_free()` would then be a
    double-free on the exporter allocation.
    
    Fix that by making `g_menu_exporter_free()` robust to some of the
    exporter members being `NULL`, and moving some of the initialisation
    code higher in `g_dbus_connection_export_menu_model()`, and removing the
    duplicate free code on the error handling path.
    
    This includes a unit test.
    
    Signed-off-by: Philip Withnall <pwithnall@gnome.org>
    
    Fixes: #3366

--- gio/gmenuexporter.c
+++ gio/gmenuexporter.c
@@ -707,11 +707,9 @@ g_menu_exporter_create_group (GMenuExporter *exporter)
 }
 
 static void
-g_menu_exporter_free (gpointer user_data)
+g_menu_exporter_free (GMenuExporter *exporter)
 {
-  GMenuExporter *exporter = user_data;
-
-  g_menu_exporter_menu_free (exporter->root);
+  g_clear_pointer (&exporter->root, g_menu_exporter_menu_free);
   g_clear_pointer (&exporter->peer_remote, g_menu_exporter_remote_free);
   g_hash_table_unref (exporter->remotes);
   g_hash_table_unref (exporter->groups);
@@ -794,21 +792,16 @@ g_dbus_connection_export_menu_model (GDBusConnection  *connection,
   guint id;
 
   exporter = g_slice_new0 (GMenuExporter);
-
-  id = g_dbus_connection_register_object (connection, object_path, org_gtk_Menus_get_interface (),
-                                          &vtable, exporter, g_menu_exporter_free, error);
-
-  if (id == 0)
-    {
-      g_slice_free (GMenuExporter, exporter);
-      return 0;
-    }
-
   exporter->connection = g_object_ref (connection);
   exporter->object_path = g_strdup (object_path);
   exporter->groups = g_hash_table_new (NULL, NULL);
   exporter->remotes = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, g_menu_exporter_remote_free);
-  exporter->root = g_menu_exporter_group_add_menu (g_menu_exporter_create_group (exporter), menu);
+
+  id = g_dbus_connection_register_object (connection, object_path, org_gtk_Menus_get_interface (),
+                                          &vtable, exporter, (GDestroyNotify) g_menu_exporter_free, error);
+
+  if (id != 0)
+    exporter->root = g_menu_exporter_group_add_menu (g_menu_exporter_create_group (exporter), menu);
 
   return id;
 }
