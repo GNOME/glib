a7557f125ee9e75ad36bdae60fe0d9ae6abb44b2
Maximiliano Sandoval <msandova@gnome.org>
2024-06-01 16:49:34 +0200

    gfile: Fix finish-func annotations
    The finish-func should not contain the library_class prefix.

--- gio/gfile.c
+++ gio/gfile.c
@@ -3900,7 +3900,7 @@ copy_async_invoke_ready (GObject *file,
 }
 
 /**
- * g_file_copy_async_with_closures: (rename-to g_file_copy_async) (finish-func g_file_copy_finish):
+ * g_file_copy_async_with_closures: (rename-to g_file_copy_async) (finish-func copy_finish):
  * @source: input [type@Gio.File]
  * @destination: destination [type@Gio.File]
  * @flags: set of [flags@Gio.FileCopyFlags]
@@ -4153,7 +4153,7 @@ g_file_move_async (GFile                *source,
 }
 
 /**
- * g_file_move_async_with_closures: (rename-to g_file_move_async) (finish-func g_file_move_finish):
+ * g_file_move_async_with_closures: (rename-to g_file_move_async) (finish-func move_finish):
  * @source: input [type@Gio.File]
  * @destination: destination [type@Gio.File]
  * @flags: set of [flags@Gio.FileCopyFlags]
