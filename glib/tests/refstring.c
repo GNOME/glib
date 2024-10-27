79bfee1371d0b5390c9ea59aacd9e6451c192285
Sebastian Dröge <sebastian@centricular.com>
2024-10-27 13:33:28 +0200

    refstring: Fix race between releasing and re-acquiring an interned GRefString
    There is a race between releasing and re-acquiring an interned
    GRefString if this happens on two threads at the same time. This can
    result in already freed memory to be returned from
    g_ref_string_new_intern().
    
    | Thread 1                       | Thread 2                      |
    | ------------------------------ | ----------------------------- |
    | g_ref_string_release()         | g_ref_string_new_intern()     |
    | g_atomic_rc_box_release_full() | g_mutex_lock()                |
    |                                | g_hash_table_lookup()         |
    | remove_if_interned()           | g_ref_string_acquire()        |
    | g_mutex_lock()                 | g_mutex_unlock()              |
    | g_hash_table_remove()          |                               |
    | g_mutex_unlock()               |                               |
    | g_free()                       |                               |
    |                                | return res; // this is freed  |
    
    This use-after-free usually also gives a critical warning because
    g_atomic_ref_count_inc() checks for the refcount having been 0
    before incrementing.
    
    It is not possible to safely implement weak references via garcbox.
    
    To avoid this race do not implement weak references via garcbox but
    instead implement the allocation of the string manually with a manually
    managed reference count. This allows to safely resurrect the interned
    string if the above race happens, and also avoids other races.
    
    As a side-effect this also
    
      * reduces the allocation size in addition to the actual string length
        from 32 bytes to 16 bytes on 64 bit platforms and keeps it at 16 bytes
        on 32 bit platforms,
    
      * doesn't lock a mutex when freeing non-interned GRefStrings.
    
    Cherry-pick to `glib-2-82`: Drop changes to `g_ref_string_equal()`,
    which doesn’t exist on this branch.

--- glib/tests/refstring.c
+++ glib/tests/refstring.c
@@ -102,6 +102,31 @@ test_refstring_intern (void)
   g_ref_string_release (s);
 }
 
+static gpointer
+intern_ref_unref (gpointer data)
+{
+  for (int i = 0; i < 1000000; i++)
+    {
+      char *s = g_ref_string_new_intern ("test!");
+      g_ref_string_release (s);
+    }
+
+  return NULL;
+}
+
+/* test_refstring_intern: Test that interning of GRefString is thread-safe */
+static void
+test_refstring_intern_thread_safety (void)
+{
+  GThread *a, *b;
+
+  a = g_thread_new (NULL, intern_ref_unref, NULL);
+  b = g_thread_new (NULL, intern_ref_unref, NULL);
+
+  g_thread_join (a);
+  g_thread_join (b);
+}
+
 int
 main (int   argc,
       char *argv[])
@@ -113,6 +138,7 @@ main (int   argc,
   g_test_add_func ("/refstring/length-auto", test_refstring_length_auto);
   g_test_add_func ("/refstring/length-nuls", test_refstring_length_nuls);
   g_test_add_func ("/refstring/intern", test_refstring_intern);
+  g_test_add_func ("/refstring/intern-thread-safety", test_refstring_intern_thread_safety);
 
   return g_test_run ();
 }
