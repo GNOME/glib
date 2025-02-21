135850a5348fedaa2f95e975202f6887908281a4
Simon McVittie <smcv@collabora.com>
2025-02-21 14:38:49 +0000

    tests: Reformat gi-compile-repository.py according to black(1)
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- girepository/tests/gi-compile-repository.py
+++ girepository/tests/gi-compile-repository.py
@@ -20,7 +20,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA  02110-1301  USA
 
-""" Integration tests for gi-compile-repository. """
+"""Integration tests for gi-compile-repository."""
 
 import os
 import unittest
