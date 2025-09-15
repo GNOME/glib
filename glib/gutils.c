ed594d819e94ad2f399c9167bfe37f728e1b6544
Tobias Stoeckmann <tobias@stoeckmann.org>
2025-09-15 22:35:40 +0200

    fuzzing: add special dirs fuzzing test
    Split custom user-dirs.dirs parser into its own gutilsprivate.c file
    for easier fuzzing support and add a fuzzing test.

--- glib/gutils.c
+++ glib/gutils.c
@@ -2241,6 +2241,8 @@ load_user_special_dirs (void)
 
 #else /* default is unix */
 
+#include "gutilsprivate.c"
+
 /* adapted from xdg-user-dir-lookup.c
  *
  * Copyright (C) 2007 Red Hat Inc.
@@ -2269,11 +2271,10 @@ static void
 load_user_special_dirs (void)
 {
   const gchar *config_dir = NULL;
+  const gchar *home_dir;
   gchar *config_file;
   gchar *data;
-  gchar **lines;
-  gint n_lines, i;
-  
+
   config_dir = g_get_user_config_dir_unlocked ();
   config_file = g_build_filename (config_dir,
                                   "user-dirs.dirs",
@@ -2285,115 +2286,10 @@ load_user_special_dirs (void)
       return;
     }
 
-  lines = g_strsplit (data, "\n", -1);
-  n_lines = g_strv_length (lines);
+  home_dir = g_get_home_dir_unlocked ();
+  load_user_special_dirs_from_data ((const gchar *) data, home_dir, g_user_special_dirs);
+
   g_free (data);
-  
-  for (i = 0; i < n_lines; i++)
-    {
-      gchar *buffer = lines[i];
-      gchar *d, *p;
-      size_t len;
-      gboolean is_relative = FALSE;
-      GUserDirectory directory;
-
-      /* Remove newline at end */
-      len = strlen (buffer);
-      if (len > 0 && buffer[len - 1] == '\n')
-	buffer[len - 1] = 0;
-      
-      p = buffer;
-      while (*p == ' ' || *p == '\t')
-	p++;
-      
-      if (strncmp (p, "XDG_DESKTOP_DIR", strlen ("XDG_DESKTOP_DIR")) == 0)
-        {
-          directory = G_USER_DIRECTORY_DESKTOP;
-          p += strlen ("XDG_DESKTOP_DIR");
-        }
-      else if (strncmp (p, "XDG_DOCUMENTS_DIR", strlen ("XDG_DOCUMENTS_DIR")) == 0)
-        {
-          directory = G_USER_DIRECTORY_DOCUMENTS;
-          p += strlen ("XDG_DOCUMENTS_DIR");
-        }
-      else if (strncmp (p, "XDG_DOWNLOAD_DIR", strlen ("XDG_DOWNLOAD_DIR")) == 0)
-        {
-          directory = G_USER_DIRECTORY_DOWNLOAD;
-          p += strlen ("XDG_DOWNLOAD_DIR");
-        }
-      else if (strncmp (p, "XDG_MUSIC_DIR", strlen ("XDG_MUSIC_DIR")) == 0)
-        {
-          directory = G_USER_DIRECTORY_MUSIC;
-          p += strlen ("XDG_MUSIC_DIR");
-        }
-      else if (strncmp (p, "XDG_PICTURES_DIR", strlen ("XDG_PICTURES_DIR")) == 0)
-        {
-          directory = G_USER_DIRECTORY_PICTURES;
-          p += strlen ("XDG_PICTURES_DIR");
-        }
-      else if (strncmp (p, "XDG_PUBLICSHARE_DIR", strlen ("XDG_PUBLICSHARE_DIR")) == 0)
-        {
-          directory = G_USER_DIRECTORY_PUBLIC_SHARE;
-          p += strlen ("XDG_PUBLICSHARE_DIR");
-        }
-      else if (strncmp (p, "XDG_TEMPLATES_DIR", strlen ("XDG_TEMPLATES_DIR")) == 0)
-        {
-          directory = G_USER_DIRECTORY_TEMPLATES;
-          p += strlen ("XDG_TEMPLATES_DIR");
-        }
-      else if (strncmp (p, "XDG_VIDEOS_DIR", strlen ("XDG_VIDEOS_DIR")) == 0)
-        {
-          directory = G_USER_DIRECTORY_VIDEOS;
-          p += strlen ("XDG_VIDEOS_DIR");
-        }
-      else
-	continue;
-
-      while (*p == ' ' || *p == '\t')
-	p++;
-
-      if (*p != '=')
-	continue;
-      p++;
-
-      while (*p == ' ' || *p == '\t')
-	p++;
-
-      if (*p != '"')
-	continue;
-      p++;
-
-      if (strncmp (p, "$HOME", 5) == 0)
-        {
-          p += 5;
-          if (*p != '/' && *p != '"')
-            continue;
-          is_relative = TRUE;
-        }
-      else if (*p != '/')
-        continue;
-
-      d = strrchr (p, '"');
-      if (d < p)
-        continue;
-      *d = 0;
-
-      d = p;
-
-      /* remove trailing slashes */
-      for (len = strlen (d); len > 1 && d[len - 1] == '/'; len--)
-        d[len - 1] = 0;
-
-      if (is_relative)
-        {
-          const gchar *home_dir = g_get_home_dir_unlocked ();
-          g_user_special_dirs[directory] = g_build_filename (home_dir, d, NULL);
-        }
-      else
-	g_user_special_dirs[directory] = g_strdup (d);
-    }
-
-  g_strfreev (lines);
   g_free (config_file);
 }
 
