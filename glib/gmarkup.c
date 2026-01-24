GNOME/glib

commit f323d9de62fb81d0e847f30ba56e2656472eba60

markup: Better error reporting

glib/gmarkup.c
@@ -80,6 +80,10 @@ struct _GMarkupParseContext
   gint char_number;
   gsize offset;
 
+  gint tag_line;
+  gint tag_char;
+  gsize tag_offset;
+
   GMarkupParseState state;
 
   gpointer user_data;
@@ -870,9 +874,11 @@ static void
 push_partial_as_tag (GMarkupParseContext *context)
 {
   GString *str = context->partial_chunk;
+
   /* sadly, this is exported by gmarkup_get_element_stack as-is */
   context->tag_stack = g_slist_concat (get_list_node (context, str->str), context->tag_stack);
   context->tag_stack_gstr = g_slist_concat (get_list_node (context, str), context->tag_stack_gstr);
+
   context->partial_chunk = NULL;
 }
 
@@ -1016,6 +1022,7 @@ emit_start_element (GMarkupParseContext  *context,
                                         (const gchar **)attr_values,
                                         context->user_data,
                                         &tmp_error);
+
   clear_attributes (context);
 
   if (tmp_error != NULL)
@@ -1148,6 +1155,10 @@ g_markup_parse_context_parse (GMarkupParseContext  *context,
           /* Possible next states: INSIDE_OPEN_TAG_NAME,
            *  AFTER_CLOSE_TAG_SLASH, INSIDE_PASSTHROUGH
            */
+          context->tag_line = context->line_number;
+          context->tag_char = context->char_number - 1;
+          context->tag_offset = context->offset - 1;
+
           if (*context->iter == '?' ||
               *context->iter == '!')
             {
@@ -1945,6 +1956,45 @@ g_markup_parse_context_get_offset (GMarkupParseContext *context)
   g_return_val_if_fail (context != NULL, 0);
 
   return context->offset;
+}
+
+/**
+ * g_markup_parse_context_get_tag_start:
+ * @context: a #GMarkupParseContext
+ * @line_number: (out): return location for the line number
+ * @char_number: (out): return location for the character number
+ * @offset: (out): return location for offset from the beginning of the document
+ *
+ * Retrieves the start position of the current start or end tag.
+ *
+ * This function can be used in the `start_element` or `end_element`
+ * callbacks to obtain location information for error reporting.
+ *
+ * Note that @line_number and @char_number are intended for human
+ * readable error messages and are therefore 1-based and in Unicode
+ * characters. @offset on the other hand is meant for programmatic
+ * use, and thus is 0-based and in bytes.
+ *
+ * The information is meant to accompany the values returned by
+ * [method@GLib.MarkupParseContext.get_position], and comes with the
+ * same accuracy guarantees.
+ *
+ * Since: 2.88
+ */
+void
+g_markup_parse_context_get_tag_start (GMarkupParseContext *context,
+                                      gsize               *line_number,
+                                      gsize               *char_number,
+                                      gsize               *offset)
+{
+  g_return_if_fail (context != NULL);
+  g_return_if_fail (line_number != NULL);
+  g_return_if_fail (char_number != NULL);
+  g_return_if_fail (offset != NULL);
+
+  *line_number = context->tag_line;
+  *char_number = context->tag_char;
+  *offset = context->tag_offset;
 }
 
 /**

glib/gmarkup.h
@@ -229,6 +229,13 @@ void                 g_markup_parse_context_get_position (GMarkupParseContext *c
                                                           gint                *char_number);
 GLIB_AVAILABLE_IN_2_88
 gsize                g_markup_parse_context_get_offset   (GMarkupParseContext *context);
+
+GLIB_AVAILABLE_IN_2_88
+void                 g_markup_parse_context_get_tag_start (GMarkupParseContext *context,
+                                                           gsize               *line_number,
+                                                           gsize               *char_number,
+                                                           gsize               *offset);
+
 GLIB_AVAILABLE_IN_ALL
 gpointer             g_markup_parse_context_get_user_data (GMarkupParseContext *context);
 

glib/tests/markup-parse.c
@@ -20,6 +20,37 @@ indent (int extra)
     }
 }
 
+static gsize tag_lines;
+static gsize tag_chars;
+static gsize tag_offset;
+
+static void
+check_positions (GMarkupParseContext *context)
+{
+  gsize chars, lines, offset;
+  int current_lines, current_chars;
+
+  g_markup_parse_context_get_tag_start (context, &lines, &chars, &offset);
+
+  g_assert_cmpint (tag_lines, <=, lines);
+  if (tag_lines == lines)
+    g_assert_cmpint (tag_chars, <=, chars);
+  g_assert_cmpint (tag_offset, <=, offset);
+  tag_lines = lines;
+  tag_chars = chars;
+  tag_offset = offset;
+
+  g_markup_parse_context_get_position (context, &current_lines, &current_chars);
+  lines = (gsize) current_lines;
+  chars = (gsize) current_chars;
+  offset = g_markup_parse_context_get_offset (context);
+
+  g_assert_cmpint (tag_lines, <=, lines);
+  if (tag_lines == lines)
+    g_assert_cmpint (tag_chars, <=, chars);
+  g_assert_cmpint (tag_offset, <=, offset);
+}
+
 static void
 start_element_handler  (GMarkupParseContext *context,
                         const gchar         *element_name,
@@ -29,7 +60,9 @@ start_element_handler  (GMarkupParseContext *context,
                         GError             **error)
 {
   int i;
-  
+
+  check_positions (context);
+
   indent (0);
   g_string_append_printf (string, "ELEMENT '%s'\n", element_name);
 
@@ -54,6 +87,8 @@ end_element_handler (GMarkupParseContext *context,
                      gpointer             user_data,
                      GError             **error)
 {
+  check_positions (context);
+
   --depth;
   indent (0);
   g_string_append_printf (string, "END '%s'\n", element_name);
@@ -164,6 +199,10 @@ test_file (const gchar       *filename,
   const gsize chunk_sizes_bytes[] = { 1, 2, 5, 12, 1024 };
   gsize i;
   GString *first_string = NULL;
+
+  tag_lines = 0;
+  tag_chars = 0;
+  tag_offset = 0;
 
   g_file_get_contents (filename, &contents, &length_bytes, &local_error);
   g_assert_no_error (local_error);

commit 86b18a51d8bad8100207420ecee4c2150e3537e6

markup: Initialize tag positions
These fields are parallel to the fields for the current
position, and thus need to be initialized to the same
values.

Fixes: f323d9de62fb81d0e8

glib/gmarkup.c
@@ -189,6 +189,10 @@ g_markup_parse_context_new (const GMarkupParser *parser,
   context->char_number = 1;
   context->offset = 0;
 
+  context->tag_line = 1;
+  context->tag_char = 1;
+  context->tag_offset = 0;
+
   context->partial_chunk = NULL;
   context->spare_chunks = NULL;
   context->spare_list_nodes = NULL;