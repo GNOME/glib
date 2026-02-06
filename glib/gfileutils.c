GNOME/glib

commit b2a0566cfab1b5c16e30fbbe18274b8565af61af

gfileutils: Use unsigned integers in g_path_get_basename()
This avoids the possibility of string length overflow on systems which
inexplicably allow paths longer than `PATH_MAX`.

Rather than devoting a whole bit of the `last_nonslash` and `base`
variables to “did the `while` loop reach zero”, instead work that out
using an extra clause on the following `if` statement.

This is not a security issue as realistically no path can be near
`SSIZE_MAX` in length.

Signed-off-by: Philip Withnall <pwithnall@gnome.org>

Fixes: #3883

glib/gfileutils.c
@@ -2619,8 +2619,8 @@ g_basename (const gchar *file_name)
 gchar *
 g_path_get_basename (const gchar *file_name)
 {
-  gssize base;
-  gssize last_nonslash;
+  size_t base;
+  size_t last_nonslash;
   gsize len;
   gchar *retval;
 
@@ -2631,10 +2631,10 @@ g_path_get_basename (const gchar *file_name)
 
   last_nonslash = strlen (file_name) - 1;
 
-  while (last_nonslash >= 0 && G_IS_DIR_SEPARATOR (file_name [last_nonslash]))
+  while (last_nonslash > 0 && G_IS_DIR_SEPARATOR (file_name[last_nonslash]))
     last_nonslash--;
 
-  if (last_nonslash == -1)
+  if (last_nonslash == 0 && G_IS_DIR_SEPARATOR (file_name[0]))
     /* string only containing slashes */
     return g_strdup (G_DIR_SEPARATOR_S);
 
@@ -2647,11 +2647,12 @@ g_path_get_basename (const gchar *file_name)
 #endif
   base = last_nonslash;
 
-  while (base >=0 && !G_IS_DIR_SEPARATOR (file_name [base]))
+  while (base > 0 && !G_IS_DIR_SEPARATOR (file_name[base]))
     base--;
 
 #ifdef G_OS_WIN32
-  if (base == -1 &&
+  if (base == 0 &&
+      !G_IS_DIR_SEPARATOR (file_name[0]) &&
       g_ascii_isalpha (file_name[0]) &&
       file_name[1] == ':')
     base = 1;

commit 961c617e121275565d5196d5f519af9dfe63d343

gfileutils: Fix a regression in `g_path_get_basename()` for plain files
Commit b2a0566cfab1b5c16e30fbbe18274b8565af61af accidentally broke calls
of `g_path_get_basename()` on input strings like `OSTree-1.0.gir`, which
contain no directory separators.

This is because, in those cases, the `base` variable would
_legitimately_ have value `-1`, which then cancelled out with some other
factors to produce the right result. After the refactoring in that
commit, that logic was lost (because it was pretty opaque).

Rework the code, adding some comments and incrementing the value of
`base` explicitly to make it point to the first character of the
basename we want to extract, rather than the directory separator
immediately preceding it (or figuratively `-1` if there was no such
directory separator).

Signed-off-by: Philip Withnall <pwithnall@gnome.org>

Fixes: https://gitlab.gnome.org/GNOME/glib/-/merge_requests/4999#note_2672815

glib/gfileutils.c
@@ -2651,18 +2651,32 @@ g_path_get_basename (const gchar *file_name)
   while (base > 0 && !G_IS_DIR_SEPARATOR (file_name[base]))
     base--;
 
-#ifdef G_OS_WIN32
-  if (base == 0 &&
-      !G_IS_DIR_SEPARATOR (file_name[0]) &&
-      g_ascii_isalpha (file_name[0]) &&
-      file_name[1] == ':')
-    base = 1;
-#endif /* G_OS_WIN32 */
+  /* Does the file_name start without a directory separator, with the only
+   * directory separators being at the end of the string? e.g. `dir/` */
+  if (base == 0 && !G_IS_DIR_SEPARATOR (file_name[0]))
+    {
+      base = 0;
 
-  len = last_nonslash - base;
+#ifdef G_OS_WIN32
+      /* Does it start with a drive letter? e.g. `C:dir/`
+       * If so, skip that. */
+      if (g_ascii_isalpha (file_name[0]) &&
+          file_name[1] == ':')
+        base = 2;
+#endif /* G_OS_WIN32 */
+    }
+  else
+    {
+      /* Otherwise, `base` now points at the last directory separator character
+       * before the component we want as the basename, so increase the index
+       * again. */
+      base += 1;
+    }
+
+  len = last_nonslash - base + 1;
   g_assert (len < SIZE_MAX);
   retval = g_malloc (len + 1);
-  memcpy (retval, file_name + (base + 1), len);
+  memcpy (retval, file_name + base, len);
   retval[len] = '\0';
 
   return retval;