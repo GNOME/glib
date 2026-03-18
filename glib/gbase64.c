bd70c4f00aa64c3b15951daf6bd8c3d9a762e47e
Alyssa Ross <hi@alyssa.is>
2026-03-18 11:45:29 +0100

    g_base64_decode_step: remove incorrect type in doc
    My compiler gives me a -Wpointer-sign warning when I try to pass a
    guint8 here.  It looks like the function would /like/ to take a guint8
    input, since it casts to that every time it uses it, but it doesn't.
    The correct type should be inferred from the code without the
    element-type annotation.

--- glib/gbase64.c
+++ glib/gbase64.c
@@ -286,7 +286,7 @@ static const unsigned char mime_base64_rank[256] = {
 
 /**
  * g_base64_decode_step: (skip)
- * @in: (array length=len) (element-type guint8): binary input data
+ * @in: (array length=len): base-64 encoded input data
  * @len: max length of @in data to decode
  * @out: (out caller-allocates) (array) (element-type guint8): output buffer
  * @state: (inout): Saved state between steps, initialize to 0
