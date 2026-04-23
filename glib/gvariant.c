GNOME/glib

commit ebbeb3180d4a1a288ec6953ed29324759e5f3f2e

gvariant: Tweak GVariantIter heap allocation size
This re-works commit 608b846f382c28b4f98639ae295cf8a52ce3f56e, because
it didn’t actually fix the warning on msys2-clang64.

Rather than go through contortions to try and squash that warning, just
allocate the full size struct instead. If people are worried about the
allocation size here, they really should already be using a stack
allocated GVariantIter instead.

Fixes CI failure https://gitlab.gnome.org/GNOME/glib/-/jobs/6444549.

Signed-off-by: Philip Withnall <pwithnall@gnome.org>

glib/gvariant.c
@@ -2989,7 +2989,7 @@ g_variant_iter_new (GVariant *value)
 {
   GVariantIter *iter;
 
-  iter = (GVariantIter *) (void *) g_slice_new (struct heap_iter);
+  iter = g_slice_new (GVariantIter);
   GVHI(iter)->value_ref = g_variant_ref (value);
   GVHI(iter)->magic = GVHI_MAGIC;
 

commit 6522dca1a609cfa2084b979018635e49211919df

gvariant: Match allocation size of g_slice_new to g_slice_free
Commit ebbeb318 "gvariant: Tweak GVariantIter heap allocation size"
changed the code path that allocates iterators on the heap to use
the full size of the public iterator struct (including padding),
but didn't change the matching g_slice_free() call. g_slice_free() is
implemented as basically g_free_sized(), so the size passed to it
should match the size that was allocated.

Fixes: ebbeb318 "gvariant: Tweak GVariantIter heap allocation size"
Signed-off-by: Simon McVittie <smcv@collabora.com>

glib/gvariant.c
@@ -3100,7 +3100,7 @@ g_variant_iter_free (GVariantIter *iter)
   g_variant_unref (GVHI(iter)->value_ref);
   GVHI(iter)->magic = 0;
 
-  g_slice_free (struct heap_iter, GVHI(iter));
+  g_slice_free (GVariantIter, iter);
 }
 
 /**